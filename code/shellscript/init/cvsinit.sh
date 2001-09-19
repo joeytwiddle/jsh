7eb5c0e582bb9cd00c422d0500f2ebc7b1e18bc2
joey <joey>
2001-09-19 15:52:47 +0000

    *** empty log message ***

--- code/shellscript/init/cvsinit.sh
+++ code/shellscript/init/cvsinit.sh
@@ -1,4 +1,9 @@
 export CVSROOT=/stuff/cvsroot
 export CVSREAD=yes
+
+# For remote:
+# export CVS_RSH=ssh
+# export CVSROOT=:ext:joey@hwi.dyn.dhs.org:/stuff/cvsroot
+
 # export VISUAL=vi
 # export EDITOR=vi
