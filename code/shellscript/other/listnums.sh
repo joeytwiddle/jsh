20776874feed5d418b8e2609ebbe4a25fbca4366
joey <joey>
2001-10-24 17:25:27 +0000

    *** empty log message ***

---
+++ code/shellscript/other/listnums.sh
@@ -0,0 +1,23 @@
+# #!/bin/sh
+
+START=$1
+END=$2
+INC="1";
+if test "$3" = "by"; then
+	INC="$4"
+fi
+
+seq $START $INC $END
+
+# X=$START
+# LIST=""
+# DONE=""
+# while [ ! "$X" = "$END" ]; do
+  # # LIST="$LIST$X "
+  # echo "$X"
+  # X=$[$X+$INC]
+  # # X=$[$X+1]
+# done
+# echo $END
+# # LIST="$LIST$END"
+# # echo "$LIST"
