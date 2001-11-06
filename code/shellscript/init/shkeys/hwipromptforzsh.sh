025bc2f127a80bc3b0814d6d18bfadf5f3b31c31
joey <joey>
2001-11-06 00:08:25 +0000

    *** empty log message ***

--- code/shellscript/init/shkeys/hwipromptforzsh.sh
+++ code/shellscript/init/shkeys/hwipromptforzsh.sh
@@ -2,8 +2,9 @@
 
 # Hwi:
 export PROMPT="%{[01;31m%}%? %{[00;32m%}%~/%{[01;37m%}%{%} "
-export RPROMPT="%{[00;37m%}%n@%{[00;37m%}%m %{[00;36m%}%* %{[00;37m%}(%{[00;35m%}%h%{[00;37m%}:%{[00;33m%}%l%{[00m%})"
-# export RPROMPT="%{ %}%{[00;37m%}%n@%{[00;37m%}%m %{[00;36m%}%* %{[00;37m%}(%{[00;35m%}%h%{[00;37m%}:%{[00;33m%}%l%{[00m%})%{m%}"
+export RPROMPT="%{ %}%{[00;37m%}%n@%{[00;37m%}%m %{[00;36m%}%* %{[00;37m%}(%{[00;35m%}%h%{[00;37m%}:%{[00;33m%}%l%{[00m%})%{m%}"
+# The trick in the line above to push RPROMPT one char right, has a problem in gnome-terminal, in which case use the line below
+# export RPROMPT="%{[00;37m%}%n@%{[00;37m%}%m %{[00;36m%}%* %{[00;37m%}(%{[00;35m%}%h%{[00;37m%}:%{[00;33m%}%l%{[00m%})"
 # bright white:
 # export PROMPT="%{[01;31m%}%? %{[00;32m%}%~/%{[01;37m %} "
 # export RPROMPT="%{[00;37m%}%n@%{[00;37m%}%m %{[00;36m%}%* %{[00;37m%}(%{[00;35m%}%h%{[00;37m%}:%{[00;33m%}%l%{[01;37m%})"
