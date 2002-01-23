7a33c56a3c38aafb3c8f3663b234a0c57e1aff43
joey <joey>
2002-01-23 04:45:31 +0000

    Does directories just before files instead of two separate clumps. Depends on find to order them as such.

--- code/shellscript/cvswrap/cvsdiff.sh
+++ code/shellscript/cvswrap/cvsdiff.sh
@@ -20,25 +20,47 @@ if test -d "$CHKOUT"; then
   cd "$CHKOUT"
 fi
 
-find . -type d | grep -v "/CVS" |
-  while read DIR; do
-    if test ! -d "$CVSROOT/$CHKOUT/$DIR/CVS/"; then
-      echo 'cvs add "'$DIR'"'
-    fi
-  done
-
-find . -type f | grep -v "/CVS/" |
-  while read FILE; do
-    COUNT=`expr $COUNT + 1`
-    CVSFILE="$CVSROOT/$CHKOUT/$FILE,v"
-    if test ! -f "$CVSFILE"; then
-      MISSING=`expr $MISSING + 1`
-      if test $DOADD; then
-        echo 'cvs add "'$FILE'"'
-      else
-        echo "$FILE"
+find . | grep -v "/CVS" |
+  while read SOMETHING; do
+    if test -d "$SOMETHING"; then
+      DIR="$SOMETHING"
+      if test ! -d "$CVSROOT/$CHKOUT/$DIR/CVS/"; then
+        echo 'cvs add "'$DIR'"'
+      fi
+    else
+      FILE="$SOMETHING"
+      COUNT=`expr $COUNT + 1`
+      CVSFILE="$CVSROOT/$CHKOUT/$FILE,v"
+      if test ! -f "$CVSFILE"; then
+        MISSING=`expr $MISSING + 1`
+        if test $DOADD; then
+          echo 'cvs add "'$FILE'"'
+        else
+          echo "$FILE"
+        fi
       fi
     fi
   done
+
+# find . -type d | grep -v "/CVS" |
+  # while read DIR; do
+    # if test ! -d "$CVSROOT/$CHKOUT/$DIR/CVS/"; then
+      # echo 'cvs add "'$DIR'"'
+    # fi
+  # done
+# 
+# find . -type f | grep -v "/CVS/" |
+  # while read FILE; do
+    # COUNT=`expr $COUNT + 1`
+    # CVSFILE="$CVSROOT/$CHKOUT/$FILE,v"
+    # if test ! -f "$CVSFILE"; then
+      # MISSING=`expr $MISSING + 1`
+      # if test $DOADD; then
+        # echo 'cvs add "'$FILE'"'
+      # else
+        # echo "$FILE"
+      # fi
+    # fi
+  # done
 
 echo "$MISSING / $COUNT files missing." >&2
