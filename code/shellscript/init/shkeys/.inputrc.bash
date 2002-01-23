b144e3cfd096c71cca76318cc45b957b4b12c9d0
joey <joey>
2002-01-23 23:39:22 +0000

    *** empty log message ***

---
+++ code/shellscript/init/shkeys/.inputrc.bash
@@ -0,0 +1,4 @@
+"\C-d" backward-word
+"\C-f" forward-word
+"\C-x" backward-kill-word
+"\C-v" kill-word
--- code/shellscript/init/startj-hwi.sh
+++ code/shellscript/init/startj-hwi.sh
@@ -52,9 +52,11 @@ bindkey "^r" expand-history
 # My preferred word movement and deletion.
 # It's purely left-handed and positioned to avoid the reserved keys qeaszc.
 # Oops d is reserved and I use it, but it works OK on Hwi.
+# Movement in big jumps
 bindkey "^d" backward-word
 bindkey "^f" forward-word
-bindkey "^x" backward-kill-word
+# Deletion in small chunks
+bindkey "^x" vi-backward-kill-word
 bindkey "^v" kill-word
 
 # trying to jump words with CTRL+arrows
