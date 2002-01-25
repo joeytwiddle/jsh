b2dd42e72d1017f7b571c2d6da105428c3b7fdbc
joey <joey>
2002-01-25 18:06:33 +0000

    *** empty log message ***

--- code/shellscript/wrap/jmake.sh
+++ code/shellscript/wrap/jmake.sh
@@ -1,1 +1,3 @@
-g++ -I $JPATH/joeylib/.comp/ $1.c -w -o $1 $JPATH/joeylib/.comp/joeylib.o
+g++ -g -I $JPATH/joeylib/.comp/ $1.c -w -o $1 $JPATH/joeylib/.comp/joeylib.o
+# g++ -I $JPATH/joeylib/.comp/ $1.c -w -o $1 $JPATH/joeylib/.comp/joeylib.o
+
