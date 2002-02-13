bacc41dcf2b8e306db24c77b99eb45e056d1b7dc
joey <joey>
2002-02-13 01:16:20 +0000

    Now deprecated by resolvedir.

--- code/shellscript/standin/realpath.sh
+++ code/shellscript/standin/realpath.sh
@@ -1,15 +1,3 @@
 `jwhich realpath` $*
 
-# X=$1;
-# Y="";
-# while test ! "$X" = "/" -a ! "$X" = "."; do
-  # C=`filename "$X"`
-  # L=`justlinks "$X"`
-  # if test ! "$L" = ""; then
-    # Y="$L$Y"
-  # else
-    # Y="/$C$Y"
-  # fi
-  # X=`dirname $X`
-# done
-# echo "$Y"
+# Can be removed really.  resolvedir does realpath with sh scripts.
