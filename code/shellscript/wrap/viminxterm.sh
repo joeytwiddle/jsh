7312a251918dcded75d658140c35de1edadfa017
joey <joey>
2002-04-12 22:12:21 +0000

    *** empty log message ***

--- code/shellscript/wrap/viminxterm.sh
+++ code/shellscript/wrap/viminxterm.sh
@@ -1,68 +1,54 @@
 #!/bin/sh
-# #!/usr/local/bin/zsh
 
-jwhich inj vim > /dev/null
+# jwhich inj vim > /dev/null
+jwhich vim > /dev/null
 if test ! "$?" = "0"; then
-  echo "Oh no!  vim not present"
-  exit 1
+	echo "viminxterm failing: vim not present"
+	exit 1
 fi
 
 FILE="$1";
 
-# MAXVOL=$[70*40];
-MAXVOL=$((80*50));
-ARGS=$*; # -bg white -fg black
+MAXVOL=`expr 80 "*" 50`
 
 if test -f "$FILE"; then
 
-  # Determine optimal height
-  LINES=`cat $FILE | countlines`
-  # echo "lines=$LINES"
-  ROWS="50"
-  if test $LINES -lt $ROWS; then
-    # ROWS=$[$LINES+2];
-    # ROWS=$(($LINES+2));
-echo "A"
-    # ROWS=$(($(($LINES+2))*1.1));
-    ROWS=`eval $LINES+2`;
-echo "B $ROWS"
-  fi
-  
-  # Determine optimal width
-  LONGEST=`longestline $FILE`
-  # echo "cols=$LONGEST"
-  # LONGEST=$[$LONGEST+1]; # THIS DOESN'T WORK WITH sh ON Solaris!
-  # LONGEST=$(($LONGEST+2));
-  LONGEST=$(($(($LONGEST+2))*1.1));
-  #echo "$LONGEST"
-  
-  # Determine optimal distribution
-  # COLS=$[$MAXVOL/$ROWS];
-  COLS=$(($MAXVOL/$ROWS));
-  #echo "$MAXVOL/$ROWS = $COLS but $LONGEST"
-  if test $LONGEST -lt $COLS; then
-    COLS=$LONGEST;
-  fi
+	# Determine optimal height
+	LINES=`cat $FILE | countlines`
+	ROWS="50"
+	if test $LINES -lt $ROWS; then
+		ROWS=`expr '(' $LINES '+' 2 ')' '*' 11 '/' 10`;
+	fi
 
-  # echo "$COLS"x"$ROWS"
-  if test $COLS -lt 20; then
-    COLS=20
-  fi
-  if test $ROWS -lt 5; then
-    ROWS=5
-  fi
+	# Determine optimal width
+	LONGEST=`longestline $FILE`
+	LONGEST=`expr "(" $LONGEST "+" 2 ")" "*" 11 "/" 10`
+
+	# Determine optimal distribution
+	COLS=`expr $MAXVOL / $ROWS`
+	if test $LONGEST -lt $COLS; then
+		COLS=$LONGEST;
+	fi
+
+	if test $COLS -lt 20; then
+		COLS=20
+	fi
+	if test $ROWS -lt 5; then
+		ROWS=5
+	fi
 
 else
 
-  COLS=40
-  ROWS=20
-  
+	# Default size for new file
+	COLS=40
+	ROWS=20
+
 fi
 
 # echo "$COLS"x"$ROWS"
 
 INTGEOM=`echo "$COLS"x"$ROWS" | sed 's|\..*x|x| ; s|\..*$||'`
-  
+
 # TITLE="vi:$ARGS"
 # TITLE=`filename "$ARGS"`"("`dirname "$ARGS"`"/)" # This seems to be what Vim actually forces on the xterm.
 TITLE=`absolutepath "$ARGS"`" [vim]"
@@ -71,7 +57,7 @@ TITLE=`absolutepath "$ARGS"`" [vim]"
 # XTFONT='-schumacher-clean-medium-r-normal-*-*-150-*-*-c-*-iso646.1991-irv';
 # XTFONT='-b&h-lucidatypewriter-medium-r-normal-*-*-120-*-*-m-*-iso8859-1';
 XTFONT='-b&h-lucidatypewriter-medium-r-normal-*-*-80-*-*-m-*-iso8859-1';
-`jwhich xterm` -fg white -bg black -geometry $INTGEOM -font $XTFONT -title "$TITLE" -e vim $*
+`jwhich xterm` -fg white -bg black -geometry $INTGEOM -font $XTFONT -title "$TITLE" -e vim "$@"
 
 # xterm -geometry 70x40 -font '-b&h-lucidatypewriter-medium-r-normal-*-*-100-*-*-m-*-iso8859-1' -title "vim:$ARGS" -e "vim $ARGS"
 # gnome-terminal -geometry 70x40 --font='-b&h-lucidatypewriter-medium-r-normal-*-*-100-*-*-m-*-iso8859-1' --title="vim:$ARGS" --execute="vim $ARGS"
