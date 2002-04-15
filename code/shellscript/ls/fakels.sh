1b0df6491e9224c948a863e6d584a65aabad91cf
joey <joey>
2002-04-15 23:58:37 +0000

    *** empty log message ***

--- code/shellscript/ls/fakels.sh
+++ code/shellscript/ls/fakels.sh
@@ -1,1 +1,3 @@
-ls -atrF -x |highlight / green | highlight "*" red
+# This is useful for systems which do not suppoer ls --color
+# but do support ls -F, so we can search for filetypes!
+ls -atrF -C | highlight "/" green | highlight "*" red
