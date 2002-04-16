3b4ac3144f4858addad17c5b43ace1fa4200542f
joey <joey>
2002-04-16 16:14:03 +0000

    *** empty log message ***

--- code/shellscript/unixwrap/jman.sh
+++ code/shellscript/unixwrap/jman.sh
@@ -1,5 +1,5 @@
 # xterm -fg black -bg white -geometry 68x60 -e "man $*"
 # whitewin -geometry 68x60 -e "man $*"
 WIDTH=`man $* | col -bx | longestline`
-WIDTH=$(($WIDTH+2))
+WIDTH=`expr $WIDTH + 2`
 whitewin -geometry "$WIDTH"x60 -e "man $*"
