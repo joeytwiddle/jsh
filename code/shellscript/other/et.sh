feec22e8118548a7641cf383fdb030c26ba0a165
joey <joey>
2002-04-16 01:40:00 +0000

    *** empty log message ***

--- code/shellscript/other/et.sh
+++ code/shellscript/other/et.sh
@@ -2,71 +2,44 @@
 
 NAME="$1"
 if test "$NAME" = "-f"; then
-  FORCE="-f";
-  NAME="$2"
+	FORCE="-f";
+	NAME="$2"
 fi
-# TOOL=`find "$JPATH/code/shellscript/" -name "$NAME"`
-# TOOL=`justlinks $JPATH/tools/ | grep "^$NAME===" | after "==="`
 LSLINE=`justlinks $JPATH/tools/$NAME`
-#echo "Got: $LSLINE"
 
 TOOL="$LSLINE";  # `echo "$LSLINE" | after symlnk`
-# echo "Found: $TOOL"
 if test "x$TOOL" = "x"; then TOOL="."; fi
 # Can't put quotes around the -f "$TOOL" !
 if test "x$TOOL" != "x" -a -f $TOOL; then
-  echo -e -n ""
-  # echo "Found tool: $TOOL"
+	echo -e -n ""
 else
-  TOOL="$PWD/$NAME.sh"
+	TOOL="$PWD/$NAME.sh"
 
-  #echo "Tool not found.  Would you like me to make $TOOL ?"
-  #read ans
-  #if [ "$ans" = "y" -o "$ans" = "yes" ]; then
-
-  echo "Tool not found.  Please enter $JPATH/code/shellscript/<path>/$NAME.sh"
-  ( cd $JPATH/code/shellscript/ &&
-    ls -d */ )
-  read theirpath
-  if [ ! "A$theirpath" = "A" ]; then
-    TOOL="$JPATH/code/shellscript/$theirpath/$NAME.sh"
-    mkdir -p `dirname "$TOOL"`
-    echo "Creating new tool $TOOL"
-    touch "$TOOL"
-    chmod a+x "$TOOL"
-    ln -sf "$TOOL" "$JPATH/tools/$NAME"
-  else
-    exit 1
-  fi
+	echo "Tool not found.  Please enter $JPATH/code/shellscript/<path>/$NAME.sh"
+	echo "Suggested directories:"
+	( cd $JPATH/code/shellscript/ &&
+	ls -d `find . -type d | grep -v "/CVS"` )
+	# ls -d */ )
+	read theirpath
+	if [ ! "A$theirpath" = "A" ]; then
+		TOOL="$JPATH/code/shellscript/$theirpath/$NAME.sh"
+		mkdir -p `dirname "$TOOL"`
+		echo "Creating new tool $TOOL"
+		touch "$TOOL"
+		chmod a+x "$TOOL"
+		ln -sf "$TOOL" "$JPATH/tools/$NAME"
+	else
+		exit 1
+	fi
 fi
-#echo ">$TOOL<"
 
 edit $FORCE "$TOOL" # now handles below
 
-#if xisrunning; then
-#  editandwait $FORCE "$TOOL" &
-#else
-#  editandwait $FORCE "$TOOL"
-#fi
-
-
-# Neither on Unix:
+# A quick check to inform the user if this command already exists on system.
+# which, where, and whereis are never guaranteed:
 # whereis $1
 # which $1
 # jwhere $1
-jwhich $1 quietly
-jwhich inj $1 quietly
-
-
-
-
-
-#case $TERM in
-#  xterm)
-#    editandwait $JPATH/tools/$1 && chmod a+x $JPATH/tools/$1 &
-#    ;;
-#  *)
-#    editandwait $JPATH/tools/$1
-#    chmod a+x $JPATH/tools/$1
-#    ;;
-#esac
+if jwhich $1 quietly; then
+	jwhich $1
+fi
