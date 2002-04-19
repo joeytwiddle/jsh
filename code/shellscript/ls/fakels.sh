fe9a21104a298c30f4dd72355746cb418f9aed89
joey <joey>
2002-04-19 22:53:13 +0000

    *** empty log message ***

--- code/shellscript/ls/fakels.sh
+++ code/shellscript/ls/fakels.sh
@@ -1,3 +1,4 @@
 # This is useful for systems which do not suppoer ls --color
 # but do support ls -F, so we can search for filetypes!
+# It is an approximation of my basic LSCOLS of directories, executables and symlinks.
 ls -atrF -C "$@" | highlight "/" green | highlight -bold "*" red | highlight "@" yellow
--- code/shellscript/other/jwatchcomp
+++ code/shellscript/other/jwatchcomp
@@ -2,8 +2,8 @@ jfc simple oneway "$1" "$2"
 OTHER=`jfc simple oneway "$2" "$1"`
 if ! test "x$OTHER" = "x"; then
   echo
-  echo "<<< DIED:"
+  echo `cursered``cursebold`"<<< DIED:"
   echo "$OTHER"
-  echo ">>>"
+  echo ">>>"`cursegrey`
   echo
 fi
