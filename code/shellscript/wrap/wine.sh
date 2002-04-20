2f2db37191be5c92c01a88ed251eb09c798bdb7b
joey <joey>
2002-04-20 23:45:49 +0000

    *** empty log message ***

--- code/shellscript/wrap/wine.sh
+++ code/shellscript/wrap/wine.sh
@@ -1,2 +1,2 @@
-xset fp- unix/:7101
+xset fp- unix/:7101 2>/dev/null # really I only want to ungrep "warning, no entries deleted from font path" the error stream, in case anything unexpected is wrong with the command.
 `jwhich wine` "$@"
