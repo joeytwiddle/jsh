e262fe66bcd6a5959e2fe5bc0128725d52182d0b
joey <joey>
2002-05-07 22:14:14 +0000

    *** empty log message ***

--- code/shellscript/init/shprompts/hwipromptforbash.sh
+++ code/shellscript/init/shprompts/hwipromptforbash.sh
@@ -18,10 +18,12 @@ PS1="\[\033[00;33m\]\!\[\033[01;31m\]\$ \[\033[00;37m\](\[\033[00;36m\]\\h \[\03
 # Panic:
 if test `hostname` = "panic"; then
   PS1='\['`curseyellow`'\]\!\['`cursered``cursebold`'\]\$\['`cursegrey`'\]/\['`cursemagenta`'\]\u\['`cursegrey`'\] \['`curseblue`'\]\t\['`cursegrey`'\] \['`cursemagenta`'\]\h\['`cursegrey`'\]\\\\\['`cursegreen`'\]\w/\['`cursegrey`'\] '
+  # magenta style, panic colors PS1="\[\033[00;33m\]\!\[\033[01;31m\]\$ \[\033[00;35m\]\u\[\033[01;37m\]\\\\\[\033[00;34m\]\t\[\033[01;37m\]/\[\033[01;35m\]\h \[\033[00;32m\]\w/\[\033[01;37m\] "
 fi
 
 if test `hostname` = "magenta" -o "$USER" = "pru"; then
 # if test `hostname` = "colossus" || test "$USER" = "pru"; then
+  # panic style, magenta colors PS1='\['`cursegreen`'\]\$\['`cursecyan`'\]\!\['`cursegrey`'\]/\['`curseblue`'\]\u\['`cursegrey`'\]\\\['`cursemagenta`'\]\t\['`cursegrey`'\]/\['`curseblue`'\]\h\['`cursegrey`'\]\\\\\['`cursecyan`'\]\w/\['`cursegrey`'\] '
   PS1="\[\033[00;32m\]\$\[\033[00;36m\]\! \[\033[01;34m\]\u\[\033[01;37m\]\\\\\[\033[00;35m\]\t\[\033[01;37m\]/\[\033[01;34m\]\h \[\033[01;36m\]\w/\[\033[01;37m\] "
   # Gnome?
   # PS1="\[\033[00;33m\]\!\[\033[01;31m\]\$\[\033[00;37m\](\[\033[00;35m\]\u\[\033[00;37m\]|\[\033[00;36m\]\t\[\033[00;37m\]|\[\033[00;35m\]\h\[\033[00;37m\])\[\033[00;32m\]\w/\[\033[00;37m\] "
