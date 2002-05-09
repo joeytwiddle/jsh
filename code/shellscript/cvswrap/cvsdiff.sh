b7ba08ba521e0c81e856d483a5d51ea8491e60b3
joey <joey>
2002-05-09 05:43:06 +0000

    *** empty log message ***

--- code/shellscript/cvswrap/cvsdiff.sh
+++ code/shellscript/cvswrap/cvsdiff.sh
@@ -1,10 +1,37 @@
+# This should not be called cvsdiff, because it ain't like cvs diff:
+# it only finds what's missing, not what's been changed.
+
+echo '# Reasons for failing update:'
+echo '# cvs update 2>/dev/null | grep -v "^\? "'
+echo '# Files which are not the same as the repository versions.'
+echo '# or cvs status 2>/dev/null | grep "^File: " | grep -v "Status: Up-to-date"'
+
+SHABLE=
+if test "$1" = "-sh"; then
+	SHABLE=true
+	shift
+fi
+
+cvs status 2>&1 | grep -v "^cvs status: Examining " | grep "^\? " |
+	while read Q X; do
+		printf "cvs add "
+		if test $SHABLE; then
+			echo "$X"
+		else
+			ls -d "$X"
+		fi
+	done
+
+exit 0
+
+
+
+# Old version
+
 # not sure why #!/bin/zsh
 
 # Searches current cvs directory and looks for directories and files
 # which have not yet been added to the _local_ repository.
-
-echo '# Alternatively, try: cvs update 2>/dev/null | grep -v "^\? "' >&2
-echo '# or cvs status 2>/dev/null | grep "^File: " | grep -v "Status: Up-to-date"' >&2
 
 # To highlight lines from cvs diff -c :
 # cvs diff -r 1.27 simgen.c | sed "s/^\!/"`curseyellow`"\!/;s/$/"`cursegrey`"/"
