5711f2b6cbc956bbd9cf90482a47fd3df1686021
joey <joey>
2002-05-16 04:01:06 +0000

    *** empty log message ***

--- code/shellscript/cvswrap/cvsvimdiff.sh
+++ code/shellscript/cvswrap/cvsvimdiff.sh
@@ -6,18 +6,21 @@ REV="$2"
 
 # if test "$FILENAME" = "" || test "$REV" = ""; then
 if test "$FILENAME" = ""; then
-	echo "cvsvimdiff <filename> <revision>"
+	echo "cvsvimdiff <filename> [ <revision#> ]"
 	echo "  will check out a temporary revision of <filename> and do a vimdiff..."
+	echo "  at present, no <revision#> means compare against the most recent repository version."
 	exit 1
 fi
 
+WHICHREV="Working"
+WHICHREV="Repository"
 if test "$REV" = ""; then
 	REV=`cvs status "$FILENAME" |
-			grep 'Working revision:' |
-			after "Working revision:" |
+			grep "$WHICHREV revision:" |
+			after "$WHICHREV revision:" |
 			sed 's/^\( \|	\)//g' |
 			getnumber`
-	echo "Diffing against current revision $REV"
+	echo "Diffing local against current $WHICHREV revision $REV"
 fi
 
 CKOUT=`jgettmp "$FILENAME-ver-$REV"`
