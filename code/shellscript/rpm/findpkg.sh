97196cb381f234d769424eb4a63bc1fd722c1292
joey <joey>
2002-05-21 04:49:23 +0000

    *** empty log message ***

--- code/shellscript/rpm/findpkg.sh
+++ code/shellscript/rpm/findpkg.sh
@@ -1,16 +1,32 @@
 if test "$1" = ""; then
-  echo "findpkg [-all] <part-of-package-name>"
+  echo "findpkg [ -all | -web ] <part-of-package-name>"
   exit 1
 fi
 
-if test "$1" = "-all"; then
-  SEARCH="$2"
-else
-  SEARCH="$1" # Could | grep -v "no description available"
-fi
+SHOWALL=
+WEBSRCH=
+while test ! "$2" = ""; do
+	case "$1" in
+		-all)
+			shift
+			SHOWALL=true
+			;;
+		-web)
+			shift
+			WEBSRCH=true
+			;;
+		*)
+			echo "$1: invalid argument"
+			shift
+			exit 1
+	esac
+done
+SEARCH="$1"
 
-# browse "http://packages.debian.org/cgi-bin/search_packages.pl?keywords=$SEARCH&version=all&searchon=all&subword=1&release=all" &
-newwin lynx "http://packages.debian.org/cgi-bin/search_packages.pl?keywords=$SEARCH&version=all&searchon=all&subword=1&release=all" &
+if test $WEBSRCH; then
+	# browse "http://packages.debian.org/cgi-bin/search_packages.pl?keywords=$SEARCH&version=all&searchon=all&subword=1&release=all" &
+	newwin lynx "http://packages.debian.org/cgi-bin/search_packages.pl?keywords=$SEARCH&version=all&searchon=all&subword=1&release=all" &
+fi
 
 # use dlocate if it's available
 BIN=`jwhich dlocate`
@@ -22,7 +38,7 @@ fi
 
 # extend columns in order to show full package name and description
 env COLUMNS=184 $BIN -l "$SEARCHEXP" |
-if test "$1" = "-all"; then
+if test $SHOWALL; then
   cat
 else
   grep -v "no description available"
