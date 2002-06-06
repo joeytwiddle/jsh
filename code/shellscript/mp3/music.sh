acb801daa47f293930cd810dd7a48f28ea3b0c43
joey <joey>
2002-06-06 18:49:30 +0000

    *** empty log message ***

--- code/shellscript/mp3/music.sh
+++ code/shellscript/mp3/music.sh
@@ -1,6 +1,1 @@
-# `jwhich xterm` +sb -sl 5000 -vb -si -sk -bg black -fg white -font '-schumacher-clean-medium-r-normal-*-*-80-*-*-c-*-iso646.1991-irv' -geometry 44x21+64+-11 -e "$JPATH/tools/music2"  &
-# `jwhich xterm` +sb -sl 5000 -vb -si -sk -bg black -fg white -font '-schumacher-clean-medium-r-normal-*-*-80-*-*-c-*-iso646.1991-irv' -geometry 44x18+64+-11 -e "$JPATH/tools/music2"  &
-# `jwhich xterm` +sb -sl 5000 -vb -si -sk -bg black -fg white -font '-schumacher-clean-medium-r-normal-*-*-80-*-*-c-*-iso646.1991-irv' -geometry 44x18+723+-10 -e "$JPATH/tools/music2"  &
-# `jwhich xterm` +sb -sl 5000 -vb -si -sk -bg black -fg white -font '-schumacher-clean-medium-r-normal-*-*-80-*-*-c-*-iso646.1991-irv' -geometry 45x21+725+-16 -e "$JPATH/tools/music2"  &
-# `jwhich xterm` +sb -sl 5000 -vb -si -sk -bg black -fg white -font '-schumacher-clean-medium-r-normal-*-*-80-*-*-c-*-iso646.1991-irv' -geometry 33x26+784+37 -e "$JPATH/tools/music2"  &
 `jwhich xterm` -geometry 43x27+989+41 +sb -sl 5000 -vb -si -sk -bg black -fg white -font '-schumacher-clean-medium-r-normal-*-*-80-*-*-c-*-iso646.1991-irv' -e "$JPATH/tools/music2"  &
