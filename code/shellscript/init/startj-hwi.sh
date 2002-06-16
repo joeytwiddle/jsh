5e48a741de6e3e993f4489bb05454985f5b23087
joey <joey>
2002-06-16 14:05:18 +0000

    *** empty log message ***

--- code/shellscript/init/startj-hwi.sh
+++ code/shellscript/init/startj-hwi.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 export JPATH=$HOME/j
 export PATH=$JPATH/tools:$PATH
 
@@ -25,12 +27,36 @@ alias hwicvs='cvs -d :pserver:joey@hwi.dyn.dhs.org:/stuff/cvsroot'
 alias cvsimc='cvs -d :pserver:anonymous@cat.org.au:/usr/local/cvsroot'
 alias cvsenhydra='cvs -d :pserver:anoncvs@enhydra.org:/u/cvs'
 
+export FIGNORE=".class"
+
 # Avoid error if not on a tty
 if test ! "$BAUD" = "0"; then
 	mesg y
 fi
 
-export FIGNORE=".class"
+export WATCH=all
+
+SHORTSHELL=`echo "$SHELL" | afterlast "/"`
+
+echo "shell = $SHORTSHELL"
+
+# xterm title change
+case $TERM in
+	xterm*)
+		case $SHORTSHELL in
+			zsh)
+				preexec () {
+					xttitle "$USER@$HOST:$PWD% $*"
+					# print -Pn "\e]0;$*\a"
+				}
+			;;
+			# Doesn't work 'cos tcsh can't exec my scripts!
+			tcsh)
+				alias postcmd 'xttitle "${USER}@${HOST}:${PWD}% \!#"'
+			;;
+		esac
+	;;
+esac
 
 # source $JPATH/tools/jshellalias
 # source $JPATH/tools/jshellsetup
