7d9c86e0bfa957191e7c51cb80ceced9028ab128
joey <joey>
2002-06-21 23:49:12 +0000

    *** empty log message ***

--- code/shellscript/dirhistory/dirhistory.sh
+++ code/shellscript/dirhistory/dirhistory.sh
@@ -5,9 +5,9 @@ TMPF=`jgettmp`
 awkdrop 1 $HOME/.dirhistory | grep "$1" > $TMPF
 
 (
-tail -4 $TMPF
+seq 4 1 | tr " " "\n" | sed "s/\(.*\)/"`cursegrey`"\b\1 /" | splicewith tail -4 $TMPF
 echo `cursecyan``cursebold`"You are here:"`cursegrey`" $PWD"
-head -4 $TMPF
+seq 1 4 | tr " " "\n" | sed "s/\(.*\)/"`cursegrey`"\f\1 /" | splicewith head -4 $TMPF
 ) |
 if test "$1" = ""; then cat; else highlight "$1"; fi |
 sed "s+/+"`cursegrey`"/"`cursegreen`"+g"
