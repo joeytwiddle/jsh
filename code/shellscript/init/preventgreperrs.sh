147a0d0ebc9773f14f5c1528679694b75f151858
joey <joey>
2002-07-03 04:10:19 +0000

    *** empty log message ***

--- code/shellscript/init/preventgreperrs.sh
+++ code/shellscript/init/preventgreperrs.sh
@@ -1,20 +1,7 @@
 # This work of genius attempts to prevent error messages
 # if user greps eg. * and has not specified how to deal with directories.
 
-# Alternative implementation could redirect stderr, grep (!) it, then pipe it out.
+REALGREP=`jwhich grep`
 
-# Note: I had to take both grep calls out of jwhich because it is used here.
-
-# Test if -r (recursive) option is present
-# (not checking for -d ATM)
-# Proper:
-# RES=`echo "$*" | sed "s/.*\(^\| \)-\(r\|[^-][^ ]*r\).*/GOT/"`
-# Proper hack:
-RES=`echo " $*" | sed "s/.* -\(r\|[^-][^ ]*r\).*/GOT/"`
-# echo ">$RES<"
-if test "$RES" = "GOT"; then
-	`jwhich grep` "$@"
-else
-	# If not, specify skip directories
-	`jwhich grep` -d skip "$@"
-fi
+# $REALGREP "$@" 1>&3 2>&1 | $REALGREP -v "^grep: .*: Is a directory$"
+$REALGREP "$@" 2> /dev/null
