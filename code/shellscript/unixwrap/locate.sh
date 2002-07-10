ff38ed8ac91580971833d179c47808088351b778
joey <joey>
2002-07-10 14:17:14 +0000

    *** empty log message ***

--- code/shellscript/unixwrap/locate.sh
+++ code/shellscript/unixwrap/locate.sh
@@ -1,1 +1,12 @@
-`jwhich locate` "$@" | highlight "$@"
+if test "$1" = ""; then
+	echo "locate [ -nh ] <regex>"
+	exit 1
+fi
+
+if test "$1" = "-nh"; then
+	shift
+	`jwhich locate` "$1"
+else
+	`jwhich locate` "$1" |
+	highlight "$@"
+fi
