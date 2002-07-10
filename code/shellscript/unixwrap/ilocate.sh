3e65f62dd38d336efe5fba57c0d84107bac30318
joey <joey>
2002-07-10 14:19:42 +0000

    *** empty log message ***

--- code/shellscript/unixwrap/ilocate.sh
+++ code/shellscript/unixwrap/ilocate.sh
@@ -1,4 +1,4 @@
 # Seems to need slocate!  Standard locate doesn't work.
-IREG=`echo "$@" | sed --file /home/joey/iregex.sed`
+IREG=`echo "$1" | sed --file /home/joey/iregex.sed`
 echo "> $IREG <"
-locate -r $IREG
+`jwhich locate` -r $IREG
--- code/shellscript/unixwrap/locate.sh
+++ code/shellscript/unixwrap/locate.sh
@@ -1,12 +1,1 @@
-if test "$1" = ""; then
-	echo "locate [ -nh ] <regex>"
-	exit 1
-fi
-
-if test "$1" = "-nh"; then
-	shift
-	`jwhich locate` "$1"
-else
-	`jwhich locate` "$1" |
-	highlight "$@"
-fi
+`jwhich locate` "$@" | highlight "$@"
