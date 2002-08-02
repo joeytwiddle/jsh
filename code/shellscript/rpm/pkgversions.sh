02dd235f4458a8c6e2ada23bff17a9d97943927f
joey <joey>
2002-08-02 22:28:15 +0000

    *** empty log message ***

---
+++ code/shellscript/rpm/pkgversions.sh
@@ -0,0 +1,7 @@
+apt-cache showpkg "$1" |
+drop 2 |
+tostring "" |
+sed 's+^\(.*\)(/var/lib/dpkg/status)\(.*\)$+\1\2 '`cursecyan`'[Installed]+' |
+# Following two equivalent:
+sed 's+/var/lib/apt/lists/++g'
+# sed 's+([^)]*/\([^)]*\))+(\1)+g'
