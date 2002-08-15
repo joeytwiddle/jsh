3af16b6504db567851cc4537a8d9faf0197123dc
joey <joey>
2002-08-15 16:08:35 +0000

    *** empty log message ***

--- code/shellscript/unixwrap/mygrep.sh
+++ code/shellscript/unixwrap/mygrep.sh
@@ -1,12 +1,10 @@
 # This work of genius attempts to prevent error messages
 # if user greps eg. * and has not specified how to deal with directories.
 
-# Hey dufus, this should be a shell alias!
-# Hey dufus, it can't be 'cos it adds to the end of the line!
-# Hey dufus, it can be an alias to mygrep!
-# Hey dufus, I think there is an argument you can provide, so why not fix it already?
+grep -d skip "$@"
+# (grep no longer redirected)
 
-REALGREP=`jwhich grep`
-
-# $REALGREP "$@" 1>&3 2>&1 | $REALGREP -v "^grep: .*: Is a directory$"
-$REALGREP "$@" 2> /dev/null
+# REALGREP=`jwhich grep`
+# 
+# # $REALGREP "$@" 1>&3 2>&1 | $REALGREP -v "^grep: .*: Is a directory$"
+# $REALGREP "$@" 2> /dev/null
