e219217b81cd764103ac3515c63a2d9bdfef1c14
joey <joey>
2002-09-19 13:53:11 +0000

    *** empty log message ***

--- code/shellscript/backup/makebackup.sh
+++ code/shellscript/backup/makebackup.sh
@@ -1,3 +1,6 @@
+## TODO: symlinks causing unwanted loops are not caught by diff :-(
+##       need to tar up files only and then diff extracted tars!
+
 if test "$1" = ""; then
 	echo "makebackup <dir/file> <backup_dir> [ <backup_prefix> ]"
 	exit 1
@@ -5,9 +8,13 @@ fi
 
 TOBACKUP="$1"
 BACKUPDIR="$2"
+DIRNAME=`dirname "$TOBACKUP"`
+BASENAME=`basename "$TOBACKUP"`
 test "$3" &&
 BACKUPNAME="$3" ||
-BACKUPNAME=`basename "$1"`
+BACKUPNAME="$BASENAME"
+
+mkdir -p "$BACKUPDIR" || exit 1
 
 VER=1
 while test -f "$BACKUPDIR/$BACKUPNAME-$VER.diff.gz" || test -f "$BACKUPDIR/$BACKUPNAME-$VER.tar.gz"; do
@@ -23,12 +30,13 @@ if test ! "$VER" = 1; then
 	cd "$EXTRACTDIR"
 	tar xfz "$BACKUPDIR/$BACKUPNAME-$PREVER.tar.gz"
 
-	diff -r "$EXTRACTDIR" "$TOBACKUP" > "$BACKUPDIR/$BACKUPNAME-$VER.diff"
+	diff -r "$EXTRACTDIR/$BASENAME" "$TOBACKUP" > "$BACKUPDIR/$BACKUPNAME-$VER.diff"
 	gzip "$BACKUPDIR/$BACKUPNAME-$VER.diff"
 
 	jdeltmp "$EXTRACTDIR"
 
 fi
 
-tar cfz "$BACKUPDIR/$BACKUPNAME-$VER.tar.gz" "$TOBACKUP"
+cd "$DIRNAME"
+tar cfz "$BACKUPDIR/$BACKUPNAME-$VER.tar.gz" "$BASENAME"
 
