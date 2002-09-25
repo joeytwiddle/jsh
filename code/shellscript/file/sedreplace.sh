7488233618f8e3684a11c1575827064fa3babab7
joey <joey>
2002-09-25 21:31:45 +0000

    Added stream mode.

--- code/shellscript/file/sedreplace.sh
+++ code/shellscript/file/sedreplace.sh
@@ -1,4 +1,4 @@
-if test "$3" = ""; then
+if test "$2" = ""; then
 	echo 'sedreplace <options> "search_string" "replace_string" <filename>...'
 	echo "  where <options> ="
 	echo "    -nobackup : do not create backup in <filename>.b4sr"
@@ -7,44 +7,52 @@ if test "$3" = ""; then
 	exit 1
 fi
 
-DOBACKUP=true
-if test "$1" = "-nobackup"; then
-	shift
-	DOBACKUP=
-fi
-SHOWCHANGES=true
-if test "$1" = "-changes"; then
-	shift
-	SHOWCHANGES=
-fi
-FROM="$1"
-TO="$2"
-shift
-shift
+if test "$3" = ""; then
 
-TMPFILE=`jgettmp sedreplace$$`
+	sed "s$FROM$TOg"
 
-for FILE do
-	if test ! -w "$FILE"; then
-		## Apparently /dev/stderr is not always writeable
-		echo "sedreplace: $FILE not writeable" >&2
-		break
+else
+
+	DOBACKUP=true
+	if test "$1" = "-nobackup"; then
+		shift
+		DOBACKUP=
 	fi
-	cat "$FILE" | sed "s$FROM$TOg" > "$TMPFILE"
-	chmod --reference="$FILE" "$TMPFILE"
-	# Shouldn't I be checking for SHOWCHANGES here?
-	if cmp "$FILE" "$TMPFILE" >&2; then
-		test $SHOWCHANGES && echo "sedreplace: no changes made to $FILE" >&2
-	else
-		if test $DOBACKUP; then
-			mv "$FILE" "$FILE.b4sr" ||
-			if test ! "$?" = 0; then
-				echo "sedreplace: problem moving \"$FILE\" to \"$FILE.b4sr\"" >&2
-				echo "Aborting!"
-				exit 1
-			fi
+	SHOWCHANGES=true
+	if test "$1" = "-changes"; then
+		shift
+		SHOWCHANGES=
+	fi
+	FROM="$1"
+	TO="$2"
+	shift
+	shift
+
+	TMPFILE=`jgettmp sedreplace$$`
+
+	for FILE do
+		if test ! -w "$FILE"; then
+			## Apparently /dev/stderr is not always writeable
+			echo "sedreplace: $FILE not writeable" >&2
+			break
 		fi
-		mv "$TMPFILE" "$FILE" ||
-			echo "sedreplace: problem moving \"$TMPFILE\" over \"$FILE\"" >&2
-	fi
-done
+		cat "$FILE" | sed "s$FROM$TOg" > "$TMPFILE"
+		chmod --reference="$FILE" "$TMPFILE"
+		# Shouldn't I be checking for SHOWCHANGES here?
+		if cmp "$FILE" "$TMPFILE" >&2; then
+			test $SHOWCHANGES && echo "sedreplace: no changes made to $FILE" >&2
+		else
+			if test $DOBACKUP; then
+				mv "$FILE" "$FILE.b4sr" ||
+				if test ! "$?" = 0; then
+					echo "sedreplace: problem moving \"$FILE\" to \"$FILE.b4sr\"" >&2
+					echo "Aborting!"
+					exit 1
+				fi
+			fi
+			mv "$TMPFILE" "$FILE" ||
+				echo "sedreplace: problem moving \"$TMPFILE\" over \"$FILE\"" >&2
+		fi
+	done
+
+fi
