f6c9b4abd808e3234bfb089356ee710f8857bf39
joey <joey>
2002-11-06 20:23:07 +0000

    *** empty log message ***

--- code/shellscript/unixwrap/whitewin.sh
+++ code/shellscript/unixwrap/whitewin.sh
@@ -1,3 +1,5 @@
-# Unlike newwin
+## Unlike newwin
 # This allows option passing as well as execution with -e <com>
-xterm -fg black -bg white "$@" &
+# xterm -fg black -bg white "$@" &
+## Now a fancy off-white win!
+xterm -fg "#003000" -bg "#ffffeb" "$@" &
