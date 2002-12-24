a1ab0632a800342d5c24230f4b6b6a910b5ac3f2
joey <joey>
2002-12-24 04:34:13 +0000

    *** empty log message ***

---
+++ code/shellscript/cygwin/cygpkgsizes.sh
@@ -0,0 +1,21 @@
+cd /
+
+'ls' /etc/setup/*.lst.gz | sed 's+^/etc/setup/++;s+\.lst\.gz$++' |
+
+# cygcheck -c | takecols 1 | drop 2 | # chop 2 |
+
+while read PKG
+do
+	printf "$PKG	"
+	gunzip -c /etc/setup/$PKG.lst.gz 2> /dev/null |
+	grep -v '/$' |
+	# while read FILE
+	# do
+		# if test -f "$FILE" 2> /dev/null; then
+			# echo "$FILE"
+		# fi
+	# done |
+	xargs du -S |
+	takecols 1 |
+	awksum
+done
