e43ca71694e9a447beb3c2f3c0ffa603b1cb6c86
joey <joey>
2003-01-06 11:31:39 +0000

    *** empty log message ***

--- code/shellscript/unixwrap/xtermopts.sh
+++ code/shellscript/unixwrap/xtermopts.sh
@@ -1,9 +1,13 @@
-ARGS=""
+ARGS="-cc 33:48,37:48,45-47:48,64:48"
 if test "$JM_UNAME" = "linux" && ! startswith `hostname` "qanir"; then
 	FONT='-b&h-lucidatypewriter-medium-r-normal-*-*-80-*-*-m-*-iso8859-1'
-	ARGS="-rightbar"
+	ARGS="$ARGS -rightbar"
+elif test "$JM_UNAME" = "cygwin"; then
+	FONT='-b&h-lucidatypewriter-medium-r-normal-*-*-12-*-*-m-*-iso8859-1'
+	# cygwin doesn't like -cc
+	ARGS=""
 else
 	FONT='-b&h-lucidatypewriter-medium-r-normal-*-*-100-*-*-m-*-iso8859-1'
 fi
 
-echo $ARGS +sb -sl 5000 -vb -si -sk -bg black -fg white -font "$FONT" -cc '33:48,37:48,45-47:48,64:48' "$@"
+echo $ARGS +sb -sl 5000 -vb -si -sk -bg black -fg white -font "$FONT" "$@"
