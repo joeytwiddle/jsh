e4940ab8b260d381f58ae6bc9a37f22ed7bd5b57
joey <joey>
2003-02-01 03:25:28 +0000

    *** empty log message ***

--- code/shellscript/posessive/revsshserver.sh
+++ code/shellscript/posessive/revsshserver.sh
@@ -6,7 +6,10 @@ URL="/cgi-bin/joey/revssh"
 SESSID=`hostname`"_$$"
 
 COM="wget -O -"
+## Note the next probably should be lynx -source (since -dump un-HTMLs)
 # COM="lynx -dump"
+## Actually we can't put the telnet in $COM because it'd need to be more
+## like: mywget() { echo "GET $URLPATH" | telnet "$URLHOST" }
 # COM="telnet ..."
 
 $COM "http://$SERVER$URL?sessid=$SESSID&init=true" |
