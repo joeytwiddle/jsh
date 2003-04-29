3d68e45bd77d6314d248763e28f61dab417c0db5
joey <joey>
2003-04-29 17:03:12 +0000

    now bgs itself in a new window by default

--- code/shellscript/sys/requestsudo.sh
+++ code/shellscript/sys/requestsudo.sh
@@ -1,14 +1,19 @@
+INFG=""
+if test "$1" = -fg
+then INFG=true; shift
+fi
+
 (
   echo 'echo "Enter root passwd to perform:"'
   echo 'echo "'"$@"'"'
   echo 'su root -c $JPATH/tmp/sudo2.tmp'
   echo 'sleep 2'
 ) > $JPATH/tmp/sudo.tmp &&
+
 echo "$@" > $JPATH/tmp/sudo2.tmp &&
 chmod u+x $JPATH/tmp/sudo.tmp $JPATH/tmp/sudo2.tmp &&
-if xisrunning; then
-	newwin $JPATH/tmp/sudo.tmp
-else
-	$JPATH/tmp/sudo.tmp
+if xisrunning && test ! "$INFG"
+then newwin $JPATH/tmp/sudo.tmp
+else $JPATH/tmp/sudo.tmp
 fi
-# 'rm' $JPATH/tmp/sudo.tmp
+'rm' $JPATH/tmp/sudo.tmp
