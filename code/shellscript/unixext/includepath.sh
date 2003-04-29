5d6ca9c02060c18b5fbc804e73089e5ecb8f32f7
joey <joey>
2003-04-29 17:04:28 +0000

    now can include *after* existing paths

--- code/shellscript/unixext/includepath.sh
+++ code/shellscript/unixext/includepath.sh
@@ -1,10 +1,17 @@
 # Usage: . includepath <root-prefix>
 
-NEWROOT="$1"
-
-PATH="$NEWROOT/bin:$PATH"
-MANPATH="$NEWROOT/man:$MANPATH"
-LD_LIBRARY_PATH="$NEWROOT/lib:$LD_LIBRARY_PATH"
+if test "$1" = -after
+then
+	NEWROOT="$2"
+	PATH="$PATH:$NEWROOT/bin"
+	MANPATH="$MANPATH:$NEWROOT/man"
+	LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$NEWROOT/lib"
+else
+	NEWROOT="$1"
+	PATH="$NEWROOT/bin:$PATH"
+	MANPATH="$NEWROOT/man:$MANPATH"
+	LD_LIBRARY_PATH="$NEWROOT/lib:$LD_LIBRARY_PATH"
+fi
 
 export PATH;
 export MANPATH;
