8842a1cf6bc3452305a8d635eadcf1f6145a0d39
joey <joey>
2003-04-29 16:39:08 +0000

    now drops JPATH but doesn't exit if problems =)

--- code/shellscript/init/startj-hwi.sh
+++ code/shellscript/init/startj-hwi.sh
@@ -11,6 +11,7 @@
 ## $_ comes out as previous command (the one called before source!)
 # echo "\$\_ = >$_<"
 # echo "\$\0 = >$0<"
+# echo "\$\* = >$*<"
 # env > /tmp/env.out
 # set > /tmp/set.out
 # chmod a+w /tmp/env.out
@@ -30,105 +31,129 @@ test -f "$BASH_BASH" &&
 ## But anything we do (eg. export NO_MORE_JSHS=true) would prevent the user
 ## from recursively running jsh.  (Not that I do that _that_ much!)
 
+lookslikejpath () {
+	test -f "$1/startj"
+}
+
+OKTOSTART=true
+
 ## Try to guess the top directory of j install
 ## If all below fails, then you should set it youself with export JPATH=...; source $JPATH/startj
-## TODO: Must ensure JPATH is full path (jsh may have been called locally)
+## TODO: Ensure JPATH is an absolute path (jsh may have been called relative to wd)
 if test ! $JPATH; then
-	if test -d "$HOME/j"; then
-		export JPATH=$HOME/j
+	if lookslikejpath "$HOME/j"
+	then export JPATH="$HOME/j"
 	## This doesn't work: bash cannot see it unless we call startj direct (no source)
-	elif test -d `dirname "$_"`; then
-		export JPATH=`dirname "$_"`
-		echo "startj: guessed JPATH=$JPATH"
+	elif lookslikejpath `dirname "$_"`
+	then export JPATH=`dirname "$_"`
+	## When I tried on Hwi with bash=2.05a-11, it needed $0
+	elif lookslikejpath `dirname "$0"`
+	then export JPATH=`dirname "$0"`
 	else
 		echo "startj: Could not find JPATH. Not starting."
-		exit 1
+		OKTOSTART=
 	fi
+	test "$OKTOSTART" && echo "startj: found JPATH=$JPATH"
 fi
-export PATH=$HOME/bin:$PATH ## Now doing this before jpath now cos i consider latter more important
-export PATH=$JPATH/tools:$PATH
 
-if jwhich jwhich
+if test $OKTOSTART
 then
-	echo "Warning: it looks like you have a different jsh in your path, which can be very dangerous (script recursion)."
-	echo "$PATH"
-	echo "Aborting"
-	exit 1
-fi
 
-test -f "$JPATH/global.conf" && . "$JPATH/global.conf"
-test -f "$JPATH/local.conf" && . "$JPATH/local.conf"
+	PATHBEFORE="$PATH"
+	export PATH="$JPATH/tools:$HOME/bin:$PATH"
 
-## Setup user bin, libs, man etc...
-# export PATH=$HOME/bin:$PATH
-## not yet finished, should be option - refer to setuppath in pclark/pubbin
-
-# zsh on Solaris gives errors on . so I use source
-
-. javainit
-. hugsinit
-
-### NB: On Hwi with /bin/sh ". startj simple" does not provide "simple" in $1 !
-
-if test ! "$1" = "simple"; then
-
-	. getmachineinfo
-
-	. joeysaliases
-	. cvsinit
-
-	# . dirhistorysetup.bash
-	. dirhistorysetup.zsh
-	. hwipromptforbash
-	. hwipromptforzsh
-	. lscolsinit
-
-	alias cvshwi='cvs -z6 -d :pserver:joey@hwi.ath.cx:/stuff/cvsroot'
-	alias cvsimc='cvs -d :pserver:anonymous@cat.org.au:/usr/local/cvsroot'
-	alias cvsenhydra='cvs -d :pserver:anoncvs@enhydra.org:/u/cvs'
-
-	export FIGNORE=".class"
-
-	## Avoid error if not on a tty
-	## Nice try Joey but doesn't work on kimo.
-	# if test ! "$BAUD" = "0"; then
-		mesg y
-	# fi
-
-	## Message on user login/out (zsh, tcsh, ...?)
-	export WATCH=all
-
-	## What shell are we running?
-	## This says SHELL=bash on tao when zsh is run.  zsh only shows in ZSH_NAME !
-	## $0 does OK for bash (at least when in .bash_profile!)
-	# changed for cygwin, hope solaris and linux r still happy!
-	SHELLPS="$$"
-	SHORTSHELL=`
-		# findjob "$SHELLPS" | # (not on cygwin!)
-		ps | grep "$SHELLPS" |
-		grep 'sh$' |
-		tail -1 |
-		sed "s/.* \([^ ]*sh\)$/\1/" |
-		sed "s/^-//"
-	`
-	# echo "shell = $SHORTSHELL"
-	## tcsh makes itself known by ${shell} envvar.
-	## This says SHELL=bash on tao when zsh is run.  zsh only shows in ZSH_NAME !
-	# dunno how we got away without this (needed for cygwin anyway):
-	SHORTSHELL=`echo "$SHORTSHELL" | afterlast "/"`
-
-	## METHOD 2:
-
-	## Which flavour shell are we running?
-	if test $ZSH_NAME; then
-		SHORTSHELL="zsh"
-		. zshkeys
-	elif test "$BASH"; then
-		SHORTSHELL="bash"
-		. bashkeys
-		shopt -s cdspell checkhash checkwinsize cmdhist dotglob histappend histreedit histverify hostcomplete mailwarn no_empty_cmd_completion shift_verbose
+	if jwhich jwhich
+	then
+		echo "Warning: it looks like you have a different jsh in your path, which can be very dangerous (script recursion)."
+		echo "$PATH"
+		echo "Aborting"
+		export PATH="$PATHBEFORE"
+		OKTOSTART=
 	fi
 
-	. xttitleprompt
-
 fi
+
+if test $OKTOSTART
+then
+
+	test -f "$JPATH/global.conf" && . "$JPATH/global.conf"
+	test -f "$JPATH/local.conf" && . "$JPATH/local.conf"
+
+	## Setup user bin, libs, man etc...
+	# export PATH=$HOME/bin:$PATH
+	## not yet finished, should be option - refer to setuppath in pclark/pubbin
+
+	# zsh on Solaris gives errors on . so I use source
+
+	. javainit
+	. hugsinit
+
+	### NB: On Hwi with /bin/sh ". startj simple" does not provide "simple" in $1 !
+
+	if test ! "$1" = "simple"; then
+
+		## TODO: Separate scripts which need to run to init stuff for runtime
+		##       from scripts which do stuff that isn't dependent for later.
+
+		. getmachineinfo
+
+		. joeysaliases
+		. cvsinit
+
+		# . dirhistorysetup.bash
+		. dirhistorysetup.zsh
+		. hwipromptforbash
+		. hwipromptforzsh
+		. lscolsinit
+
+		alias cvshwi='cvs -z6 -d :pserver:joey@hwi.ath.cx:/stuff/cvsroot'
+		alias cvsimc='cvs -d :pserver:anonymous@cat.org.au:/usr/local/cvsroot'
+		alias cvsenhydra='cvs -d :pserver:anoncvs@enhydra.org:/u/cvs'
+
+		export FIGNORE=".class"
+
+		## Avoid error if not on a tty
+		## Nice try Joey but doesn't work on kimo.
+		# if test ! "$BAUD" = "0"; then
+			mesg y
+		# fi
+
+		## Message on user login/out (zsh, tcsh, ...?)
+		export WATCH=all
+
+		## What shell are we running?
+		## This says SHELL=bash on tao when zsh is run.  zsh only shows in ZSH_NAME !
+		## $0 does OK for bash (at least when in .bash_profile!)
+		# changed for cygwin, hope solaris and linux r still happy!
+		SHELLPS="$$"
+		SHORTSHELL=`
+			# findjob "$SHELLPS" | # (not on cygwin!)
+			ps | grep "$SHELLPS" |
+			grep 'sh$' |
+			tail -1 |
+			sed "s/.* \([^ ]*sh\)$/\1/" |
+			sed "s/^-//"
+		`
+		# echo "shell = $SHORTSHELL"
+		## tcsh makes itself known by ${shell} envvar.
+		## This says SHELL=bash on tao when zsh is run.  zsh only shows in ZSH_NAME !
+		# dunno how we got away without this (needed for cygwin anyway):
+		SHORTSHELL=`echo "$SHORTSHELL" | afterlast "/"`
+
+		## METHOD 2:
+
+		## Which flavour shell are we running?
+		if test $ZSH_NAME; then
+			SHORTSHELL="zsh"
+			. zshkeys
+		elif test "$BASH"; then
+			SHORTSHELL="bash"
+			. bashkeys
+			shopt -s cdspell checkhash checkwinsize cmdhist dotglob histappend histreedit histverify hostcomplete mailwarn no_empty_cmd_completion shift_verbose
+		fi
+
+		. xttitleprompt
+
+	fi # ! simple
+
+fi # OKTOSTART
