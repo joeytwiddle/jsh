3e7a8ae1d0d4472428ab30b535f431becff30f45
joey <joey>
2003-04-29 17:10:40 +0000

    half-commented, repeat \ escaping with sed's 'g', words now include 0-9 and some extra chars are broken off

--- code/shellscript/stream/escapenewlines.sh
+++ code/shellscript/stream/escapenewlines.sh
@@ -1,5 +1,10 @@
 # unj escapenewlines "$@"
 
+# echo "  '\\'s and '\\n's are slash-escaped."
+# echo "  If -x is specified, then after escaping words and some special characters are"
+# echo "  expanded onto separate lines."
+# echo "  One example use of this is to perform diffing by words rather than by lines."
+
 EXPAND_WORDS=
 if test "$1" = -x
 then shift; EXPAND_WORDS=true
@@ -9,15 +14,16 @@ NL="
 "
 
 # unj escapenewlines "$@" |   ## C version
-sed 's+\\+\\\\+;s+$+\\n+' "$@" | tr -d '\n' |
+sed 's+\\+\\\\+g;s+$+\\n+' "$@" | tr -d '\n' |
 
 if test "$EXPAND_WORDS"
 then
 	## NOTE: we will interpret some 'n's wrong, namely this one: "...\\n..."
 	#   whole-word whitespace special-chars escaped-newline  ## Sufficient for my HTML purposes
-	sed "s=\([a-zA-Z]+\|[ 	]+\|[;\"'<>]\|\\\\n\)=\\$NL\1\\$NL=g" |
+	sed "s=\([a-zA-Z]+\|\[0-9\.]+\|[ 	]+\|[\=\./;\"'<>]\|\\\\n\)=\\$NL\1\\$NL=g" |
 	tr -s '\n'
 	## Others (testing):
+	# sed "s=\([a-zA-Z]+\|[ 	]+\|[;\"'<>]\|\\\\n\)=\\$NL\1\\$NL=g" |
 	# sed "s=\([a-zA-Z]+\|[ 	]+\|[;\"'<>]\|\\\\n\|[^a-zA-Z 	;\"'<>\\]*\)=\\$NL\1\\$NL=g" |
 	# sed "s+\( \|	\|\\\\n\|[a-zA-Z]*\)+\\$NL\1\\$NL+g"
 	# sed 's+\( \|	\|\\\\n\)+'"\\$NL\1\\$NL+g"
