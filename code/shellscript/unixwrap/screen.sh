710baafd0a7cb3456accc1f0cc74edc0ee9b2374
joey <joey>
2003-05-01 04:48:11 +0000

    Commit of $FILE from $CVSUSER | $USER | $CVS_USER

--- code/shellscript/unixwrap/screen.sh
+++ code/shellscript/unixwrap/screen.sh
@@ -1,2 +1,15 @@
 export SCREEN_RUNNING=true
-unj screen "$@"
+
+if test "$*"
+then
+
+	unj screen "$@"
+
+else
+
+	unj screen -list
+	echo "Special char is Ctrl+k, help is Ctrl+k then ?"
+	sleep 2
+	unj screen "-e^kk" -a -D -RR "$@"
+
+fi
