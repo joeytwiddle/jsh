062a1ea89451b0bfcd6f1ee366e3ef3aa1a9a843
joey <joey>
2003-05-02 15:07:07 +0000

    Prototyping new method; currently buggy but reverts to old.

--- code/shellscript/sh/findjshdeps.sh
+++ code/shellscript/sh/findjshdeps.sh
@@ -1,3 +1,38 @@
+if test ! trynew ### PROBLEM: grep requests LOADS of memory to check this long regexp
+then
+
+	LIST=`
+		## Disabled to make it less dangerous:
+		find /bin /usr/bin /sbin -maxdepth 1 -type f | afterlast /
+		find $JPATH/tools -maxdepth 1 -type l | afterlast /
+	`
+
+	REGEXP=`
+		printf '\\<\\('
+		echo "$LIST" |
+		sort | ## doesn\'t help
+		sed 's+$+\\\|+' |
+		tr -d '\n' |
+		sed 's+\\\|$++'
+		printf '\\)\\>'
+	`
+
+	echo "$REGEXP"
+	find $JPATH/code/shellscript -type f -not -path "*/CVS/*" |
+
+	while read SCRIPT
+	do
+		echo "Trying $SCRIPT"
+		grep "$REGEXP" "$SCRIPT" &&
+		echo "  are dependencies in $SCRIPT" && echo
+	done
+
+	exit
+
+fi
+
+
+
 TMPTREE="$JPATH/tmp/jshdeps"
 
 rm -rf "$TMPTREE"
