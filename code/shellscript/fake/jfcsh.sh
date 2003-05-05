64bd9d5f5fe0bea64b54cbe85ca6b5163061b457
joey <joey>
2003-05-05 17:31:26 +0000

    for efficiency, may be told that input is already sorted =)

--- code/shellscript/fake/jfcsh.sh
+++ code/shellscript/fake/jfcsh.sh
@@ -4,21 +4,29 @@
 
 case "$1" in
 	""|-h|--help)
-		echo "jfcsh [ -bothways | -common ] <file_A> <file_B>"
+		echo "jfcsh [ -bothways | -common ] [ -sorted ] <file_A> <file_B>"
 		echo "  will show lines in file_A which are not in file_B."
-		echo "  NOTE: syntax is likely to change as jfcsh becomes a standin for binary jfc."
-		echo "        Main difference in change is the verbose output of jfc."
+		echo "    -bothways: also shows lines in file_B not in file_A, verbosely for user"
+		echo "    -common: instead, show lines which are in both file_A and file_B"
+		echo "    -sorted: for efficiency, jfcsh may assume files are already sorted"
+		echo "  NOTE: Syntax may change as jfcsh becomes a standin for binary jfc."
+		echo "        But preferably a bridge-wrapper will be used, to retain syntax."
+		echo "        The main difference is the verbose output of jfc."
+		echo "        Is it ever useful for jfc to stand in for jfcsh (now it uses diff is it always fast)?"
 		exit 1
 	;;
 esac
 
 BOTHWAYS=
 COMMON=
+SORT=sort
 while true; do
 	if test "$1" = "-bothways"; then
 		BOTHWAYS=true
 	elif test "$1" = "-common"; then
 		COMMON=true
+	elif test "$1" = "-sorted"; then
+		SORT=cat
 	else
 		break
 	fi
@@ -36,14 +44,15 @@ else
 
 	## I used to do this very inefficiently by grepping $2 once for each line in $1!
 	## Now instead I first sort the files, then GNU diff them, and extract from that.
-	## Note: This gives a slightly different output.  Now all extra lines are shown,
-	## including duplicates, wheras before only unique lines were shown.
+	## Note: This gives a slightly different output.  Now all "unpaired" lines are shown,
+	## including unpaired duplicates of already matched lines, wheras before
+	## only unique lines were shown.
 
 	A=`jgettmp "$1"`
 	B=`jgettmp "$2"`
 
-	cat "$1" | sort > "$A"
-	cat "$2" | sort > "$B"
+	$SORT "$1" > $A
+	$SORT "$2" > $B
 
 	test $BOTHWAYS && (
 		cursecyan
@@ -52,7 +61,7 @@ else
 		echo
 	)
 
-	diff "$A" "$B" |
+	diff $A $B |
 		grep "^< " | sed "s/^< //"
 
 	test $BOTHWAYS && (
@@ -62,11 +71,11 @@ else
 		cursenorm
 		echo
 
-		diff "$B" "$A" |
+		diff $B $A |
 			grep "^< " | sed "s/^< //"
 
-		## Or:
-		# diff "$A" "$B" |
+		## Or equivalently:
+		# diff $A $B |
 			# grep "^> " | sed "s/^> //"
 
 		cursecyan
