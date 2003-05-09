ed2569e253ba1bee7f99d5034a70c5bf78d9548c
joey <joey>
2003-05-09 17:45:02 +0000

    *** empty log message ***

--- code/shellscript/jshstub/installjshstub.sh
+++ code/shellscript/jshstub/installjshstub.sh
@@ -7,15 +7,30 @@ mkdir tools
 cd tools
 wget "http://hwi.ath.cx/jshstubtools/jshstub" -O jshstub
 chmod a+x jshstub
-'ls' /home/joey/j/tools/ | while read X; do ln -s jshstub "$X"; done
+
+## Link all the jshtools to jshstub
+'ls' /home/joey/j/tools/ |
+while read X
+do ln -s jshstub "$X"
+done 2>&1 |
+grep -v "jshstub.*File exists"
+
 cd ..
 
 # ln -s tools/jsh .
-# ln -s tools/startj-hwi ./startj
+ln -s tools/startj-hwi ./startj ## Needed at least for jsh to start!
 
-echo "Recommend:"
-# echo "zsh"
-# echo "export JPATH=$JPATH"
+echo "@ Stub jsh installed in $JPATH"
+
+echo "@ I have already done:"
+echo "export JPATH=$JPATH"
+
+echo "@ Type the following to start:"
 echo "source \$JPATH/tools/startj-hwi"
+zsh || echo "Sorry jshstub only works with zsh (not bash)."
 
-zsh
+# $JPATH/tools/joeybashsource /
+# echo "@ Type the following to start:"
+# echo "alias source='. $JPATH/tools/joeybashsource'"
+# echo "source \$JPATH/tools/startj-hwi"
+# bash
