60feb9cd070a97b2856f7e952886a299b25c8259
joey <joey>
2003-05-17 01:17:19 +0000

    Got it working on binaries too!

--- code/shellscript/backup/contractsymlinks.sh
+++ code/shellscript/backup/contractsymlinks.sh
@@ -2,8 +2,10 @@ find . -type l |
 while read X
 do
 	'ls' -ld "$X"
+	# echo "$X" >&2
 	rm "$X"
 done |
 sed 's| -> |	->	|' |
 sed 's|.* \([^ 	]*	->	.*\)|\1|' |
+sort |
 cat >> .symlinks.list
--- code/shellscript/backup/makebackup.sh
+++ code/shellscript/backup/makebackup.sh
@@ -5,12 +5,16 @@
 ##       No, rather than strip them, we should turn them into a file which can be diffed, and from which they can be recreated.
 ##       Would a plain tar do this?
 
+## Now has external dependency: contractsymlinks
+
 ## Turns out I needed diff -r -u before patch -p0 would work.
 ## But it still doesn't deal with binary files well, and missing files are just skipped!
-##                                                       ok dealt with using -N
+##                                                       ok that should be fixed by using -N, but it ain't
 ##                                                       alternatively -P is forward-only
 
-## Now has external dependency: contractsymlinks
+## Actually it seems to work but not for binary files.  Trying -a.
+
+## Wahoo got it!!
 
 # Paranoid; sensible.
 set -e
@@ -38,29 +42,30 @@ do VER=`expr $VER + 1`
 done
 
 OURTMPDIR=`jgettmpdir makebak`
-CREATEDIR="$OURTMPDIR/ver$VER"
-mkdir -p "$CREATEDIR"
-if test "$CREATEDIR" = ""; then echo "Problem with CREATEDIR = >$CREATEDIR<"; exit 1; fi
-echo "Copying $TOBACKUP to $CREATEDIR"
-cd "$DIRNAME"
-cp -a "$BASENAME" "$CREATEDIR"
-
-# Fix symlink problems by removing them!
-# but list them to a file so their changes may be seen.
-echo "Moving symlinks in $CREATEDIR into $BASENAME/.symlinks.list"
-cd "$CREATEDIR" &&
-if test "`pwd`" = "$CREATEDIR"
-then
-	if test -d "$BASENAME"
-	then
-		cd "$BASENAME"
-		contractsymlinks
-	fi
-else echo "Problem: `pwd` != $CREATEDIR"
-fi
 
 if test ! "$VER" = 0
 then
+
+	CREATEDIR="$OURTMPDIR/ver$VER"
+	echo "Copying $TOBACKUP to $CREATEDIR"
+	mkdir -p "$CREATEDIR"
+	if test "$CREATEDIR" = ""; then echo "Problem with CREATEDIR = >$CREATEDIR<"; exit 1; fi
+	cd "$DIRNAME"
+	cp -a "$BASENAME" "$CREATEDIR"
+
+	# Fix symlink problems by removing them!
+	# but list them to a file so their changes may be seen.
+	echo "Moving symlinks in $CREATEDIR into $BASENAME/.symlinks.list"
+	cd "$CREATEDIR" &&
+	if test "`pwd`" = "$CREATEDIR"
+	then
+		if test -d "$BASENAME"
+		then
+			cd "$BASENAME"
+			contractsymlinks
+		fi
+	else echo "Problem: `pwd` != $CREATEDIR"
+	fi
 
 	## We can no longer be paranoid, because expr and diff will often return 0!
 	set +e
@@ -73,11 +78,12 @@ then
 	cd "$EXTRACTDIR"
 	tar xfz "$BACKUPDIR/$BACKUPNAME-$PREVER.tar.gz"
 	cd "$EXTRACTDIR/$BASENAME"
+	echo "Contracting symlinks"
 	contractsymlinks
 
 	echo "Comparing against previous copy, storing diff in $BACKUPDIR/$BACKUPNAME-$VER.diff"
 	cd "$EXTRACTDIR"
-	diff -r -u -N "$BASENAME" "../ver$VER/$BASENAME" > "$BACKUPDIR/$BACKUPNAME-$VER.diff"
+	diff -r -u -N -a "$BASENAME" "../ver$VER/$BASENAME" > "$BACKUPDIR/$BACKUPNAME-$VER.diff"
 	gzip "$BACKUPDIR/$BACKUPNAME-$VER.diff"
 
 	# rm "$BACKUPDIR/$BACKUPNAME-$PREVER.tar.gz"
@@ -87,7 +93,6 @@ fi
 echo "Backing up $TOBACKUP into $BACKUPDIR/$BACKUPNAME-$VER.tar.gz"
 cd "$DIRNAME"
 ## Or to make a backup with the symlinks:
-# cd "$CREATEDIR"
 tar cfz "$BACKUPDIR/$BACKUPNAME-$VER.tar.gz" "$BASENAME"
 
 cd /tmp # anywhere should do
