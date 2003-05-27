4eb272f6286da13a5570e33d8c85fc2fdfc5783d
joey <joey>
2003-05-27 12:07:07 +0000

    *** empty log message ***

---
+++ code/shellscript/zip/findfileinzips.sh
@@ -0,0 +1,25 @@
+FILEPAT="$1"
+shift
+for ZIPFILE
+do
+	if endswith "$ZIPFILE" .zip .ZIP
+	then VIEWCOM="unzip -v"
+	elif endswith "$ZIPFILE" .jar .JAR
+	then VIEWCOM="jar tf"
+	elif endswith "$ZIPFILE" .tar.gz .TAR.GZ .tgz .TGZ
+	# elif contains "$ZIPFILE" "\(.tar.gz\|.TAR.GZ\|.tgz\|.TGZ\)"
+	then VIEWCOM="tar tfz"
+	elif endswith "$ZIPFILE" .tar.bz2 .TAR.BZ2# .tgj .TGJ
+	then VIEWCOM="tar tfj"
+	elif endswith "$ZIPFILE" .tar .TAR
+	then VIEWCOM="tar tf"
+	elif endswith "$ZIPFILE" .rar .RAR
+	then VIEWCOM="echo"; echo "Don't know how to unzip rar: $ZIPFILE" >&2
+	else VIEWCOM="echo"; echo "Don't know how to unzip: $ZIPFILE" >&2
+	fi
+	# echo ":: $VIEWCOM $ZIPFILE" >&2
+	( $VIEWCOM "$ZIPFILE" || ( echo "Error running: $VIEWCOM $ZIPFILE" >&2 ) ) |
+	# $VIEWCOM "$ZIPFILE" |
+	grep "$FILEPAT" &&
+	echo "  \_ Found in $ZIPFILE" && echo
+done
