913eb0d49424ac9071f4a1622bb4ee97cf84895f
joey <joey>
2003-06-27 11:25:17 +0000

    fixing option

--- code/shellscript/cdrw/cdwritetracks.sh
+++ code/shellscript/cdrw/cdwritetracks.sh
@@ -1,6 +1,12 @@
+if test "$1" = -fix
+then FIX=true; shift
+else FIX=
+fi
+
 for I in "$@"
 do
 	nice --20 mpg123 --cdr - "$I" |
 	nice --20 cdrecord -dev=0,0,0 -audio -pad -nofix -
 done
-nice --20 cdrecord -fix
+
+test "$FIX" && nice --20 cdrecord -dev=0,0,0 -fix
