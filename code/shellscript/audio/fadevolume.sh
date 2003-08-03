fc53b8d1b023c5aace22befe066e8c2781e50fd5
joey <joey>
2003-08-03 06:15:49 +0000

    standalone, j+sh release

--- code/shellscript/audio/fadevolume.sh
+++ code/shellscript/audio/fadevolume.sh
@@ -1,24 +1,26 @@
-if test $1; then
-  GAP=$1
-else
-  GAP=60
+## First argument is seconds between each nudge down in volume.
+## Defaults to 60 seconds (slow fade).
+if [ "$1" ]
+then GAP="$1"
+else GAP=60
 fi
 
 DONE=
-while test ! $DONE; do
-  VOL=`aumix -q | grep "vol" | after "vol " | before ","`
-  VOL=$(($VOL-1))
-  echo $VOL
-  aumix -v $VOL
-  if test "$VOL" = "0" || test "$VOL" = "-1"; then
-    DONE=true
-  else
-    sleep $GAP
+while test ! $DONE
+do
+  VOL=`aumix -q | grep "vol" | sed 's+vol ++;s+,.*++'`
+  VOL=`expr "$VOL" - 1`
+  echo "$VOL"
+  aumix -v "$VOL"
+  if [ "$VOL" -lt 1 ]
+  then DONE=true
+  else sleep $GAP
   fi
 done
 
-# killall mpg123; mykill "music"; killall mp3blaster
+## Stop programs playing music:
+# killall mpg123 mp3blaster
 # xmms -t &
+# # echo | mykill music
 # sleep 3
-## Doesn't work with music(2|)
 # aumix -v 50
