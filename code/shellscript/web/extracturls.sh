7cad756c73e2c4bd7450580daa4cbc86775c69a4
joey <joey>
2003-08-16 03:07:52 +0000

    *** empty log message ***

--- code/shellscript/web/extracturls.sh
+++ code/shellscript/web/extracturls.sh
@@ -1,1 +1,10 @@
-extractregex -atom "[Hh][Rr][Ee][Ff]=['\"]{0,1}(http[^'\"> \n]*)"
+## The final '+' (instead of '*') in each expression means we won't match the empty url "xyz://".
+
+## From plain text:
+extractregex -atom "([A-Za-z]+:\/\/[^'\"> \n]+)"
+
+## From HTML (absolute links only):
+# extractregex -atom "[Hh][Rr][Ee][Ff]=['\"]{0,1}([A-Za-z]+:\/\/[^'\"> \n]+)"
+
+## From HTML (absolute and relative links):
+# extractregex -atom "[Hh][Rr][Ee][Ff]=['\"]{0,1}([^'\"> \n]+)"
