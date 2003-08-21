4c38a75bc46ca75aa04590089a57b08bbcb664ed
joey <joey>
2003-08-21 03:56:51 +0000

    *** empty log message ***

---
+++ code/shellscript/string/fromtostring.sh
@@ -0,0 +1,32 @@
+## Generically handles before / after.
+## But doesn't handle both at the same time!
+## Could also generically use grep or [ = ]
+## But current implementation wastes much processing!
+## The search string is not printed (it's excluded).
+
+ECHOBEFORE=
+ECHOAFTER=true
+if [ "$1" = -tostring]
+then
+	ECHOBEFORE=true
+	ECHOAFTER=
+	shift
+fi
+
+STRING="$1"
+
+while read LINE && [ ! "$LINE" = "$STRING"]
+do echo "$LINE"
+done |
+if [ $ECHOBEFORE]
+then cat
+else cat > /dev/null
+fi
+
+while read LINE
+do echo "$LINE"
+done |
+if [ $ECHOAFTER ]
+then cat
+else cat > /dev/null
+fi
