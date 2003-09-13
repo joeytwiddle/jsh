fe9222526a1df54f70e4c5cc5a1a5511f2909c7a
joey <joey>
2003-09-13 23:14:54 +0000

    *** empty log message ***

--- code/shellscript/text/treesh.sh
+++ code/shellscript/text/treesh.sh
@@ -1,11 +1,11 @@
-. importshfn debug
+[ "$DEBUG" ] && . importshfn debug
 
 NL="
 "
 
-"(1(2(3|)|)|)"
-
 commonstring () {
+	## To test how much of "$2" matches with "123", build regexp like:
+	##   "(1(2(3|)|)|)"
 	echo "$FIRSTLINE" |
 	sed "s+.+\0\\$NL+g" |
 	grep -v "^$" |
@@ -30,45 +30,43 @@ read FIRSTLINE
 while read SECONDLINE
 do
 
-	debug
-	debug "commonsofar:"
+	[ "$DEBUG" ] && debug
+	[ "$DEBUG" ] && debug "commonsofar:"
 	# echo "$COMMONSOFAR"
-	debug "first         = $FIRSTLINE"
-	debug "second        = $SECONDLINE"
+	[ "$DEBUG" ] && debug "first         = $FIRSTLINE"
+	[ "$DEBUG" ] && debug "second        = $SECONDLINE"
 	COMMON=`commonstring "$FIRSTLINE" "$SECONDLINE"`
-	debug "common        = $COMMON"
+	[ "$DEBUG" ] && debug "common        = $COMMON"
 
 	NOTABOVE=`startswith "$COMMON" "$CURRENTCOMMON" && echo yes`
 	NOTBELOW=`startswith "$CURRENTCOMMON" "$COMMON" && echo yes`
 	SAME=`[ "$COMMON" = "$CURRENTCOMMON" ] && echo yes`
 
-	debug "notabove      = $NOTABOVE"
-	debug "notbelow      = $NOTBELOW"
-	debug "same          = $SAME"
-
-	APPEND=""
+	[ "$DEBUG" ] && debug "notabove      = $NOTABOVE"
+	[ "$DEBUG" ] && debug "notbelow      = $NOTBELOW"
+	[ "$DEBUG" ] && debug "same          = $SAME"
 
 	if [ ! $SAME ] && [ $NOTABOVE ]
 	then
+		[ "$DEBUG" ] && debug ">>>>"
 		COMMONSOFAR="$COMMONSOFAR$NL$COMMON"
 		CURRENTCOMMON="$COMMON"
-		APPEND=" {"
-		debug ">>>>"
+		echo "+ $CURRENTCOMMON {"
 	fi
+
+	echo ". $FIRSTLINE$APPEND"
 
 	if [ ! $SAME ] && [ $NOTBELOW ]
 	then
 		while ! startswith "$SECONDLINE" "$CURRENTCOMMON"
 		do
+			[ "$DEBUG" ] && debug "<<<<"
+			echo "- $CURRENTCOMMON }"
 			COMMONSOFAR=`echo "$COMMONSOFAR" | chop 1`
 			CURRENTCOMMON=`echo "$COMMONSOFAR" | tail -1`
-			APPEND="$APPEND }"
-			debug "<<<<"
-			debug "newcurrentcommon = $CURRENTCOMMON"
+			[ "$DEBUG" ] && debug "newcurrentcommon = $CURRENTCOMMON"
 		done
 	fi
-
-	echo "$FIRSTLINE$APPEND"
 
 	FIRSTLINE="$SECONDLINE"
 
