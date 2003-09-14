a4e2e0ea0146b177b5736af5fccc75fb3c0f2446
joey <joey>
2003-09-14 02:45:02 +0000

    *** empty log message ***

--- code/shellscript/screen/changescreencolours.sh
+++ code/shellscript/screen/changescreencolours.sh
@@ -8,9 +8,13 @@ chooserandomarg () {
 	done | chooserandomline
 }
 
+# BORDER=`chooserandomarg RW GW BW CK MW YK`
+# WINDOWBAR=`chooserandomarg rw gk bw ck mw yk`
+# WINDOWHIGHLIGHT=`chooserandomarg rW gK bW CK MW YK`
+
 BORDER=`chooserandomarg RW GW BW CK MW YK`
-WINDOWBAR=`chooserandomarg rw gk bw ck mw yk`
-WINDOWHIGHLIGHT=`chooserandomarg rW gK bW CK MW YK`
+WINDOWBAR=bw
+WINDOWHIGHLIGHT=`chooserandomarg rW gK CK MW YK`
 
 # SCREEN_CAPTION="%{$BORDER} [%{BC}%H%{$BORDER}:%{BC}$SCREENNAME%{$BORDER}] %{$WINDOWBAR} %-w%{kw} %{$WINDOWHIGHLIGHT} [%n] %t %{kw} %{$WINDOWBAR}%+w %=%{$BORDER} %M %d %c"
 SCREEN_CAPTION="%{$BORDER} [%H:$SCREENNAME] %{$WINDOWBAR} %-w%{kw} %{$WINDOWHIGHLIGHT} [%n] %t %{kw} %{$WINDOWBAR}%+w %=%{$BORDER} %M %d %c"
