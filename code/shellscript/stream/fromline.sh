f495a5dc9dbe46ab5abe9d7c06ff6a001ddc8328
joey <joey>
2003-09-17 03:19:33 +0000

    *** empty log message ***

--- code/shellscript/stream/fromline.sh
+++ code/shellscript/stream/fromline.sh
@@ -1,11 +1,27 @@
-## TODO: Rename grepfrom
+## Consider: Renaming it grepfrom
 ## Hides all lines until first occurrence of grep pattern (regexp) is met.
-## TODO: -x exclusive option
 
-PAT="$1"
+if [ "$1" = -x ]
+then
+	shift
+	PAT="$1"
+	awk ' BEGIN { X=0 } { if ( X ) { print $0'\n' } } /'"$PAT"'/ { X=1 } '
+else
+	PAT="$1"
+	awk ' BEGIN { X=0 } /'"$PAT"'/ { X=1 } { if ( X ) { print $0'\n' } } '
+fi
 
-while read LINE
-do echo "$LINE" | grep "$PAT" && break
-done
-
-cat
+# OUT="&1" ## Ha! It was saving a file with that name!
+# if [ "$1" = -x ]
+# then OUT="/dev/null"; shift
+# fi
+# 
+# PAT="$1"
+# 
+# ## Drop each line until it matches
+# while read LINE
+# do printf "%s\n" "$LINE" | grep "$PAT" > $OUT && break
+# done
+# 
+# ## Just let the rest of the stream through
+# cat
--- code/shellscript/stream/toline.sh
+++ code/shellscript/stream/toline.sh
@@ -1,13 +1,34 @@
-## TODO: Rename grepto
+## Consider: Renaming it grepto, or maybe awkto now!
+## TODO: awk doesn't handle /s well, need to \/ escape them
 ## Hides all lines after first occurrence of grep pattern (regexp) is met.
-## TODO: -x exclusive option
 
-PAT="$1"
+if [ "$1" = -x ]
+then
+	shift
+	PAT="$1"
+	awk ' /'"$PAT"'/ { exit } { print $0'\n' } '
+else
+	PAT="$1"
+	awk ' { print $0'\n' } /'"$PAT"'/ { exit } '
+fi
 
-while read LINE
-do
-	echo "$LINE" | grep "$PAT" && break
-	echo "$LINE"
-done
-
+## Drop the rest of the stream
 cat > /dev/null
+
+# # OUT="/dev/stdout"
+# OUT="&1" ## Ha! It was saving a file with that name!
+# if [ "$1" = -x ]
+# then OUT="/dev/null"; shift
+# fi
+# 
+# PAT="$1"
+# 
+# ## Print each line until a match is found
+# while read LINE
+# do
+	# printf "%s\n" "$LINE" | grep "$PAT" > $OUT && break
+	# printf "%s\n" "$LINE"
+# done
+# 
+# ## Drop the rest of the stream
+# cat > /dev/null
