d8b8af9ad1893ca41e774251f7f48dfd91fcc793
joey <joey>
2003-09-18 12:46:55 +0000

    *** empty log message ***

--- code/shellscript/screen/changescreencolours.sh
+++ code/shellscript/screen/changescreencolours.sh
@@ -1,51 +1,64 @@
-# BORDER="BW"
-# WINDOWBAR="bw"
-# WINDOWHIGHLIGHT="gk"
-
-chooserandomarg () {
-	for X
-	do echo "$X"
-	done | chooserandomline
-}
-
-# BORDER=`chooserandomarg RW GW BW CK MW YK`
-# WINDOWBAR=bw
-# WINDOWHIGHLIGHT=`chooserandomarg rW gK CK MW YK`
-
-# WINDOWBAR=`chooserandomarg rk gk bk ck mk yk`
-# WINDOWHIGHLIGHT=`chooserandomarg RW GK BW CK MW yW`
-# BORDER=$WINDOWHIGHLIGHT
-
-# WINDOWBAR=`chooserandomarg rk bk mk yk`
-# WINDOWHIGHLIGHT=`chooserandomarg RW BW MW`
-# BORDER=$WINDOWHIGHLIGHT
-
-WINDOWHIGHLIGHT=`chooserandomarg RW GK MW CK yK`
-BORDER=$WINDOWHIGHLIGHT
-WINDOWBAR=bk
-
-SCREENBUILDYEAR=`screen --version | afterlast - | sed 's+\([[:digit:]]*\).*+\1+' | sed 's+^0++'` # 2100 incompliant
-# echo "SCREENBUILDYEAR = >$SCREENBUILDYEAR<"
-if [ "$SCREENBUILDYEAR" -lt 3 ]
+if [ "$STY" ]
 then
-	# WINLIST="%{$WINDOWBAR} %{$WINDOWHIGHLIGHT} %n %t %{kw} %{$WINDOWBAR} %W "
-	WINLIST="%{$WINDOWBAR} %{$WINDOWHIGHLIGHT} %n %{kw} %{$WINDOWBAR} %w "
-	## todo
-	RIGHTJUST=""
-	DATEBIT="%d/%M %c"
-else
-	WINLIST="%{$WINDOWBAR} %-w%{kw} %{$WINDOWHIGHLIGHT} %n %t %{kw} %{$WINDOWBAR}%+w "
-	RIGHTJUST="%="
-	DATEBIT="%d/%M"
-fi
-# SCREEN_CAPTION="%{$BORDER} [%{BC}%H%{$BORDER}:%{BC}$SCREENNAME%{$BORDER}] %{$WINDOWBAR} %-w%{kw} %{$WINDOWHIGHLIGHT} [%n] %t %{kw} %{$WINDOWBAR}%+w %=%{$BORDER} %M %d %c"
-# SCREEN_CAPTION="%{$BORDER} [%H:$SCREENNAME] %{$WINDOWBAR} %-w%{kw} %{$WINDOWHIGHLIGHT} [%n] %t %{kw} %{$WINDOWBAR}%+w %=%{$BORDER} %M %d %c"
-# SCREEN_CAPTION="%{$BORDER} [%H:$SCREENNAME] %{$WINDOWBAR} %-w%{kw} %{$WINDOWHIGHLIGHT} %t %{kw} %{$WINDOWBAR}%+w %=%{$BORDER} %M %d %c"
-# SCREEN_CAPTION="%{$BORDER}%H:$SCREENNAME(%{$WINDOWBAR} %-w%{kw} %{$WINDOWHIGHLIGHT} %t %{kw} %{$WINDOWBAR}%+w %=%{$BORDER})%d/%M"
-# SCREEN_CAPTION="%{$BORDER}$SHORTHOST:$SCREENNAME%{$WINDOWHIGHLIGHT}(%{$WINDOWBAR} %-w%{kw} %{$WINDOWHIGHLIGHT} %t %{kw} %{$WINDOWBAR}%+w $RIGHTJUST%{$WINDOWHIGHLIGHT})%{$BORDER}%d/%M"
-# SCREEN_CAPTION="%{$BORDER} [%H:$SCREENNAME] $WINLIST $RIGHTJUST%{$BORDER} %M %d %c"
-# SCREEN_CAPTION="%{$BORDER}$SHORTHOST:$SCREENNAME%{$WINDOWHIGHLIGHT}($WINLIST $RIGHTJUST%{$WINDOWHIGHLIGHT})%{$BORDER}%d/%M"
-# SCREEN_CAPTION="%{$BORDER} [$SHORTHOST:$SCREENNAME] $WINLIST $RIGHTJUST%{$BORDER} %M %d %c"
-SCREEN_CAPTION="%{$BORDER}$SHORTHOST:$SCREENNAME ($WINLIST$RIGHTJUST%{$BORDER}) $DATEBIT"
 
-screen -X caption always "$SCREEN_CAPTION"
+	# BORDER="BW"
+	# WINDOWBAR="bw"
+	# WINDOWHIGHLIGHT="gk"
+
+	chooserandomarg () {
+		for X
+		do echo "$X"
+		done | chooserandomline
+	}
+
+	# BORDER=`chooserandomarg RW GW BW CK MW YK`
+	# WINDOWBAR=bw
+	# WINDOWHIGHLIGHT=`chooserandomarg rW gK CK MW YK`
+
+	# WINDOWBAR=`chooserandomarg rk gk bk ck mk yk`
+	# WINDOWHIGHLIGHT=`chooserandomarg RW GK BW CK MW yW`
+	# BORDER=$WINDOWHIGHLIGHT
+
+	# WINDOWBAR=`chooserandomarg rk bk mk yk`
+	# WINDOWHIGHLIGHT=`chooserandomarg RW BW MW`
+	# BORDER=$WINDOWHIGHLIGHT
+
+	WINDOWHIGHLIGHT=`chooserandomarg RW GK MW CK yK`
+	BORDER=$WINDOWHIGHLIGHT
+	# WINDOWBAR=bk
+	WINDOWBAR=bw
+
+	SCREENBUILDYEAR=`screen --version | afterlast - | sed 's+\([[:digit:]]*\).*+\1+' | sed 's+^0++'` # 2100 incompliant
+	# echo "SCREENBUILDYEAR = >$SCREENBUILDYEAR<"
+	if [ "$SCREENBUILDYEAR" -lt 3 ]
+	then
+		# WINLIST="%{$WINDOWBAR} %{$WINDOWHIGHLIGHT} %n %t %{kw} %{$WINDOWBAR} %W "
+		WINLIST="%{$WINDOWBAR} %{$WINDOWHIGHLIGHT} %n %{kw} %{$WINDOWBAR} %w "
+		## todo
+		RIGHTJUST=""
+		DATEBIT="%d/%M %c"
+	else
+		## Looks like tabs:
+		# WINLIST="%{$WINDOWBAR} %-w%{kB}\\ %{kw}%n %t %{kB}/%{$WINDOWBAR}%+w "
+		WINLIST="%{$WINDOWBAR} %-w%{kw} %{$WINDOWHIGHLIGHT} %n %t %{kw} %{$WINDOWBAR}%+w "
+		RIGHTJUST="%="
+		DATEBIT="%d/%M"
+	fi
+	# SCREEN_CAPTION="%{$BORDER} [%{BC}%H%{$BORDER}:%{BC}$SCREENNAME%{$BORDER}] %{$WINDOWBAR} %-w%{kw} %{$WINDOWHIGHLIGHT} [%n] %t %{kw} %{$WINDOWBAR}%+w %=%{$BORDER} %M %d %c"
+	# SCREEN_CAPTION="%{$BORDER} [%H:$SCREENNAME] %{$WINDOWBAR} %-w%{kw} %{$WINDOWHIGHLIGHT} [%n] %t %{kw} %{$WINDOWBAR}%+w %=%{$BORDER} %M %d %c"
+	# SCREEN_CAPTION="%{$BORDER} [%H:$SCREENNAME] %{$WINDOWBAR} %-w%{kw} %{$WINDOWHIGHLIGHT} %t %{kw} %{$WINDOWBAR}%+w %=%{$BORDER} %M %d %c"
+	# SCREEN_CAPTION="%{$BORDER}%H:$SCREENNAME(%{$WINDOWBAR} %-w%{kw} %{$WINDOWHIGHLIGHT} %t %{kw} %{$WINDOWBAR}%+w %=%{$BORDER})%d/%M"
+	# SCREEN_CAPTION="%{$BORDER}$SHORTHOST:$SCREENNAME%{$WINDOWHIGHLIGHT}(%{$WINDOWBAR} %-w%{kw} %{$WINDOWHIGHLIGHT} %t %{kw} %{$WINDOWBAR}%+w $RIGHTJUST%{$WINDOWHIGHLIGHT})%{$BORDER}%d/%M"
+	# SCREEN_CAPTION="%{$BORDER} [%H:$SCREENNAME] $WINLIST $RIGHTJUST%{$BORDER} %M %d %c"
+	# SCREEN_CAPTION="%{$BORDER}$SHORTHOST:$SCREENNAME%{$WINDOWHIGHLIGHT}($WINLIST $RIGHTJUST%{$WINDOWHIGHLIGHT})%{$BORDER}%d/%M"
+	# SCREEN_CAPTION="%{$BORDER} [$SHORTHOST:$SCREENNAME] $WINLIST $RIGHTJUST%{$BORDER} %M %d %c"
+	SCREEN_CAPTION="%{$BORDER}$SHORTHOST:$SCREENNAME ($WINLIST$RIGHTJUST%{$BORDER}) $DATEBIT"
+
+	screen -X caption always "$SCREEN_CAPTION"
+
+else
+
+	echo "No STY found."
+	exit 1
+
+fi
