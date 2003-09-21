d9f83debd93aba42d5aaa8976fb24c6352909de1
joey <joey>
2003-09-21 13:33:36 +0000

    Prints too early!

--- code/shellscript/music/randommp3.sh
+++ code/shellscript/music/randommp3.sh
@@ -3,40 +3,49 @@
 
 SEARCH="$1"
 
-TRACK=`cat $JPATH/music/list.m3u | grep -i "$SEARCH" | ungrep INCOMPLETE | chooserandomline`
+while true
+do
 
-## Inform the log
-echo "$TRACK" >> $JPATH/logs/xmms.log
+	TRACK=`cat $JPATH/music/list.m3u | grep -i "$SEARCH" | ungrep INCOMPLETE | chooserandomline`
 
-## Echo output to user
+	## Inform the log
+	echo "$TRACK" >> $JPATH/logs/xmms.log
 
-	# filename "$TRACK"
-	SIZE=`du -sh "$TRACK" | takecols 1`
-	# echo "$SIZE: "`filename "$TRACK"`" ("`dirname "$TRACK"`")"
-	# echo "$SIZE: "`dirname "$TRACK";curseyellow`/`filename "$TRACK";cursenorm`""
-	echo "$SIZE: "`curseyellow;cursebold`filename "$TRACK"`cursenorm`
+	## Echo output to user
 
-	echo "`cursered`del \"$TRACK\"`cursenorm`"
+		# filename "$TRACK"
+		SIZE=`du -sh "$TRACK" | takecols 1`
+		# echo "$SIZE: "`filename "$TRACK"`" ("`dirname "$TRACK"`")"
+		# echo "$SIZE: "`dirname "$TRACK";curseyellow`/`filename "$TRACK";cursenorm`""
+		echo "$SIZE: "`curseyellow;cursebold`filename "$TRACK"`cursenorm`
 
-	MP3INFO=`mp3info "$TRACK"`
-	echo "$MP3INFO" |
-	grep -v "^File: " |
-	sed "s+[[:alpha:]]*:+`cursemagenta`\0`cursenorm`+g" |
-	# sed "s+\(File:[^ ]* \)\(.*\)+`curseblue`\1 `curseblue`\2+"
-	cat
+		echo "`cursered`del \"$TRACK\"`cursenorm`"
 
-## Normalise volume (gain)
+		MP3INFO=`mp3info "$TRACK"`
+		echo "$MP3INFO" |
+		grep -v "^File: " |
+		sed "s+[[:alpha:]]*:+`cursemagenta`\0`cursenorm`+g" |
+		# sed "s+\(File:[^ ]* \)\(.*\)+`curseblue`\1 `curseblue`\2+"
+		cat
 
-	if which mp3gain > /dev/null 2>&1
-	then
-		NEWTRACK="/tmp/randommp3-gainchange.mp3"
-		cp "$TRACK" "$NEWTRACK"
-		mp3gain -r -c "$NEWTRACK"
-		TRACK="$NEWTRACK"
-	fi
+	## Normalise volume (gain)
 
-/usr/bin/time -f "%e seconds ( Time: %E CPU: %P Mem: %Mk )" playmp3andwait "$TRACK"
+		if which mp3gain > /dev/null 2>&1
+		then
+			NEWTRACK="/tmp/randommp3-gainchange.mp3"
+			cp "$TRACK" "$NEWTRACK"
+			mp3gain -r -c "$NEWTRACK"
+			TRACK="$NEWTRACK"
+		fi
 
-echo
-echo "--------------------------------------"
-echo
+	echo "Waiting for last mpg123 to finish."
+	wait
+	echo "Done waiting!"
+
+	/usr/bin/time -f "%e seconds ( Time: %E CPU: %P Mem: %Mk )" playmp3andwait "$TRACK" &
+
+	echo
+	echo "--------------------------------------"
+	echo
+
+done
