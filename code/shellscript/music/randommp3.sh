c0c7a452d4bf2ac71694c909629c4e277f5222a4
joey <joey>
2003-09-21 13:33:18 +0000

    Employs mp3gain, but pauses.

--- code/shellscript/music/randommp3.sh
+++ code/shellscript/music/randommp3.sh
@@ -1,20 +1,42 @@
 # jsh-depends: playmp3andwait takecols chooserandomline filename
 # jsh-depends-ignore: music del
+
 SEARCH="$1"
+
 TRACK=`cat $JPATH/music/list.m3u | grep -i "$SEARCH" | ungrep INCOMPLETE | chooserandomline`
+
+## Inform the log
 echo "$TRACK" >> $JPATH/logs/xmms.log
-# filename "$TRACK"
-SIZE=`du -sh "$TRACK" | takecols 1`
-# echo "$SIZE: "`filename "$TRACK"`" ("`dirname "$TRACK"`")"
-# echo "$SIZE: "`dirname "$TRACK";curseyellow`/`filename "$TRACK";cursenorm`""
-echo "$SIZE: "`curseyellow;cursebold;filename "$TRACK";cursenorm`
-echo "`cursered`del \"$TRACK\"`cursenorm`"
-MP3INFO=`mp3info "$TRACK"`
-echo "$MP3INFO" |
-grep -v "^File: " |
-sed "s+[[:alpha:]]*:+`cursemagenta`\0`cursenorm`+g" |
-# sed "s+\(File:[^ ]* \)\(.*\)+`curseblue`\1 `curseblue`\2+"
+
+## Echo output to user
+
+	# filename "$TRACK"
+	SIZE=`du -sh "$TRACK" | takecols 1`
+	# echo "$SIZE: "`filename "$TRACK"`" ("`dirname "$TRACK"`")"
+	# echo "$SIZE: "`dirname "$TRACK";curseyellow`/`filename "$TRACK";cursenorm`""
+	echo "$SIZE: "`curseyellow;cursebold`filename "$TRACK"`cursenorm`
+
+	echo "`cursered`del \"$TRACK\"`cursenorm`"
+
+	MP3INFO=`mp3info "$TRACK"`
+	echo "$MP3INFO" |
+	grep -v "^File: " |
+	sed "s+[[:alpha:]]*:+`cursemagenta`\0`cursenorm`+g" |
+	# sed "s+\(File:[^ ]* \)\(.*\)+`curseblue`\1 `curseblue`\2+"
+	cat
+
+## Normalise volume (gain)
+
+	if which mp3gain > /dev/null 2>&1
+	then
+		NEWTRACK="/tmp/randommp3-gainchange.mp3"
+		cp "$TRACK" "$NEWTRACK"
+		mp3gain -r -c "$NEWTRACK"
+		TRACK="$NEWTRACK"
+	fi
+
 /usr/bin/time -f "%e seconds ( Time: %E CPU: %P Mem: %Mk )" playmp3andwait "$TRACK"
+
 echo
 echo "--------------------------------------"
 echo
