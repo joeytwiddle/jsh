b2eb1b5065e7aced186d4335fa85bbc4c56842ed
joey <joey>
2003-10-03 23:30:22 +0000

    *** empty log message ***

--- code/shellscript/javadev/diffjars.sh
+++ code/shellscript/javadev/diffjars.sh
@@ -1,3 +1,10 @@
+if [ "$1" = -src ]
+then
+	DECOMPILE=true; shift
+	# JARDIFFCOM="diffsrcjars"
+	JARDIFFCOM="diffdirs -showdiffswith diff"
+fi
+
 [ "$JARDIFFCOM" ] || JARDIFFCOM=diffdirs
 if [ "$1" = -diffcom ]
 then JARDIFFCOM="$2"; shift; shift
@@ -9,11 +16,13 @@ JARB=`realpath "$2"`
 JARADIR=`jgettmpdir "$JARA"`
 JARBDIR=`jgettmpdir "$JARB"`
 
-cd $JARADIR
-jar xf "$JARA"
+cd $JARADIR &&
+jar xf "$JARA" &&
+[ "$DECOMPILE" ] && javadecompile 2>&1 | grep -v "^Parsing " && find $JARADIR -type "*.class" | withalldo rm
 
-cd $JARBDIR
-jar xf "$JARB"
+cd $JARBDIR &&
+jar xf "$JARB" &&
+[ "$DECOMPILE" ] && javadecompile 2>&1 | grep -v "^Parsing " && find $JARBDIR -type "*.class" | withalldo rm
 
 $JARDIFFCOM $JARADIR $JARBDIR
 
