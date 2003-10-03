d074a6a1d548947a27f61760f84ccbdf70aa6550
joey <joey>
2003-10-03 18:38:29 +0000

    *** empty log message ***

--- code/shellscript/text/treevim.sh
+++ code/shellscript/text/treevim.sh
@@ -1,38 +1,44 @@
-## If it doesn't exist, create the necessary plugin for vim.
+## This is one method of viewing the output of tree.sh and tree.hs
+## It folds up branches in the tree using a vim plugin, and sets
+## colours and useful keymaps for opening/closing the folds.
 
-VIMPLUGIN=~/.vim/plugin/treefolding.vim
+## If it doesn't exist, create the necessary plugin for vim:
+
+VIMPLUGIN=/tmp/treefolding.vim
 
 if [ ! -f "$VIMPLUGIN" ]
 then
 
-	mkdir -p `basename "$VIMPLUGIN"`
-	cat > "$VIMPLUGIN" << EOF
-		" Keybindings
-		:map - zc
-		:map = zo
-		:map _ zC
-		:map + zO
-		:map <kMinus> zc
-		:map <kPlus> zo
-		:map <kDivide> zm
-		:map <kMultiply> zr
-		" Regions to fold and to match for highlighting (next line is the essential one)
-		:syntax region myFold matchgroup=myDummy start="{" end="}" transparent fold
-		:syntax match TreeListTag "^\(+\|-\) [^{}]*"
-		:syntax match TreeListTagNorm "^\(\.\|\*\) "
-		" Colours
-		:highlight Folded ctermbg=DarkBlue ctermfg=White guibg=#0000b0 guifg=White
-		:highlight FoldColumn ctermbg=DarkBlue ctermfg=White cterm=bold gui=bold guifg=White guibg=#0000b0
-		:highlight TreeListTag ctermbg=darkblue ctermfg=grey cterm=none gui=bold guibg=#000060
-		:highlight TreeListTagNorm ctermbg=darkblue ctermfg=grey cterm=none gui=bold guibg=#000060
-		" Options
-		:set foldtext=getline(v:foldstart).'\ \ \ ['.(v:foldend-v:foldstart).'\ lines]'
-		:set fdc=0
-		" Go
-		:set foldmethod=syntax
-		:syn sync fromstart
+mkdir -p `basename "$VIMPLUGIN"`
+cat > "$VIMPLUGIN" << EOF
+" Keybindings
+:map - zc
+:map = zo
+:map _ zC
+:map + zO
+:map <kMinus> zc
+:map <kPlus> zo
+:map <kDivide> zm
+:map <kMultiply> zr
+" Regions to fold and to match for highlighting (next line is the essential one)
+:syntax region myFold matchgroup=myDummy start="{" end="}" transparent fold
+:syntax match TreeListTag "^\(+\|-\) [^{}]*"
+:syntax match TreeListTagNorm "^\(\.\|\*\) "
+" Colours
+:highlight Folded ctermbg=DarkBlue ctermfg=White guibg=#0000b0 guifg=White
+:highlight FoldColumn ctermbg=DarkBlue ctermfg=White cterm=bold gui=bold guifg=White guibg=#0000b0
+:highlight TreeListTag ctermbg=darkblue ctermfg=grey cterm=none gui=bold guibg=#000060
+:highlight TreeListTagNorm ctermbg=darkblue ctermfg=grey cterm=none gui=bold guibg=#000060
+" Options
+:set foldtext=getline(v:foldstart).'\ \ \ ['.(v:foldend-v:foldstart).'\ lines]'
+:set fdc=0
+" Go
+:set foldmethod=syntax
+:syn sync fromstart
 EOF
 
 fi
 
-vim - -R $VIMOPTS
+## Run vim with the plugin:
+
+vim - -R $VIMOPTS +":so $VIMPLUGIN"
