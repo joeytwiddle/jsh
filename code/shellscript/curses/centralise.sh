9780ff2dc88659eeb8a725fbf0ef6926377ae972
joey <joey>
2003-10-12 21:11:34 +0000

    important bugfix - at least 1 previous version was broken!

--- code/shellscript/curses/centralise.sh
+++ code/shellscript/curses/centralise.sh
@@ -3,7 +3,7 @@ PADLEFT=">"
 PADBRACE=" "
 PADRIGHT="<"
 
-if test "$1" = "" || test "$1" == "--help"
+if [ "$1" = "" ] || [ "$1" = "--help" ]
 then
 	echo "centralise <string>"
 	echo "centralise <padchars> <string>"
@@ -11,15 +11,15 @@ then
 	exit 1
 fi
 
-if test ! "$2" = ""
+if [ ! "$2" = "" ]
 then
-	if test "$3" = ""
+	if [ "$3" = "" ]
 	then
 		PADLEFT="$1"
 		PADRIGHT="$1"
 		shift
 	else
-		if test "$1" = "-pad"
+		if [ "$1" = "-pad" ]
 		then shift
 		fi
 		PADLEFT="$1"
@@ -31,17 +31,17 @@ fi
 
 STRING="$1"
 
-if test "$COLUMNS" = ""; then COLUMNS=80; fi
+if [ "$COLUMNS" = "" ]
+then COLUMNS=80
+fi
 LEN=`strlen "$STRING"`
 REMAINING=`expr "$COLUMNS" - "$LEN"`
 LEFT=`expr "$REMAINING" / 2 - 2`
 for X in `seq 1 $LEFT`
-do
-	printf "%s" "$PADLEFT"
+do printf "%s" "$PADLEFT"
 done
 printf "%s" "$PADBRACE$STRING$PADBRACE"
 for X in `seq 1 $LEFT`
-do
-	printf "%s" "$PADRIGHT"
+do printf "%s" "$PADRIGHT"
 done
 printf "\n"
