ffcb1a83480879d62750b646fbefaf546e86866f
joey <joey>
2004-04-19 01:00:38 +0000

    I think this bloated functionality was migrated elsewhere sometime ago. Anyway, this shorter version seems to achieve the same.

--- code/shellscript/unixwrap/jman.sh
+++ code/shellscript/unixwrap/jman.sh
@@ -1,32 +1,14 @@
-REALMAN=`jwhich man`
-# INJ=`jwhich inj $1`
-
-## If screen is running, could pop up in an extra window.  Could make this X/screen window popup functionality generic
-
-## If user is running in X, we pop up a separate window for them
-if [ "$STY" ]
-then screen -X screen -t '"'"$1"'"' $REALMAN -a "$@"
-elif xisrunning
-then
-	# [ "$INJ" ] && whitewin -title "jdoc $1" -geometry 80x60 -e jdoc "$1"
-	## man will try to fit page within COLUMNS>=80plz, and then we will fit to whatever man outputs
-	export COLUMNS=120
-	## First, check a manual page actually exists: (man will print error for us if not)
-	if rememo $REALMAN -a "$@" > /dev/null
-	then
-		## Need to format output to find widest line
-		WIDTH=`memo $REALMAN -a "$@" | col -bx | longestline`
-		# WIDTH=`expr $WIDTH + 2`
-		if test "$WIDTH" -lt "10"; then echo "col -bx | longestline failed" | tee -a "$JPATH/logs/jshdebug"; WIDTH="80"; fi
-		whitewin -title "Manual: $*" -geometry "$WIDTH"x60 -e $REALMAN -a "$@"
-	fi
-else
-	$REALMAN -a "$@"
-	# [ "$INJ" ] && jdoc "$1"
+## Popup the man window first if running in X:
+if xisrunning
+then manpopup "$@"
 fi
 
-## Always in terminal; looks for documentation within j project
-if test -x "$JPATH/tools/$1"
+## If the command is a jsh script, show jsh documentation (may popup, but always asks questions in the terminal):
+if [ -x "$JPATH/tools/$1" ]
 then jdoc "$@"
 fi
 
+## Show the man page last if not running in X:
+if ! xisrunning
+then manpopup "$@"
+fi
