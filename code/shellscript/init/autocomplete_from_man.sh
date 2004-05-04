9cd13161d89ff2bf1e12ea6401ee146149b0d779
joey <joey>
2004-05-04 05:40:47 +0000

    Trimmed and tidied

--- code/shellscript/init/autocomplete_from_man.sh
+++ code/shellscript/init/autocomplete_from_man.sh
@@ -1,92 +1,23 @@
-## Extracts the options for command-line programs from man pages, and adds them to the shell completion rules.
-## Works with bash and zsh.
-## eg.: source this script then type: tar --<Tab>
+## autocomplete_from_man: Adds to bash or zsh the ability to use tab-completion for the - and -- options of the current command (provided it has a man page installed).
+## eg.: source this script with ". ./autocomplete_from_man", then type: tar --<Tab>
 
-## To clear [bash]: complete -r
+# jsh-depends: extractpossoptsfrommanpage
+# jsh-ext-depends: sed find
 
-## TODO: cache rules so they may be loaded faster (means generating them as stream?)
-## CONSIDER: save completions rules in global file rather than bloating every shell; use completion function and grep to retrieve them when needed
-
-
-
-# ## Method 1: pre-generate rules for selected commands
-# 
-# ## TODO: obtain more commands from shell-history
-# COMMANDS="ls man tar ssh rsync mplayer btdownloadcurses grep diff cvs svn dpkg apt-get rpm wget lynx java jar $*"
-# if [ "$ZSH_NAME" ] ## TODO: convert the bash string to a zsh array!
-# then COMMANDS=(ls man tar ssh rsync mplayer btdownloadcurses grep diff cvs svn dpkg apt-get rpm wget lynx java jar $*)
-# fi
-# 
-# for COMMAND in $COMMANDS
-# do
-# 
-	# echo -n "$COMMAND " >&2
-# 
-	# MANOPTS="--help "`memo extractpossoptsfrommanpage "$COMMAND" 2>/dev/null | tr '\n' ' '`
-# 
-	# if [ "$ZSH_NAME" ]
-	# then
-		# compctl -tn -f -c -u -r -k "($MANOPTS)" -H 0 '' $COMMAND ## -f files -c commands -u users -r running jobs +-H last_resort_history -k man_options
-	# elif [ "$BASH" ]
-	# then
-		# complete -W "$MANOPTS" $COMMAND
-	# fi
-# 
-# done
-# 
-# echo >&2
-
-
-
-## Method 2: generate for any command on the fly
-
-mkdir -p /tmp/completion_options
-
-function joeyComplete0 {
-	## Heuristic:
-	if [ ! "$ARGS" ]
-	then
-		reply=
-		COMPREPLY=
-	else
-		## Cache:
-		MEMOFILE=/tmp/completion_options/"$COMMAND".cached
-		if [ ! -f "$MEMOFILE" ] || [ "$REMEMO" ]
-		then extractpossoptsfrommanpage "$COMMAND" 2>/dev/null | tr '\n' ' ' > "$MEMOFILE"
-		fi
-		reply=(--help `cat "$MEMOFILE"`) ## zsh
-		COMPREPLY=(--help `cat "$MEMOFILE"`) ## bash
-		COMPREPLY2="--help "`cat "$MEMOFILE"` ## bash
-	fi
-}
-
-## zsh version: works on any command on the fly.  Jsh memoing was too slow, and I could not get non-glob matches with -tn to avoid this glob search.
-## So instead I added an heuristic and simple caching inline to make it fast.
-if [ "$ZSH_NAME" ]
-then
-
-	function joeyComplete {
-		read -c COMMAND ARGS
-		joeyComplete0
-	}
-
-	compctl -f -c -u -r -K joeyComplete -H 0 '' "*" -tn
-
-fi
-
-## bash version: looking at man page, I cannot get it to run on any command, only on those I list
+## bash version:
 if [ "$BASH" ]
 then
 
 	function joeyComplete {
 		COMMAND="$1"
 		ARGS="$1"
-		joeyComplete0
-		## Fix for bug: it shows you the options, but doesn't let you complete them!  Also acts as cache!
-		complete -W "$COMPREPLY2" "$COMMAND"
+		WORDS="--help "`extractpossoptsfrommanpage "$COMMAND"`
+		## Fix for bug: "it shows you the options, but doesn't let you complete them!"  Also acts as a cache.
+		complete -W "$WORDS" "$COMMAND"
+		COMPREPLY=($WORDS)
 	}
 
-	## Since bash only runs completion on specified commands, we just go and get the names of all commands in $PATH:
+	## Since bash only runs completion on named commands, we must go and get the names of all commands in $PATH:
 	complete -F joeyComplete `
 		echo "$PATH" | tr ':' '\n' |
 		while read DIR
@@ -96,3 +27,30 @@ then
 
 fi
 
+## zsh version: I could not prevent named matches from continuing on to the glob function, despite use of "-tn".
+## So I added an heuristic and simple caching to make it fast.
+if [ "$ZSH_NAME" ]
+then
+
+	mkdir -p /tmp/completion_options
+
+	function joeyComplete {
+		read -c COMMAND ARGS
+		## Heuristic:
+		if [ ! "$ARGS" ]
+		then
+			reply=
+		else
+			## Cache:
+			MEMOFILE=/tmp/completion_options/"$COMMAND".cached
+			if [ ! -f "$MEMOFILE" ] || [ "$REMEMO" ]
+			then extractpossoptsfrommanpage "$COMMAND" > "$MEMOFILE"
+			fi
+			reply=(--help `cat "$MEMOFILE"`)
+			## Ne marche pas: compctl -f -c -u -r -k "($reply)" -H 0 '' "$COMMAND" -tn
+		fi
+	}
+
+	compctl -f -c -u -r -K joeyComplete -H 0 '' "*" -tn
+
+fi
