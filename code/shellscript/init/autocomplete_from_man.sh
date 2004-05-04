025857ab737eecdf47ad124a551ad0703302f71f
joey <joey>
2004-05-04 04:27:29 +0000

    Works on all commands for both zsh and bash!

--- code/shellscript/init/autocomplete_from_man.sh
+++ code/shellscript/init/autocomplete_from_man.sh
@@ -40,27 +40,34 @@
 
 ## Method 2: generate for any command on the fly
 
+mkdir -p /tmp/completion_options
+
+function joeyComplete0 {
+	## Heuristic:
+	if [ ! "$ARGS" ]
+	then
+		reply=
+		COMPREPLY=
+	else
+		## Cache:
+		MEMOFILE=/tmp/completion_options/"$COMMAND".cached
+		if [ ! -f "$MEMOFILE" ] || [ "$REMEMO" ]
+		then extractpossoptsfrommanpage "$COMMAND" 2>/dev/null | tr '\n' ' ' > "$MEMOFILE"
+		fi
+		reply=(--help `cat "$MEMOFILE"`) ## zsh
+		COMPREPLY=(--help `cat "$MEMOFILE"`) ## bash
+		COMPREPLY2="--help "`cat "$MEMOFILE"` ## bash
+	fi
+}
+
 ## zsh version: works on any command on the fly.  Jsh memoing was too slow, and I could not get non-glob matches with -tn to avoid this glob search.
 ## So instead I added an heuristic and simple caching inline to make it fast.
 if [ "$ZSH_NAME" ]
 then
 
-	mkdir /tmp/completion_options
-
 	function joeyComplete {
 		read -c COMMAND ARGS
-		## Heuristic:
-		if [ ! "$ARGS" ]
-		then
-			reply=""
-		else
-			## Cache:
-			MEMOFILE=/tmp/completion_options/"$COMMAND".cached
-			if [ ! -f "$MEMOFILE" ] || [ "$REMEMO" ]
-			then extractpossoptsfrommanpage "$COMMAND" 2>/dev/null | tr '\n' ' ' > "$MEMOFILE"
-			fi
-			reply=(--help `cat "$MEMOFILE"`)
-		fi
+		joeyComplete0
 	}
 
 	compctl -f -c -u -r -K joeyComplete -H 0 '' "*" -tn
@@ -68,16 +75,24 @@ then
 fi
 
 ## bash version: looking at man page, I cannot get it to run on any command, only on those I list
-## BUG: also, it's broken: it shows you the options, but doesn't let you complete them!  I suspect the reply array.
 if [ "$BASH" ]
 then
 
 	function joeyComplete {
 		COMMAND="$1"
-		COMPREPLY=(--help `memo extractpossoptsfrommanpage "$COMMAND" 2>/dev/null | tr '\n' ' '`)
+		ARGS="$1"
+		joeyComplete0
+		## Fix for bug: it shows you the options, but doesn't let you complete them!  Also acts as cache!
+		complete -W "$COMPREPLY2" "$COMMAND"
 	}
 
-	complete -F joeyComplete cat patch make
+	## Since bash only runs completion on specified commands, we just go and get the names of all commands in $PATH:
+	complete -F joeyComplete `
+		echo "$PATH" | tr ':' '\n' |
+		while read DIR
+		do find "$DIR" -type f -maxdepth 1
+		done | sed 's+.*/++'
+	`
 
 fi
 
