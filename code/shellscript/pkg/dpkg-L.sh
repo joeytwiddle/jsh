c09671f377ecbc1ea470e1a0b7fcce2cf2c48f89
joey <joey>
2004-05-22 06:33:19 +0000

    *** empty log message ***

---
+++ code/shellscript/pkg/dpkg-L.sh
@@ -0,0 +1,25 @@
+## Fast version of dpkg -L
+
+if [ "$1" = -test ]
+then
+	shift
+	SYS=`jgettmp dpkg_-L`
+	HAK=`jgettmp dpkg-L`
+	# dpkg -L "$@" | filesonly | sort > $SYS
+	time dpkg-L  "$@" | sort > $HAK
+	time dpkg -L "$@" | sort > $SYS
+	vimdiff $SYS $HAK
+	jdeltmp $SYS $HAK
+	exit
+fi
+
+PACKAGENAME="$1"
+BASENAME=/var/lib/dpkg/info/$PACKAGENAME
+
+## Reads only one file, but doesn't cksum all.
+# cat $BASENAME.md5sums |
+# dropcols 1 |
+# sed 's+^+/+'
+
+cat $BASENAME.list $BASENAME.conffiles |
+removeduplicatelines
