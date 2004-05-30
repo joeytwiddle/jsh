33e8594ca453beaf30b4449e95be9f9351c27678
joey <joey>
2004-05-30 08:23:53 +0000

    *** empty log message ***

--- code/shellscript/installwrapper/get_imwheel_softmouse.sh
+++ code/shellscript/installwrapper/get_imwheel_softmouse.sh
@@ -1,4 +1,5 @@
-mkdir -p $HOME/bin
-wget "http://hwi.ath.cx/projects/imwheel_softmouse/src/imwheel" -O $HOME/bin/imwheel
-wget "http://hwi.ath.cx/projects/imwheel_softmouse/src/.imwheelrc" -O $HOME/.imwheelrc
-echo "To run imwheel without detaching, type: \$HOME/bin/imwheel -d"
+mkdir -p $HOME/bin &&
+wget -nv "http://hwi.ath.cx/projects/imwheel_softmouse/src/imwheel" -O $HOME/bin/imwheel &&
+wget -nv "http://hwi.ath.cx/projects/imwheel_softmouse/src/.imwheelrc" -O $HOME/.imwheelrc &&
+echo "To run imwheel without detaching, type: \$HOME/bin/imwheel -d" &&
+xterm -e $HOME/bin/imwheel -d
