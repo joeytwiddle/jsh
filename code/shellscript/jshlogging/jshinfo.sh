dc9d45bf9364aa5e511482681e9925a77fcb7ddc
joey <joey>
2004-06-05 07:38:22 +0000

    *** empty log message ***

---
+++ code/shellscript/jshlogging/jshinfo.sh
@@ -0,0 +1,1 @@
+echo "`curseyellow;cursebold`[INFO] `cursecyan`$*`cursenorm`" >&2
---
+++ code/shellscript/jshlogging/jshwarn.sh
@@ -0,0 +1,6 @@
+## TODO: CONSIDER: WARN=off disabled it?
+## TODO: CONSIDER: some global variable sends jsh logs to a file?
+# if [ "$WARN" ]
+# then
+echo "`curseyellow;cursebold`[WARN] `cursecyan`$*`cursenorm`" >&2
+# fi
