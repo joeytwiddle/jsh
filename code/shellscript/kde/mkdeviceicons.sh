664eecd4564729ed4a7911e2e207d210b4f798e9
joey <joey>
2004-06-14 01:40:43 +0000

    *** empty log message ***

---
+++ code/shellscript/kde/mkdeviceicons.sh
@@ -0,0 +1,76 @@
+LETTERS="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+LETTER=C
+
+mkdir -p "$HOME/Desktop" || exit 123
+
+## Because we go in reverse; we need to get the last letter!
+cat /etc/fstab | grep "^/dev/hd" |
+while read DEVICE MNTPOINT FSTYPE OPTIONS DUMP PASS
+do LETTER=`echo "$LETTER" | tr " $LETTERS" "$LETTERS"`
+done
+
+cat /etc/fstab | grep "^/dev/hd" |
+
+reverse |
+
+while read DEVICE MNTPOINT FSTYPE OPTIONS DUMP PASS
+do
+
+	LETTER=`echo "$LETTER" | tr "$LETTERS" " $LETTERS"`
+
+	if [ -d "$MNTPOINT" ]
+	then
+
+		MNTNAME=`echo "$MNTPOINT" | sed 's+.*/++'`
+		FILE="$HOME/Desktop/Drive $LETTER $MNTNAME ($FSTYPE)"
+
+		(
+			echo "[Desktop Entry]"
+			echo "Dev=$DEVICE"
+			echo "Icon=hdd_mount"
+			echo "MountPoint=$MNTPOINT"
+			echo "Type=FSDevice"
+			echo "UmountIcon=hdd_unmount"
+		) > "$FILE"
+
+	fi
+
+done
+
+cat > $HOME/Desktop/"CD-Rom Drive" << !
+[Desktop Action Eject]
+Exec=kdeeject %v
+Name=Eject
+
+[Desktop Entry]
+Actions=Eject
+Dev=/dev/cdrom
+Encoding=UTF-8
+Icon=cdrom_mount
+MountPoint=/cdrom
+ReadOnly=true
+Type=FSDevice
+UnmountIcon=cdrom_unmount
+X-KDE-Priority=TopLevel
+!
+
+cat > $HOME/Desktop/"Floppy Disc" << !
+[Desktop Action Format]
+Exec=kfloppy %v
+Name=Format
+
+[Desktop Entry]
+Actions=Format
+Dev=/dev/fd0
+Encoding=UTF-8
+Icon=3floppy_mount
+MountPoint=/floppy
+ReadOnly=false
+Type=FSDevice
+UnmountIcon=3floppy_unmount
+X-KDE-Priority=TopLevel
+!
+
+[ -f "$HOME/Desktop/Home ] && touch $HOME/Desktop/Home
+[ -f "$HOME/Desktop/"Install to harddisk" ] && touch $HOME/Desktop/"Install to harddisk"
+
