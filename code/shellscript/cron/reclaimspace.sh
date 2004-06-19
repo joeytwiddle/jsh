60bc7e175ea007cfd43f5700ac89a7726dc08297
joey <joey>
2004-06-19 01:33:11 +0000

    *** empty log message ***

--- code/shellscript/cron/reclaimspace.sh
+++ code/shellscript/cron/reclaimspace.sh
@@ -4,8 +4,30 @@
 ## This didn't work when if test -f "$FILE" hadn't quotes on a spaced file.
 set -e
 
-df | drop 1 | takecols 1 4 6 |
-grep -v "/cdrom" |
+# export MINKBYTES=10240 ## 10Meg
+[ "$MINKBYTES" ] || export MINKBYTES=51200 ## 50Meg
+
+## TODO: determine whether mount is ro, and if so skip it.
+
+## BUG: There is some situation whereby the -lt test complains: 100%: integer expression expected
+##      Ah yes this is when a really long device (eg. a file) is used, and the numbers drop to the next line!
+##      The solution would be to join each line containing no spaces to the next line.  Although this (in fact the script anyway) would have trouble if the filename/device contains spaces.
+##      Here is a dodgy hack:
+nicedf () {
+	df |
+	sed 's+^\([^ ]*\)[ ]*$+\1 JOIN_LINE+' |
+	escapenewlines |
+	# pipeboth |
+	sed 's+ JOIN_LINE\\n+ +g' |
+	unescapenewlines
+}
+
+nicedf | drop 1 |
+
+takecols 1 4 6 |
+grep -v "/cdr" |
+
+# pipeboth |
 
 while read PARTITION SPACE POINT
 do
@@ -13,35 +35,43 @@ do
 	GOAGAIN=true
 	ATTEMPTSMADE=0
 
+	# [ -d "$POINT"/RECLAIM ] && cd "$POINT"/RECLAIM && find . -type f | countlines
+
+	## Ideally we wouldn't do this if we check before and space is ok.
+	[ -d "$POINT"/RECLAIM ] &&
+	cd "$POINT"/RECLAIM &&
+	find . -type f |
+	( randomorder && echo ) | ## need this end line otherwise read FILE on last entry ends the stream and hence the sh is killed.
+
 	## I can't get set -e to work on these tests; because they are in while loop?
-	while test "$SPACE" -lt 10240 && test "$GOAGAIN"
+	while [ "$SPACE" -lt "$MINKBYTES" ] && [ "$GOAGAIN" ]
 	do
 
 		ATTEMPTSMADE=`expr "$ATTEMPTSMADE" + 1`
-		if test "$ATTEMPTSMADE" -gt 50
+		if [ "$ATTEMPTSMADE" -gt 999 ]
 		then
+			## BUG: of course this can be reached legitimately before the work is done if the reclaim dir is full of many small or empty files.
 			error "Stopping on $ATTEMPTSMADE"st" reclamation attempt, assuming problem!"
 			exit 12
 		fi
 
 		GOAGAIN=
 
-		echo "Partition $PARTITION mounted at $POINT has $SPACE"k" < 10M of space."
+		echo "Partition $PARTITION mounted at $POINT has $SPACE"k" < $MINKBYTES"k" of space."
 
-		if test -d "$POINT"/RECLAIM
+		if [ -d "$POINT"/RECLAIM ]
 		then
 
-			FILE=`
-				cd "$POINT"/RECLAIM &&
-				find . -type f |
-				chooserandomline
-			`
-			if test -f "$POINT"/RECLAIM/"$FILE"
+			read FILE
+			# debug "$FILE"
+
+			if [ -f "$POINT"/RECLAIM/"$FILE" ]
 			then
 				echo "Reclaiming: rm -f $POINT"/RECLAIM/"$FILE"
 				rm -f "$POINT"/RECLAIM/"$FILE" &&
 				GOAGAIN=true
-			else echo "But there is nothing in $POINT/RECLAIM to reclaim."
+			else
+				echo "But there is nothing in $POINT/RECLAIM to reclaim.  $FILE"
 			fi
 
 		else
@@ -50,7 +80,7 @@ do
 
 		fi
 
-		SPACE=`df | grep "^$PARTITION" | takecols 4`
+		SPACE=`nicedf | grep "^$PARTITION" | takecols 4`
 
 	done
 
