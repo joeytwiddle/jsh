41ccd161324c68b078b998acbcd96cde44b00f37
joey <joey>
2004-06-19 01:41:53 +0000

    *** empty log message ***

--- code/shellscript/sys/jchroot.sh
+++ code/shellscript/sys/jchroot.sh
@@ -16,6 +16,14 @@ mv "$TARGET"/dev/zero "$TARGET"/dev/zero.b4
 dd if=/dev/zero bs=1024 count=100 of="$TARGET"/dev/zero
 chmod ugo+r "$TARGET"/dev/zero
 
+## Bind all the same mounts
+for MNTPNT in /mnt/*
+do
+	if [ -d "$MNTPNT" ] && [ -d "$TARGET"/"$MNTPNT" ]
+	then mount --bind "$MNTPNT" "$TARGET"/"$MNTPNT"
+	fi
+done
+
 
 
 ### Chroot
@@ -30,3 +38,10 @@ chroot "$TARGET" umount -lf /proc
 
 mv -f $TARGET/dev/null.b4 $TARGET/dev/null
 mv -f $TARGET/dev/zero.b4 $TARGET/dev/zero
+
+for MNTPNT in /mnt/*
+do
+	if [ -d "$MNTPNT" ] && [ -d "$TARGET"/"$MNTPNT" ] && df | grep "^$MNTPNT[ 	].*$TARGET/$MNTPNT$"
+	then umount "$TARGET"/"$MNTPNT"
+	fi
+done
