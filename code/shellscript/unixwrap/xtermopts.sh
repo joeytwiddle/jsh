4cf24416369c6cbd2bae7a69833a12d42b9b6da8
joey <joey>
2004-06-21 15:55:03 +0000

    *** empty log message ***

--- code/shellscript/unixwrap/xtermopts.sh
+++ code/shellscript/unixwrap/xtermopts.sh
@@ -1,8 +1,8 @@
 # jsh-ext-depends-ignore: linux
 # jsh-ext-depends: hostname
 # jsh-depends: endswith
-ARGS="-cc 33:48,37:48,45-47:48,64:48,126:48"
-if test "$VNCDESKTOP" = "X"
+# ARGS="-cc 33:48,37:48,45-47:48,64:48,126:48"
+if [ "$VNCDESKTOP" = "X" ]
 then ## Actually for low-res desktops; we could guess this from xdpyinfo?
 	# FONT='-b&h-lucidatypewriter-medium-r-normal-*-*-100-*-*-m-*-iso8859-1'
 	# FONT='-b&h-lucidatypewriter-medium-r-normal-*-11-*-*-*-m-*-iso8859-1'
@@ -10,18 +10,17 @@ then ## Actually for low-res desktops; we could guess this from xdpyinfo?
 	# FONT='-schumacher-clean-medium-r-normal-*-*-120-*-*-c-*-iso646.1991-irv'
 	FONT='-schumacher-clean-medium-r-normal-*-12-*-*-*-c-*-iso646.1991-irv'
 # elif test "$JM_UNAME" = "linux" && ! startswith `hostname` "qanir"
-elif test "$JM_UNAME" = "linux" && ! endswith `hostname` cs.bris.ac.uk
-then
-	FONT='-b&h-lucidatypewriter-medium-r-normal-*-*-80-*-*-m-*-iso8859-1'
-	ARGS="$ARGS -rightbar"
-elif test "$JM_UNAME" = "cygwin"
+elif [ "$JM_UNAME" = "cygwin" ]
 then
 	FONT='-b&h-lucidatypewriter-medium-r-normal-*-*-12-*-*-m-*-iso8859-1'
 	# cygwin doesn't like -cc
 	ARGS=""
-else
+elif endswith `hostname` cs.bris.ac.uk
+then
 	## Uni?
 	FONT='-b&h-lucidatypewriter-medium-r-normal-*-*-100-*-*-m-*-iso8859-1'
+else
+	FONT='-b&h-lucidatypewriter-medium-r-normal-*-*-80-*-*-m-*-iso8859-1'
 fi
 ## Big Courier:
 # FONT='-adobe-courier-medium-r-normal-*-*-140-*-*-p-*-iso8859-2'
