ad6f3ee77e8a0225e36bda56723d6a48af07d733
joey <joey>
2004-08-27 18:10:27 +0000

    *** empty log message ***

--- code/shellscript/net/traffic_shaping.sh
+++ code/shellscript/net/traffic_shaping.sh
@@ -11,6 +11,8 @@ DEV=eth1
 ##   - debug stats pfifo handle 12: is showing up bytes (esp. at rule creation) but I don't know where they come from.  I do not believed it is being pointed to anywhere (at least not intentionally!).
 ##   - I have separated into a number of different discs/bands? for debugging of what's going where.  But probably we only need four bands total...
 
+## OK finally realised that the disc #s are hex, so we can probably have max f of them, not 9!
+
 function filter_port () {
 	TYPE="$1"
 	PORTDIR="$2"
@@ -32,6 +34,7 @@ case "$1" in
 		;;
 
 		start)
+			echo -n "shaping: "
 			## configure "$DEV" so that there is a bandwidth cap on large packets going up the DSL line
 			## and then garp advertising the true gateway's IP, so that other hosts use us rather than it
 
@@ -54,21 +57,43 @@ case "$1" in
 			## ones before dropping. a buffer of 1600 tokens means that at any time we are ready to burst one of
 			## these big ones (at the peakrate, 128kbit/s). the mtu of 1518 instead of 1514 is in case I ever start
 			## using vlan tagging, because if mtu is too low (like 1500) then all traffic blocks
-			# /sbin/tc qdisc add dev "$DEV" parent 1:3 handle 13: tbf rate 20kbit buffer 1600 peakrate 40kbit mtu 1518 mpu 64 latency 50ms
-			# /sbin/tc qdisc add dev "$DEV" parent 1:3 handle 13: tbf rate 80kbit buffer 1600 peakrate 100kbit mtu 1518 mpu 64 latency 50ms
-			# /sbin/tc qdisc add dev "$DEV" parent 1:3 handle 13: tbf rate 60kbit buffer 1600 peakrate 75kbit mtu 1518 mpu 64 latency 50ms
-			/sbin/tc qdisc add dev "$DEV" parent 1:9 handle 19: tbf rate 80kbit buffer 1600 peakrate 90kbit mtu 1518 mpu 64 latency 50ms
-			# /sbin/tc qdisc add dev "$DEV" parent 1:9 handle 19: tbf rate 80kbit buffer 1600 peakrate 100kbit mtu 1518 mpu 64 latency 50ms
-			# # /sbin/tc qdisc add dev "$DEV" parent 1:9 handle 19: tbf rate 80kbit buffer 1600 peakrate 120kbit mtu 1518 mpu 64 latency 50ms
+			# # /sbin/tc qdisc add dev "$DEV" parent 1:3 handle 13: tbf rate 20kbit buffer 1600 peakrate 40kbit mtu 1518 mpu 64 latency 50ms
+			# # /sbin/tc qdisc add dev "$DEV" parent 1:3 handle 13: tbf rate 80kbit buffer 1600 peakrate 100kbit mtu 1518 mpu 64 latency 50ms
+			# # /sbin/tc qdisc add dev "$DEV" parent 1:3 handle 13: tbf rate 60kbit buffer 1600 peakrate 75kbit mtu 1518 mpu 64 latency 50ms
+			# /sbin/tc qdisc add dev "$DEV" parent 1:9 handle 19: tbf rate 80kbit buffer 1600 peakrate 90kbit mtu 1518 mpu 64 latency 50ms
+			# # /sbin/tc qdisc add dev "$DEV" parent 1:9 handle 19: tbf rate 80kbit buffer 1600 peakrate 100kbit mtu 1518 mpu 64 latency 50ms
+			# # # /sbin/tc qdisc add dev "$DEV" parent 1:9 handle 19: tbf rate 80kbit buffer 1600 peakrate 120kbit mtu 1518 mpu 64 latency 50ms
+			## Decided they should have equal weighting:
+			# /sbin/tc qdisc add dev "$DEV" parent 1:9 handle 19: tbf rate 60kbit buffer 1600 peakrate 70kbit mtu 1518 mpu 64 latency 50ms
+			# /sbin/tc qdisc add dev "$DEV" parent 1:9 handle 19: tbf rate 50kbit buffer 1600 peakrate 60kbit mtu 1518 mpu 64 latency 50ms
+
+			# /sbin/tc qdisc add dev "$DEV" parent 1:9 handle 19: tbf rate 50kbit buffer 1600 peakrate 60kbit mtu 1518 mpu 64 latency 50ms
+			/sbin/tc qdisc add dev "$DEV" parent 1:9 handle 19: tbf rate 40kbit buffer 1600 peakrate 50kbit mtu 1518 mpu 64 latency 50ms
+			# /sbin/tc qdisc add dev "$DEV" parent 1:9 handle 19: tbf rate 30kbit buffer 1600 peakrate 40kbit mtu 1518 mpu 64 latency 50ms
+			# /sbin/tc qdisc add dev "$DEV" parent 1:9 handle 19: tbf rate 20kbit buffer 1600 peakrate 30kbit mtu 1518 mpu 64 latency 50ms
 
 			## For small packets:
-			/sbin/tc qdisc add dev "$DEV" parent 1:8 handle 18: tbf rate 20kbit buffer 1600 peakrate 30kbit mtu 1518 mpu 64 latency 50ms
+			# /sbin/tc qdisc add dev "$DEV" parent 1:8 handle 18: tbf rate 20kbit buffer 1600 peakrate 30kbit mtu 1518 mpu 64 latency 50ms
+			# # /sbin/tc qdisc add dev "$DEV" parent 1:8 handle 18: tbf rate 30kbit buffer 1600 peakrate 40kbit mtu 1518 mpu 64 latency 50ms
+			# # # /sbin/tc qdisc add dev "$DEV" parent 1:8 handle 18: tbf rate 20kbit buffer 1600 peakrate 120kbit mtu 1518 mpu 64 latency 50ms
+			## Decided they should have equal weighting:
+			# /sbin/tc qdisc add dev "$DEV" parent 1:8 handle 18: tbf rate 40kbit buffer 1600 peakrate 50kbit mtu 1518 mpu 64 latency 50ms
+			# /sbin/tc qdisc add dev "$DEV" parent 1:8 handle 18: tbf rate 50kbit buffer 1600 peakrate 60kbit mtu 1518 mpu 64 latency 50ms
+
+			# /sbin/tc qdisc add dev "$DEV" parent 1:8 handle 18: tbf rate 50kbit buffer 1600 peakrate 60kbit mtu 1518 mpu 64 latency 50ms
+			/sbin/tc qdisc add dev "$DEV" parent 1:8 handle 18: tbf rate 40kbit buffer 1600 peakrate 50kbit mtu 1518 mpu 64 latency 50ms
 			# /sbin/tc qdisc add dev "$DEV" parent 1:8 handle 18: tbf rate 30kbit buffer 1600 peakrate 40kbit mtu 1518 mpu 64 latency 50ms
-			# # /sbin/tc qdisc add dev "$DEV" parent 1:8 handle 18: tbf rate 20kbit buffer 1600 peakrate 120kbit mtu 1518 mpu 64 latency 50ms
+			# /sbin/tc qdisc add dev "$DEV" parent 1:8 handle 18: tbf rate 20kbit buffer 1600 peakrate 30kbit mtu 1518 mpu 64 latency 50ms
 
 			## add fifos to the other bands so we can have some stats
 			for SUBDISC in `seq 7 -1 1`
-			do /sbin/tc qdisc add dev "$DEV" parent 1:$SUBDISC handle 1$SUBDISC: pfifo
+			do
+				# if [ "$SUBDISC" = 6 ]
+				# then
+					# /sbin/tc qdisc add dev "$DEV" parent 1:$SUBDISC handle 1$SUBDISC: tbf rate 240kbit buffer 1600 peakrate 280kbit mtu 1518 mpu 64 latency 50ms
+				# else
+					/sbin/tc qdisc add dev "$DEV" parent 1:$SUBDISC handle 1$SUBDISC: pfifo
+				# fi
 			done
 
 			## add a filter so DIP's within the house go to prio band #1 instead of being assigned by TOS
@@ -81,56 +106,82 @@ case "$1" in
 
 			### Critical:
 
+			## Games:
+			##          unreal....most................
+			for PORT in 7775 7776 7777 7778 7779 27900
+			do
+				filter_port batch$PORT sport $PORT 1
+				filter_port batch$PORT dport $PORT 1
+			done
+
 			## Hwi's mail services:
-			filter_port smtp sport 25 1
-			filter_port imap2 sport 143 1
-			filter_port imap3 sport 220 1
-			filter_port imaps sport 993 1
-			filter_port pop2 sport 109 1
-			filter_port pop3 sport 110 1
-			filter_port pop3s sport 995 1
+			filter_port smtp     sport 25   3
+			filter_port ssmtp    sport 465  3
+			filter_port imap2    sport 143  3
+			filter_port imap3    sport 220  3
+			filter_port imaps    sport 993  3
+			filter_port pop2     sport 109  3
+			filter_port pop3     sport 110  3
+			filter_port pop3s    sport 995  3
 			## Remote mail:
-			filter_port smtp dport 25 3
-			filter_port imap2 dport 143 3
-			filter_port imap3 dport 220 3
-			filter_port imaps dport 993 3
-			filter_port pop2 dport 109 3
-			filter_port pop3 dport 110 3
-			filter_port pop3s dport 995 3
+			filter_port smtp     dport 25   3
+			filter_port ssmtp    dport 465  3
+			filter_port imap2    dport 143  3
+			filter_port imap3    dport 220  3
+			filter_port imaps    dport 993  3
+			filter_port pop2     dport 109  3
+			filter_port pop3     dport 110  3
+			filter_port pop3s    dport 995  3
 
 			## Peercast:
 			filter_port peercast dport 7144 4
 			filter_port peercast sport 7144 4
+			filter_port peercast dport 7145 4
+			filter_port peercast sport 7145 4
+			## Dialect:
+			filter_port peercast dport 7900 4
+			filter_port peercast sport 7900 4
+			## Dialect's remote port:
+			filter_port peercast dport 7100 4
+			filter_port peercast sport 7100 4
 
 			### Interactive:
 
 			## apparently, one "could tell ssh from scp; scp sets the IP diffserv flags to indicate bulk traffic"
 			## but i don't know how to do this.  And what about rsync?
-			filter_port ssh sport 22 4
-			filter_port ssh dport 22 4
+			filter_port ssh      sport 22   4
+			filter_port ssh      dport 22   4
 
-			## Vnc:
-			for VNCPORT in `seq 5900 5999`
+			## Vnc-http, Vnc, and X
+			for VNCPORT in `seq 5800 5899` `seq 5900 5999` `seq 6000 6010`
 			do
 				filter_port vnc dport $VNCPORT 4
 				filter_port vnc sport $VNCPORT 4
 			done
 
+			## Realplay
+			filter_port realplay dport 554 5
+			filter_port realplay sport 554 5
+
 			## Fast websurfing:
-			filter_port http dport 80 5
-			filter_port https dport 443 5
+			filter_port http   dport 80   5
+			filter_port https  dport 443  5
 			## Lower priority webserver:
-			filter_port http sport 80 6
-			filter_port https sport 443 6
+			filter_port http   sport 80   6
+			filter_port https  sport 443  6
+
+			## DNS:
+			filter_port domain dport 53   6
+			filter_port domain sport 53   6
 
 			## CVS:
-			filter_port cvs dport 2401 7
-			filter_port cvs sport 2401 7
+			filter_port cvs    dport 2401 7
+			filter_port cvs    sport 2401 7
 
 			## And all the smeggin rest:
 			## I gave up on socks line 217 of /etc/services, resume there?  I don't really know which ones are needed.  rsync might be preferably batched.  irc probably needs higher priority, unless it's being used for d/l'ing!
-			##          ftp telnet dns? gopher finger hostnames rtelnet sftp nntp ntp! snmp irc ldap snpp talk ntalk rsync ftps ftps-data telnets ircs socks
-			for PORT in 21  23     53   70     79     101       107     115  119  123  161  194 389  444  517  518   873   990  989       992     994  1080
+			##          ftp telnet gopher finger hostnames rtelnet sftp nntp ntp! snmp irc ldap snpp talk ntalk rsync ftps ftps-data telnets ircs socks
+			for PORT in 21  23     70     79     101       107     115  119  123  161  194 389  444  517  518   873   990  989       992     994  1080
 			do
 				filter_port batch$PORT sport $PORT 7
 				filter_port batch$PORT dport $PORT 7
@@ -159,6 +210,7 @@ case "$1" in
 			## It takes 5-10 seconds for the failback to happen, but it works :-)
 			# /usr/sbin/arpspoof -i "$DEV" 192.168.168.1 >/dev/null 2>&1 &
 			# echo $! >/var/run/shapedsl.arpspoof.pid
+			echo "startified"
 		;;
 
 		stop)
