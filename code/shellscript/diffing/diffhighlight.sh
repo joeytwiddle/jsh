855fa5a45c6471dc400191ca826283cd2a4b9a35
joey <joey>
2005-03-01 19:22:22 +0000

    *** empty log message ***

--- code/shellscript/diffing/diffhighlight.sh
+++ code/shellscript/diffing/diffhighlight.sh
@@ -1,6 +1,6 @@
 ## Originally for svn diffs.
 ## Added regexps for cvs diffs.
-highlight -bold "^\(+++\|---\|===\|Index\|@\|\*\*\*\|[0-9][0-9acd,]*\).*" magenta |
-highlight       "^[+>][ 	].*" green |
-highlight -bold "^[-<][ 	].*" red |
-highlight       "^[|\!][ 	].*" yellow
+highlight -bold "^\(+++\|---\|===\|Index\|@\|\*\*\*\|[0-9][0-9acd,]*$\).*" magenta |
+highlight       "^[+>].*" green |
+highlight -bold "^[-<].*" red |
+highlight       "^[|\!].*" yellow
