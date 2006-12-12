3f9867a4c6417422b8cdd9bfa7ec710fac418143
joey <joey>
2006-12-12 00:00:23 +0000

    using dlocate

--- code/shellscript/rpm/dpkgsize.sh
+++ code/shellscript/rpm/dpkgsize.sh
@@ -1,6 +1,9 @@
 if which dlocate > /dev/null 2>&1
 then
-	echo `dlocate -du "$@" | grep total | takecols 1`"	$@"
+	echo `
+		dlocate -du "$@" |
+		prepend_each_line "$*:	" | tee -a "$JPATH/logs/pkgdfiles.txt" | dropcols 1 | ## optionally save the filenames
+		grep total | takecols 1`"	$@"
 	exit
 fi
 
@@ -9,6 +12,7 @@ fi
 # My version, much slower but robust to missing files
 ## I had problems with unwanted printing of file lists after the size list but maybe that problem is elsewhere
 # FILES=`dlocate -L "$@" | while read Y; do
+## TODO: the list which is saved should include non-files; the du should include symlinks (but obviously not directories)
 FILES=`dpkg-L "$@" | while read Y; do
         if test -f "$Y"; then
                 echo "$Y"
@@ -17,7 +21,7 @@ done`
 if test "$FILES" = ""; then
 	echo "0	$@"
 else
-	echo "$FILES" >> $JPATH/logs/pkgdfiles.txt
+	echo "$FILES" | prepend_each_line "$*:	" >> $JPATH/logs/pkgdfiles.txt ## optionally save the filenames
 	DUSK=`du -sk $FILES`
 	# echo "$DUSK" > files-"$@".txt
 	PKGSIZE=`echo "$DUSK" | takecols 1 | awksum`
