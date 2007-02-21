6dde0a0190ebcb7ed7f11eeba1852960b49d1d14
joey <joey>
2007-02-21 14:09:33 +0000

    Added meters

--- code/shellscript/net/traffic_shaping_monitor.sh
+++ code/shellscript/net/traffic_shaping_monitor.sh
@@ -18,4 +18,35 @@ done | beforelast ";"
 
 INTERFACE=`ifonline`
 
-jwatchchanges -fine /sbin/tc -s qdisc ls dev $INTERFACE "|" trimempty "|" sed "\"$SEDSTR\"" | highlight '[^ ]*:'
+add_levels_to_tc_output () {
+	TMPFILE=/tmp/tc_output.tmp
+	/sbin/tc -s qdisc ls dev $INTERFACE | trimempty | sed "$SEDSTR" > "$TMPFILE"
+	TOTAL=`cat "$TMPFILE" | grep "^ *Sent " | tail -n 1 | beforefirst " bytes " | afterlast " "`
+	cat "$TMPFILE" |
+	while read ARG1 ARG2 REST
+	do
+		if [ "$ARG1" = Sent ]
+		then
+			# jshinfo "$ARG2 / $TOTAL"
+			PROP="$(((COLUMNS-4)*ARG2/TOTAL))"
+			BAR=""
+			for X in `seq 0 $PROP`
+			do BAR="$BAR""#"
+			done
+			echo " Sent $ARG2 $REST"
+			echo "[$BAR]"
+		else
+			if [ "$ARG1" = qdisc ]
+			then echo
+			fi
+			echo "$ARG1 $ARG2 $REST"
+		fi
+	done
+}
+
+add_levels_to_tc_output
+
+. importshfn jwatchchanges
+
+jwatchchanges -fine add_levels_to_tc_output | highlight '[^ ]*:'
+
