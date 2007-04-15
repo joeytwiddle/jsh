f686fa447acdd1774afee2d17013857f2dc87ae2
joey <joey>
2007-04-15 12:19:21 +0000

    *** empty log message ***

--- code/shellscript/other/getrandom.sh
+++ code/shellscript/other/getrandom.sh
@@ -8,12 +8,20 @@ RNDDEV=/dev/urandom
 if [ "$USE_RND_DEV" ] || ( which hexdump >/dev/null 2>&1 && [ -e "$RNDDEV" ] )
 then
 
-	## In this case RAND_MAX is 4294967296 (numbers range 0-(65536*2 = 2^32)-1)
-	cat "$RNDDEV" | headbytes 4 | hexdump -d | head -1 | dropcols 1 |
-	while read ORDER2 ORDER1
-	# do echo "$((ORDER2*65536+$ORDER1))" ## couldn't do it =/
-	do calc "$ORDER2*65536+$ORDER1"
-	done
+	RAND_MAX=4294967295
+	[ "$1" ] && START="$1" || START=0
+	[ "$2" ] && END="$2" || END=4294967295
+
+	NUM=`
+		## In this case RAND_MAX is 4294967296 (numbers range 0-(65536^2 = 2^32)-1)
+		cat "$RNDDEV" | headbytes 4 | hexdump -d | head -1 | dropcols 1 |
+		while read ORDER2 ORDER1
+		# do echo "$((ORDER2*65536+$ORDER1))" ## couldnt do it =/
+		do calc "$ORDER2*65536+$ORDER1"
+		done
+	`
+
+	calc "$NUM*($END+1-$START)/$RAND_MAX+$START"
 
 elif [ "$RANDOM" ]
 then
