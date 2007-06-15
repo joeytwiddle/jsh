326884adc41ddc8d5b4cd1df15f3af9a28c39d5f
joey <joey>
2007-06-15 21:38:33 +0000

    *** empty log message ***

--- code/shellscript/fs/sortfilesbydate.sh
+++ code/shellscript/fs/sortfilesbydate.sh
@@ -1,1 +1,16 @@
-ls -rtd "$@"
+## This implementation cannot deal with more files that shell args can take:
+# if [ ! "$*" ]
+# ## We weren't given any args; so presumably we were streamed the files via our input stream...
+# ## WARNING: this infinitely recursive implementation will frag your system if input stream generates no args!:
+# # then withalldo sortfilesbydate
+# ## This one is safe:
+# then withalldo ls -rtd
+# else ls -rtd "$@"
+# fi
+
+## Stream-only version:
+while read FILE
+do find "$FILE" -maxdepth 0 -printf "%A@ %p\n"
+done |
+sort -n -k 1 |
+dropcols 1
