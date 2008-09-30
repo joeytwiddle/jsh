65bac3638ead02448e2a31024ea5d4044a09616d
joey <joey>
2008-09-30 20:31:53 +0000

    joey@bart:/home/joey/j/code/shellscript/fun/.

--- code/shellscript/fun/randomfiglet.sh
+++ code/shellscript/fun/randomfiglet.sh
@@ -10,13 +10,13 @@ if [ "$CAP" ]
 then TEXT=`echo "$TEXT" | tr 'qwertyuioplkjhgfdsazxcvbnm' 'QWERTYUIOPLKJHGFDSAZXCVBNM'`
 fi
 
+[ "$FIGLET_FONT_DIRS" ] || FIGLET_FONT_DIRS="/usr/share/figlet/ /stuff/mirrors/www.figlet.org/fonts/"
+
 GOODONES="banner big mini script shadow slant small smshadow smslant standard 3x5 acrobatic alligator alphabet avatar banner3-D banner3 banner4 basic bulbhead calgphy2 chunky colossal computer contessa contrast cosmic cricket cursive cyberlarge cybermedium cybersmall doh doom drpepper eftirobot eftitalic eftiwater epic fourtops fuzzy goofy gothic graffiti invita italic jazmine kban larry3d lcd letters linux lockergnome madrid maxfour nancyj nipples o8 ogre pawp pebbles pepper poison puffy rectangles relief rev roman rounded rowancap rozzo sblood script serifcap shadow short slant slide slscript small speed stampatello starwars stop straight tanja thick thin threepoint ticks ticksslant tinker-toy tombstone trek twopoint univers usaflag weird"
 # GOODONESRE=`echo "$GOODONES" | sed "s/^/(/;s/$/)/;s/ /|/g"`
 GOODONESRE='/('`echo "$GOODONES" | tr ' ' '|'`').flf$'
 FIGFONT=`
-	find /usr/share/figlet/ \
-	     /stuff/mirrors/www.figlet.org/fonts/ \
-	     -name "*.flf" |
+	find $FIGLET_FONT_DIRS -name "*.flf" |
 	egrep "$GOODONESRE" |
 	chooserandomline
 `
@@ -26,5 +26,5 @@ SHORTFIGFONT=`echo "$FIGFONT" | sed 's+^.*/++;s+\.flf$++'`
 [ "$COLUMNS" ] || COLUMNS=80
 
 echo
-/usr/bin/figlet -w "$COLUMNS" -f "$FIGFONT" "$TEXT"
+figlet -w "$COLUMNS" -f "$FIGFONT" "$TEXT"
 echo
