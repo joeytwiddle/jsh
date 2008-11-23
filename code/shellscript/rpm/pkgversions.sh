08cc13c2118aa6f73f34fd4c03779b746056be0a
joey <joey>
2008-11-23 14:08:17 +0000

    rewrite, better

--- code/shellscript/rpm/pkgversions.sh
+++ code/shellscript/rpm/pkgversions.sh
@@ -2,16 +2,47 @@
 # jsh-ext-depends: sed apt-cache
 # jsh-depends: cursecyan cursenorm drop tostring
 
+# e.g.: pkgversions `findpkg xserver | striptermchars | takecols 2`
+# bug: This does not actually list packages which are not installed, that could get huge.
+#      This is the all versions of all the installed packages matching *$1*
+if [ "$1" = "-all" ]
+then
+	shift
+	findpkg "$1" | striptermchars | takecols 2 | withalldo pkgversions
+	exit
+fi
+
 ## TODO: Change "[Selected]" to "[Installed]" to be accurate, but also check for dependencies on it by other scripts!
 ## Doh!  See also: apt-show-versions
 ## See also: dpkg --print-avail
 
-for X in "$@"
+for PKG in "$@"
 do
+
+	CURRENT_VERSION=`dpkg -l | grep "^..  $PKG " | takecols 3`
+	CURRENT_VERSION_RE=`toregexp "$CURRENT_VERSION"`
+	# echo "CURRENT_VERSION=$CURRENT_VERSION"
+
+	# apt-cache show "$PKG" | grep "^Version: " |
+
+	apt-cache showpkg "$PKG" |
+	grep "/var/lib/\(apt\|dpkg\)/" |
+	grep -v "^[ 	]*File: " |
+	sed 's+/var/lib/dpkg/status+installed+' |
+	sed 's+/var/lib/apt/lists/++' |
+	sed 's+\(dists_\|debian_\|_binary-i386_Packages\)++g' |
+
+	( [ "$1" != "$*" ] && prepend_each_line "$PKG=" || cat ) |
+
+	( [ "$CURRENT_VERSION" ] && highlight "$CURRENT_VERSION_RE" || cat )
+
 	if [ "$1" != "$*" ]
-	then echo "$X:"
+	then echo # ; echo "$PKG:"
 	fi
-	apt-cache showpkg "$X" 2>/dev/null |
+
+	continue
+
+	apt-cache showpkg "$PKG" 2>/dev/null |
 	drop 2 |
 	tostring "" |
 	# Why doesn't this line work?!
@@ -21,5 +52,6 @@ do
 	# Following two equivalent:
 	sed 's+/var/lib/apt/lists/++g'
 	# sed 's+([^)]*/\([^)]*\))+(\1)+g'
+
 done
 
