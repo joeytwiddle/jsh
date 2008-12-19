e9f1dd1c67279da38d65f5c3371a498ddfc5d7ed
joey <joey>
2008-12-19 18:42:27 +0000

    compresses output of identical files

--- code/shellscript/file/diffdirs.sh
+++ code/shellscript/file/diffdirs.sh
@@ -17,7 +17,27 @@ DIRB="$2"
 findfiles () {
 	cd "$1"
 	# find . -type f
-	find . -type f -or -type l
+	find . -type f -or -type l | sed 's+^\./++' | sort
+}
+
+IDCNT=0
+
+function report() {
+	if [ "$IDCNT" -gt 0 ]
+	then
+		[ "$IDCNT" -gt 1 ] && /bin/echo -n " [$IDCNT files]"
+		/bin/echo
+		IDCNT=0
+	fi
+	/bin/echo "$@"
+}
+
+function identical() {
+	if [ "$IDCNT" = 0 ]
+	then /bin/echo -n "Identical:" "$@"
+	else /bin/echo -n "" "$@"
+	fi
+	IDCNT=$((IDCNT+1))
 }
 
 (
@@ -32,29 +52,40 @@ do
 
 	if [ ! -f "$DIRA/$FILE" ] && [ -f "$DIRB/$FILE" ] ## Second check is in case both are broken symlinks, although TODO: should really check targets are the same
 	then
-		echo "`cursegreen`Only in $DIRB/: $FILE`cursenorm`"
+		report "`cursegreen`Only in $DIRB/: $FILE`cursenorm`"
 	elif [ ! -f "$DIRB/$FILE" ] && [ -f "$DIRA/$FILE" ] ## Second check is in case both are broken symlinks, although TODO: should really check targets are the same
 	then
-		echo "`cursered;cursebold`Only in $DIRA/: $FILE`cursenorm`"
+		report "`cursered;cursebold`Only in $DIRA/: $FILE`cursenorm`"
 	else
 		# if cmp "$DIRA/$FILE" "$DIRB/$FILE" > /dev/null
-		## These are faster alternatives:
-		if [ "`filesize "$DIRA/$FILE"`" = "`filesize "$DIRB/$FILE"`" ] ## Note: this detects a symlink to an identical file as different!
+		## These are faster alternatives, but not as thorough:
+		if [[ $(filesize "$DIRA/$FILE") = $(filesize "$DIRB/$FILE") ]] ## Note: this detects a symlink to an identical file as different!
 		# if test "`qkcksum "$DIRA/$FILE" | takecols 1 2`" = "`qkcksum "$DIRB/$FILE" | takecols 1 2`" ## only faster for bigger files!
 		## This was no good, because the filenames are different, and are echoed back!: if [ "`qkcksum \"$DIRA/$FILE\"`" = "`qkcksum \"$DIRB/$FILE\"`" ]
 		## This doesn't work on files with spaces: # if test "`qkcksum \`realpath "$DIRA/$FILE"\` | takecols 1 2`" = "`qkcksum \`realpath "$DIRB/$FILE"\` | takecols 1 2`" ## only faster for bigger files!
 		# then noop
-		then echo "Identical: $FILE"
-		# else echo "Differ: $FILE"
+		then
+			# /bin/echo -e -n "\rIdentical: $FILE   "
+			identical "$FILE"
+		# else report "Differ: $FILE"
 		else
-			echo "`curseyellow`Differ: diff \"$DIRA/$FILE\" \"$DIRB/$FILE\"`cursenorm`"
+			# report "`curseyellow`Differ: diff \"$DIRA/$FILE\" \"$DIRB/$FILE\"`cursenorm`"
+			DIFFSUMMARY=`NOEXEC=1 IKNOWIDONTHAVEATTY=1 diffsummary "$DIRA/$FILE" "$DIRB/$FILE"`
+			report "`curseyellow`Differ: diff $DIFFSUMMARY`cursenorm`"
 			if [ "$SHOWDIFFSWITH" ]
 			then
-				echo "Here are the differences:"
-				$SHOWDIFFSWITH "$DIRA/$FILE" "$DIRB/$FILE"
-				echo
+				report "Here are the differences:"
+				report $SHOWDIFFSWITH "$DIRA/$FILE" "$DIRB/$FILE"
+				report echo
 			fi
 		fi
 	fi
 
 done
+
+## Dammit we lost our value of IDCNT!
+# if [ "$IDCNT" -gt 0 ]
+# then echo
+# fi
+echo
+
