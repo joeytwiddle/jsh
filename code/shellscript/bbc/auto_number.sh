929950bf3ead470897d4469fdf0f44e57ac283fb
joey <joey>
2009-06-12 21:47:02 +0000

    *** empty log message ***

--- code/shellscript/bbc/auto_number.sh
+++ code/shellscript/bbc/auto_number.sh
@@ -1,25 +1,49 @@
+## BUG TODO: '\'s get swallowed!  '\\'s probably get converted to '\'s.
+## TODO: REM lines tend to get their spaces squeezed.  This is not really desirable.
+## TODO: Renumbering can break GOTO and GOSUB calls.  We should warn when they are present!  Or we could try to fix them...
+## TODO: Rename this script "bbcbasicrenumber", or factor out the common (non-BBC) stuff, and split into two scripts...
+
 if [ "$1" = -numall ]
 then shift ; NUMBER_ALL=true
 fi
 
-LINENUM=10000
+if [ "$1" = -run ]
+then shift ; RUN=true
+fi
+
+# LINENUM=10000
+# LINENUM=10000
+# LINENUM=00001 ## pretty-printing pre-padding with '0's is lost!
+# LINENUM=10001
+LINENUM=10010
 
 cat "$@" |
 
+## First of all, mark all lines where we will want a line number - place a '.' at the start.
 if [ "$NUMBER_ALL" ]
 then
 	# sed 's+.*+. \0+' ## Add .s to all lines
-	sed 's/^\([ ]*[[:digit:]][[:digit:]]*\( \|\)\|^\)/./' ## Add .s to all lines, stripping line # from those which have it
-else sed 's/^[ ]*[[:digit:]][[:digit:]]*\( \|\)/./' ## Add .s to lines with line numbers (and drop line number)
+	sed 's/^\([ ]*[[:digit:]][[:digit:]]*\( \|\)\|^\)/./' ## Add '.' to each line, stripping numbers from any lines which have them.
+else sed 's/^[ ]*[[:digit:]][[:digit:]]*\( \|\)/./' ## Add '.' to each line with a line number, and strip the line number.
 fi |
 
+## Now add a line number to EVERY line!
 while read LINE
 do
-	echo "$LINENUM $LINE"
-	LINENUM=`expr "$LINENUM" + 20`
+	echo "  $LINENUM $LINE"
+	# LINENUM=`expr "$LINENUM" + 20`
+	# LINENUM=`expr "$LINENUM" + 1`
+	LINENUM=`expr "$LINENUM" + 10`
 done |
 
-sed 's/^[ ]*[[:digit:]][[:digit:]]* \($\|[^.]\)/\1/' | ## Drop lines with line number but no .
-sed 's/^\([ ]*[[:digit:]][[:digit:]]* \)\./\1/' | ## Drop .s from lines with .s
+## Now remove unwanted line numbers - when no '.' is present:
+sed 's/^[ ]*[[:digit:]][[:digit:]]* \($\|[^.]\)/\1/' | ## Drop lines with line number but no '.'  (Aka drop line numbers when not wanted - no '.' was present.)
+
+## And finally remove the '.' markers, now we are finished with them.
+sed 's/^\([ ]*[[:digit:]][[:digit:]]* \)\./\1/' | ## Drop '.' from every line with '.' (cleanup, leave just the line number, or nothing).
 # sed 's/^\([ ]*[[:digit:]][[:digit:]]*\) $/\1/' |
+
 cat
+
+[ "$RUN" ] && echo "RUN"
+
