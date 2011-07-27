889a3b87b109a704e2b5e8c662d66b72115acf25
joey <joey>
2011-07-27 21:21:33 +0000

    check out a temporary version of <filename> and do a vimdiff...

---
+++ code/shellscript/versioning/gitvimdiff.sh
@@ -0,0 +1,37 @@
+if [ "$1" = "" ] || [ "$2" = "" ] || [ "$1" = --help ]
+then
+	echo
+	echo "gitvimdiff <commit> <filename>"
+	echo
+	echo "gitvimdiff -n <num> <filename>"
+	echo
+	echo "  will check out a temporary version of <filename> and do a vimdiff..."
+	echo
+	echo "  You may set \$DIFFCOM to use a different program to compare the files."
+	echo
+	echo "  The -n form goes <num> revisions back, so you don't have to lookup the commit ids."
+	echo
+	exit 1
+fi
+
+if [ "$1" = -n ]
+then
+	shift
+	revisionIndex="$1"
+	commitID=`git log | grep "^commit " | head -n "$revisionIndex" | tail -n 1 | cut -d ' ' -f 2`
+else
+	commitID="$1"
+fi
+
+filename="$2"
+
+olderFile="$filename"."$commitID"
+
+verbosely git diff "$commitID" "$filename" |
+# cat | diffhighlight
+patch -R -o "$olderFile" "$filename"
+
+[ "$DIFFCOM" ] || DIFFCOM="vimdiff"
+
+"$DIFFCOM" "$olderFile" "$filename"
+
