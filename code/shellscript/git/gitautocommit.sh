8906a8753299242f843a1833337f5569667f2c74
joey <joey>
2011-07-28 08:49:19 +0000

    git

--- code/shellscript/git/gitautocommit.sh
+++ code/shellscript/git/gitautocommit.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+if [ "$1" = --help ]
+then
+	echo
+	echo "gitautocommit <git_options>"
+	echo
+	echo "  will stage and commit all changed files under the current directory."
+	echo
+	echo "  Without the -m option, you will see a preview of the changes, which won't be"
+	echo "  committed if you do not add a message."
+	echo
+	echo "  Files to always ignore are currently specified by regexps within this script."
+	echo
+	exit 1
+fi
+
 if [ "$1" = -m ]
 then COMMIT_MESSAGE="$2" ; shift ; shift
 fi
@@ -7,6 +22,10 @@ fi
 git status "$@" | grep "^#[ 	]*modified:[ 	]*" | dropcols 1 2 |
 withalldo -r verbosely git add
 
+pipeboth=cat
+[ "$VERBOSE" ] && pipeboth=pipeboth
+
+[ "$VERBOSE" ] && jshinfo "Adding:"
 # git status "$@" | fromline "^# Untracked files:$" | grep "^#	" | dropcols 1 |
 ## This was insatisfactory, listing the top missing folder and nothing below it.  So let's focus on files...
 find . -type f |
@@ -18,10 +37,15 @@ grep -v "/[.]git/" | ## git itself!
 grep -v "/build/.dependency-info/" | ## Build files (Eclipse?)
 grep -v "/[.]gqview/" | ## Was randomly in my fuse-j-sh project
 grep -v "\.recovered\.[0-9]*$" |
+$pipeboth |
 withalldo -r verbosely git add
+echo
 
+[ "$VERBOSE" ] && jshinfo "Removing:"
 git status "$@" | grep "^#[ 	]*deleted:[ 	]*" | dropcols 1 2 |
+$pipeboth |
 withalldo -r verbosely git rm
+echo
 
 # verbosely git commit -m "`geekdate`" "$@"
 if [ "$COMMIT_MESSAGE" ]
---
+++ code/shellscript/git/gitdiff.sh
@@ -0,0 +1,1 @@
+git diff "$@" | diffhighlight | more
