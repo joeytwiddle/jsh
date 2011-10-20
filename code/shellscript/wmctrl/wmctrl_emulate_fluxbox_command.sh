6a2c7242f694d966cc6c5022a47000552a4cadc2
joey <joey>
2011-10-20 17:26:47 +0000

    dropped bash specifics, created POSIX sh version

--- code/shellscript/wmctrl/wmctrl_emulate_fluxbox_command.sh
+++ code/shellscript/wmctrl/wmctrl_emulate_fluxbox_command.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/dash
 
-if [[ -z "$1" ]]; then
-	exit 1
+if [ -z "$1" ]
+then exit 1
 fi
 
 ## Multiple switch requests have a tendency to undo each other if performed too
@@ -9,14 +9,14 @@ fi
 ## BUG: the process order is likely to be non-deterministic for 3 or more requests.
 lockfile=/tmp/wmctrl_emulate_fluxbox_command.$USER.lock
 
-# lockfile -1 -r 99 -l 15 "$lockfile"
+lockfile -1 -r 99 -l 15 "$lockfile"
 
 ## Might respond slightly faster.  Might also be buggy.
-n=0
-while [[ -f "$lockfile" && $n < 99 ]]
-do sleep 0.1 ; n=$((n+1))
-done
-touch "$lockfile"
+# n=0
+# while [ -f "$lockfile" ] && [ $n -lt 99 ]
+# do sleep 0.1 ; n=$((n+1))
+# done
+# touch "$lockfile"
 
 case "$1" in
 	":Workspace")
@@ -30,16 +30,16 @@ case "$1" in
 		;;
 	":NextWorkspace")
 		howFar="$2"
-		[[ -z $howFar ]] && howFar=1
+		[ -z $howFar ] && howFar=1
 		wsps=$(wmctrl -d | grep -A"$howFar" '[[:digit:]]\+ \+\*')
 		count=$(echo "$wsps" | wc -l)
 		expected=$((howFar+1))
-		if [[ $count < $expected ]]
+		if [ $count -lt $expected ]
 		then
 			## We are probably near the end of the list, so we need to look from the top
 			missing=$((expected-count))
 			target_workspace=$(wmctrl -d | head -n $missing | tail -n 1 | cut -f1 -d ' ')
-		elif [[ $count = $expected ]]
+		elif [ $count = $expected ]
 		then
 			target_workspace=$(echo "$wsps" | tail -n 1 | cut -f1 -d ' ')
 		else
@@ -49,16 +49,16 @@ case "$1" in
 		;;
 	":PrevWorkspace")
 		howFar="$2"
-		[[ -z $howFar ]] && howFar=1
+		[ -z $howFar ] && howFar=1
 		wsps=$(wmctrl -d | grep -B"$howFar" '[[:digit:]]\+ \+\*')
 		count=$(echo "$wsps" | wc -l)
 		expected=$((howFar+1))
-		if [[ $count < $expected ]]
+		if [ $count -lt $expected ]
 		then
 			## We are probably near the beginning of the list, so we need to look from the bottom
 			missing=$((expected-count))
 			target_workspace=$(wmctrl -d | tail -n $missing | head -n 1 | cut -f1 -d ' ')
-		elif [[ $count = $expected ]]
+		elif [ $count = $expected ]
 		then
 			target_workspace=$(echo "$wsps" | head -n 1 | cut -f1 -d ' ')
 		else
@@ -72,8 +72,8 @@ case "$1" in
 		;;
 esac
 
-[[ ! -z $target_workspace ]] && wmctrl -s $target_workspace
+[ ! -z $target_workspace ] && wmctrl -s $target_workspace
 
-sleep 0.1
+# sleep 0.1
 rm -f "$lockfile"
 
