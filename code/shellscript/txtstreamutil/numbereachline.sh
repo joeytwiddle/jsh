a33ccca48f3b835dd854427af3625952639affc5
joey <joey>
2011-11-04 20:01:09 +0000

    joey@pea  2009/02/10 18:27 GMT

--- code/shellscript/txtstreamutil/numbereachline.sh
+++ code/shellscript/txtstreamutil/numbereachline.sh
@@ -1,8 +1,13 @@
+if [ "$PADDING" ]
+then PADSTR="%0$PADDING"i
+else PADSTR="%03i"
+fi
+
 N=0
 escapeslash | ## since echo "$LINE" will lose \s if not doubled.  BUG: what else might echo "$LINE" lose?
 while read LINE
 do
 	# printf "%s\n" "$N	$LINE"
-	printf "%03i\t%s\n" "$N" "$LINE"
+	printf "$PADSTR\t%s\n" "$N" "$LINE"
 	N=$(($N+1))
 done
