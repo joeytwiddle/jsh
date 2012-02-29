7684e417ef27ae2cc21597e5ab773002899e2401
joey <joey>
2012-02-29 02:45:31 +0000

    good

--- code/shellscript/versioning/cvscommitall.sh
+++ code/shellscript/versioning/cvscommitall.sh
@@ -13,12 +13,23 @@ then
 
 cat << !
 
-cvscommit [ <filename> | -auto ] <filepatterns>..
+[ DO=1 ] cvscommitall [ <base_filename> | -auto ] <files>..
 
-  Will first preview, then commit each of the files in <filepatterns> to cvs under
-  the name <filename>.
+  Will batch commit each of the listed files to CVS under the base filename.
+
+  Without DO=1 it actually only prints a preview of what would be done.
 
   -auto will determine the filename by stripping any trailing version number.
+
+  Currently prompts the user to choose between date or given order.
+
+    TODO: This should not mess with stdout!  Since a cmdlist comes after.
+    TODO: Options -d -g to force one of the other could avoid that step.
+    TODO: Auto is dodgy, check the code, it *could* commit different filenames!
+
+  Example:
+
+    cvscommitall cool.c old/cool.c.myversion*
 
 !
 
@@ -27,26 +38,34 @@ exit 0
 fi
 
 FILENAME="$1"
-# shift
-
-# for X
-# do
-	# echo "$X"
-# done
 
 FILES_AS_ORDERED=` echolines "$@" `
 FILES_BY_DATE=` echolines "$@" | sortfilesbydate `
 
-echo "Files in date order:"
+
+
+# DO=verbosely
+# DO=jshinfo
+# DO=echo
+
+if [ "$DO" = 1 ]
+then DO=verbosely
+else DO=echo ; jshinfo "Trial run - will not act!"
+fi
+
+
+
 echo "$FILES_BY_DATE"
+# Print after because long lists will hide the top
+jshinfo "This list is in (d)ate order."
 
 if [ "$FILES_AS_ORDERED" = "$FILES_BY_DATE" ]
 then
-	echo "  Date order and given order match."
+	jshinfo "Date order and given order match."
 else
 	[ "$DEBUG" ] && echo "$FILES_AS_ORDERED" > /tmp/x
 	[ "$DEBUG" ] && echo "$FILES_BY_DATE" > /tmp/y
-	echo "  Date order and given order DO NOT match."
+	jshwarn "Date order and given order DO NOT match."
 fi
 
 # echo "Do you wish me to commit in this order under the filename '$FILENAME'?"
@@ -69,16 +88,13 @@ fi
 
 if [ -f "$FILENAME" ]
 then
-	jshwarn "$FILENAME exists but will be overwritten.  If it does not match the latest of those to be committed, you may with to back it up (or make it part of the pattern)."
+	jshwarn "$FILENAME exists but would be overwritten.  If it does not match the latest of those to be committed, you may with to back it up (or make it part of the pattern)."
+	# TODO: We should just do this automatically.  Move it to a tempfile at the start, and back for one final commit, no - just leave it uncommited.
 fi
 
 echo "Do you wish me to commit them in (d)ate order or (g)iven order under the filename '$FILENAME'?"
 
 read ANS
-
-# DO=verbosely
-# DO=jshinfo
-DO=echo
 
 if [ "$ANS" = d ]
 then echo "$FILES_BY_DATE"
@@ -104,4 +120,6 @@ do
 	$DO cvs edit "$TOCOMMIT"
 	# $DO del "$X" "$TOCOMMIT"
 done
+
+[ "$DO" = echo ] && jshinfo "Call me again with DO=1"
 
