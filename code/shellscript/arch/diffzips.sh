f098ef46d1f6e3bdb999f94fcab23ea27b1932c8
joey <joey>
2012-03-09 19:20:58 +0000

    joey@hwi -20 +26 2012/01/21 10:52 GMT

--- code/shellscript/arch/diffzips.sh
+++ code/shellscript/arch/diffzips.sh
@@ -1,2 +1,3 @@
+## No need to unpack them, we can check the file hashes in the index.
 diffcoms -vimdiff "unzip -v \"$1\" | grep -v '\.svn'" "unzip -v \"$2\" | grep -v '\.svn'"
 # diffcoms -color "unzip -v \"$1\" | grep -v '\.svn'" "unzip -v \"$2\" | grep -v '\.svn'"
