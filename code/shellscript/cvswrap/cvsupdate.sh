28fd348e28c51c3d7697c86d3ba97d32a839fc47
joey <joey>
2012-05-05 17:51:37 +0000

    from pod, compressed and with annotated output

--- code/shellscript/cvswrap/cvsupdate.sh
+++ code/shellscript/cvswrap/cvsupdate.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
-cvs -z 9 -q update "$@" |
-	grep -v "^\? " ## BUG: returns non-zero exit code, if the update had nothing to output
-# cvs update "$@" 2>&1 |
-	# grep -v "^? " |
-	# grep -v "^cvs update: Updating " |
-	# grep -v "^cvs server: Updating "
 
-## BUG: With args "-AdP <dir>" cvsedit doesn't target the target dir, but cwd!
-cvsedit "$@"
+# cvs update "$@" 2>&1 |
+# grep -v "^? " |
+# grep -v "^cvs update: Updating " |
+# grep -v "^cvs server: Updating "
+
+cvs -z 9 -q update "$@" |
+grep -v "^\? " | ## BUG: returns non-zero exit code, if the update had nothing to output
+sed 's+^M +M locally Modified +
+     s+^U +U          Updated +
+     s+^P +P          Patched +
+     s+^C +C       Conflicts! +
+'
+
+## BUG: With args "-AdP <dir>" cvsedit "$@" doesn't target the target dir, but cwd!
+cvsedit # "$@"
 
