beebce1bde9142ce56bc9e04b631c98e5bf7de00
joey <joey>
2012-07-29 20:41:12 +0000

    joey@hwi  2012/07/29 21:34 BST

--- code/shellscript/git/gitdiff.sh
+++ code/shellscript/git/gitdiff.sh
@@ -3,3 +3,7 @@
 ## To see both, ... ?
 git diff "$@" | diffhighlight | more
 ## Alternatively, diffs may be colored by setting [color "diff"] section of .gitconfig
+
+## Interestingly, git itself calls 'pager' which points to /bin/less on my
+## system, and shows colours just fine.  But if we call pager instead of more
+## above, the colour escape codes do not come out nicely!
