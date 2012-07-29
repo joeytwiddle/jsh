51d1cae2eae647cc33f5967ebbeae5df5302cc0e
joey <joey>
2012-07-29 20:50:03 +0000

    joey@hwi  2012/06/11 16:52 BST

--- code/shellscript/media/mplayer.sh
+++ code/shellscript/media/mplayer.sh
@@ -9,11 +9,6 @@
 	# madplay "$@"; exit
 # fi
 
-## See also: new versions of mplayer have a -stop-xscreensaver option
-## consider: could killall -STOP it, then unhalt it at end.
-killall xscreensaver && XSCREENSAVER_WAS_RUNNING=true
-## Despite Debian accepting the -stop-xscreensaver option now, xscreensaver still appears!
-
 # OPTS="-vo gl,xv,x11" ## under gentoo this selects x11 which is slow.  gl sucks for me under compiz
 OPTS="-vo x11" ## No acceleration, always works.  Lets me adjust contrast.  Works under compiz.
 # OPTS="-vo xv" ## I thought this allowed us to adjust contrast but it doesn't right now.
@@ -52,13 +47,14 @@ do
 	esac
 done
 
+[ "$FAST" ] || FAST=0
 ## Some highly compressed videos can be too slow to fully decompress!
 # FAST=1
 ## Mplayer recommends:
 # [ "$FAST" ] && OPTS="$OPTS -ao sdl -vfm ffmpeg -lavdopts lowres=1:fast:skiploopfilter=all"
 ## But I found this was enough for me and not so bad quality (autoq/sync may not be needed):
 ## G's A:
-[ "$FAST" = 0 ] && OPTS="$OPTS -vfm ffmpeg -autoq 5 -autosync 5"
+# [ "$FAST" = 1 ] && OPTS="$OPTS -vfm ffmpeg -autoq 5 -autosync 5"
 ## KMD under Compiz:
 # [ "$FAST" ] && OPTS="$OPTS -vfm ffmpeg -autoq 5 -autosync 5 -framedrop -hardframedrop"
 ## BSG S3:
@@ -72,16 +68,26 @@ done
 # [ "$FAST" ] && OPTS="$OPTS -vo x11 -vfm ffmpeg -autoq 5 -autosync 5 -framedrop"
 ## Others (Sunny highly compress h264):
 # [ "$FAST" ] && OPTS="$OPTS -nobps -ni -forceidx -mc 0"
-[ "$FAST" = 1 ] && OPTS="$OPTS -vfm ffmpeg -lavdopts lowres=0:fast:skiploopfilter=all -autoq 5 -autosync 5"
+
+[ "$FAST" -gt 0 ] && OPTS="$OPTS -vfm ffmpeg -lavdopts lowres=0:fast:skiploopfilter=all -autoq 5 -autosync 5"
 ## Note that -framedrop can be undesirable if the video is a highly-compressed
 ## h264 - it will cause us to frequently lose large chunks!
+
 ## A heavy flv from YouTube (crashes on HTLGI video!):
-[ "$FAST" = 2 ] && OPTS="$OPTS -vfm ffmpeg -lavdopts lowres=0:fast:skiploopfilter=all -autoq 5 -autosync 5 -framedrop -nocorrect-pts"
-## lowres=1 crashes on some videos, on others it makes decoding faster but with lower image quality
-[ "$FAST" = 3 ] && OPTS="$OPTS -vfm ffmpeg -lavdopts lowres=1:fast:skiploopfilter=all -autoq 5 -autosync 5 -framedrop -nocorrect-pts -nobps -ni -mc 0 -vo sdl"
+[ "$FAST" -gt 1 ] && OPTS="$OPTS -vfm ffmpeg -lavdopts lowres=0:fast:skiploopfilter=all -autoq 5 -autosync 5 -framedrop -nocorrect-pts"
+
 ## On pod -ao sdl was failing to keep up (clipping and reporting underruns, P&R) whilst -ao alsa was fine.  Leaving -ao sdl until desperate.
 ## This may be wrong for hwi - hwi's default alsa is significantly slower than sdl because it duplexes.
-[ "$FAST" -gt 9 ] && OPTS="$OPTS -ao sdl"
+[ "$FAST" -gt 4 ] && OPTS="$OPTS -ao sdl"
+
+## -vo sdl is the sort of thing you can do yourself, if you remember to.
+[ "$FAST" -gt 5 ] && OPTS="$OPTS -vo sdl"
+[ "$FAST" -gt 5 ] && REMEMBER_WINDOW_POSITIONS=true   # If sdl drops X resolution, window positions may be lost!
+
+## lowres=1 crashes on many videos, on just a few it makes decoding faster but with lower image quality
+[ "$FAST" -gt 8 ] && OPTS="$OPTS -vfm ffmpeg -lavdopts lowres=1:fast:skiploopfilter=all -autoq 5 -autosync 5 -framedrop -nocorrect-pts -nobps -ni -mc 0 -vo sdl"
+
+
 
 ## AFAIK VNC only works with the x11 vo:
 if [ "$VNCDESKTOP" = "X" ]
@@ -91,7 +97,8 @@ fi
 
 ## Graphic equalizer
 [ "$EQ" ] || EQ="none"
-[ "$EQ" = wireless ]   && OPTS="$OPTS -af equalizer=0:0:1:1:2:2:3:3:4:4" ## Louder middle and treble
+# [ "$EQ" = wireless ]   && OPTS="$OPTS -af equalizer=0:0:1:1:2:2:3:3:4:4" ## Louder middle and treble (can cause crackle)
+[ "$EQ" = wireless ]   && OPTS="$OPTS -af equalizer=-4:-4:-3:-3:-2:-2:-1:-1:0:0" ## Quieter bass
 [ "$EQ" = headphones ] && OPTS="$OPTS -af equalizer=4:3:2:1:1:0:0:0:0:0" ## Louder bass
 [ "$EQ" = speakers ]   && OPTS="$OPTS -af equalizer=2:3:3:2:1:0:0:0:0:0" ## Louder bass and middle
 
@@ -101,12 +108,23 @@ then
 	# OPTS="$OPTS -vf eq2=1.0:1.0:0.0:1.0:0.6:1.0:1.0"  ## Fix red gamma on hwi
 	# OPTS="$OPTS -vf eq2=1.0:1.0:0.0:1.0:0.7:1.0:1.0"  ## Fix red gamma on hwi
 	# OPTS="$OPTS -vf eq2=1.0:1.2:0.0:1.0:0.8:1.1:1.1"  ## Fix red gamma on hwi and increase contrast
-	OPTS="$OPTS -vf eq2=1.2:1.0:0.0:1.0:1.0:1.1:1.1"  ## Fix red gamma on hwi and increase gamma
+	# OPTS="$OPTS -vf eq2=1.2:1.0:0.0:1.0:1.0:1.1:1.1"  ## Fix red gamma on hwi and increase gamma
+	OPTS="$OPTS -vf eq2=1.3:1.0:0.05:1.0:0.7:1.0:1.1"  ## Fix red gamma on hwi, extra gamma and a little brightness
 	# OPTS="$OPTS -vo x11" ## keeps my x gamma fixes, but doesn't scale (don't use this and eq2!)
 	# OPTS="$OPTS -vo gl"  ## keeps x fixes, but a little blue just like eq2
 fi
 
+## See also: new versions of mplayer have a -stop-xscreensaver option
+## consider: could killall -STOP it, then unhalt it at end.
+killall xscreensaver && XSCREENSAVER_WAS_RUNNING=true
+## Despite Debian accepting the -stop-xscreensaver option now, xscreensaver still appears!
+
+[ "$REMEMBER_WINDOW_POSITIONS" ] && wmctrl_store_positions
+
 [ "$MPLAYER" ] || MPLAYER=mplayer
 verbosely unj $MPLAYER $OPTS "$@"
 
+[ "$REMEMBER_WINDOW_POSITIONS" ] && wmctrl_restore_positions
+
 [ "$XSCREENSAVER_WAS_RUNNING" ] && xscreensaver -no-splash &
+
