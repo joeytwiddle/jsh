02ab846a9c2a45058eb6a051fdf09285061b08bf
joey <joey>
2012-07-29 20:30:43 +0000

    joey@hwi  2012/06/19 22:30 BST

--- code/shellscript/git/gitdiff.sh
+++ code/shellscript/git/gitdiff.sh
@@ -2,3 +2,4 @@
 ## To see the staged differences, pass: --cached
 ## To see both, ... ?
 git diff "$@" | diffhighlight | more
+## Alternatively, diffs may be colored by setting [color "diff"] section of .gitconfig
