68e20aa119710bdf68f11545f66c289887887ad3
joey <joey>
2012-07-30 15:54:56 +0000

    joey@hwi  2012/07/27 15:36 BST

---
+++ code/shellscript/security/freshuser.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+cmd="`escapeargs "$@"`"
+[ -z "$cmd" ] && cmd="/bin/bash"
+
+## -x might use my zsh PS1 if we dont shebang /bin/bash at the top
+# . hwipromptforbash
+# export PS1
+# set -x
+
+# Simple convenience tool.  Type freshuser when you want to try something
+# without being your normal user.
+
+sudo useradd -m freshuser
+
+echo "Entering temporary account"
+# # TODO: export X session!  or start X session!
+# if [ -z "$*" ]
+# then
+	# sudo su - freshuser -c /bin/bash
+# else
+	# sudo su - freshuser -c "$*"
+# fi
+
+sudo su - freshuser -c "$cmd"
+
+sudo userdel freshuser
+sudo rm -rf /home/freshuser
+# sudo $JPATH/jsh del /home/freshuser
+
