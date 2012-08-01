b27b311dd1343d08e003d0ca89f184b330766280
joey <joey>
2012-08-01 08:39:33 +0000

    joey@hwi -0 +4 2012/07/30 17:38 BST

--- code/shellscript/apache/apachelogs.sh
+++ code/shellscript/apache/apachelogs.sh
@@ -4,6 +4,10 @@ tail -n 5 -f "$root"/var/log/apache2/*.log |
 
 sed -u 's+\(.*\)"\(.*\)"+\1{\2}+' |
 
+## Reduce the length of the lines a little bit
+## Hide date fields
+sed -u 's+\[[^]]*[0-9][0-9]:[0-9][0-9]:[0-9][0-9][^]]*\] ++' |
+
 highlight '{[^}]*}' magenta |
 highlight '\[[^]]*\]' grey |
 highlight '" \(200\|304\)\>' green |
