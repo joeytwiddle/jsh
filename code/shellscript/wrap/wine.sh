36072fe55c2c55cbc21fc37e77ada6fa3e049d3e
joey <joey>
2012-08-04 14:26:33 +0000

    joey@hwi -0 +9 2012/07/18 19:32 BST

--- code/shellscript/wrap/wine.sh
+++ code/shellscript/wrap/wine.sh
@@ -1,8 +1,17 @@
 #!/bin/sh
+
+## Wine used to crash due to my crazy fonts, so this disabled them temporarily
 # xset fp- unix/:7101 2>/dev/null
 # ( sleep 2m; xset fp+ unix/:7101 ) &
 ## This was confusing, when jwhich failed and returned "", we just called "@", which occasionally does run a working command under linux >.<
 # `jwhich wine` "$@"
+
+## Fullscreen wine apps (games, demos) often shrink the desktop, disturbing window positioning.
+## Not all of them restore the desktop after running either!
+## So we will store window positions before running, and restore afterwards.
 wmctrl_store_positions
 unj wine "$@"
+xrandr -s 1280x1024
+# wmctrl_restore_positions doesn't work in compiz
+findjob compiz >/dev/null ||
 wmctrl_restore_positions
