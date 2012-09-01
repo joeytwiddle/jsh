5fd23c264b009f2755b73393b455769586975c82
joey <joey>
2012-09-01 04:15:33 +0000

    maybe_do

--- code/shellscript/filetools/symlink_duplicates.sh
+++ code/shellscript/filetools/symlink_duplicates.sh
@@ -1,10 +1,13 @@
 if [ "$2" = "" ] || [ "$1" = --help ]
 then
 
-	echo "[ DO=1 ] symlink_duplicates <dir> <absolute_archive_dirs>..."
+	echo
+	echo "[ DO= | DO=1 ] symlink_duplicates <dir> <absolute_archive_dirs>..."
 	echo
 	echo "  For each file in <dir>, will try to find a copy of that file in one of the <archive_dirs>."
 	echo "  If one is found, the file in <dir> will be removed and replaced by a symlink to the copy from <archive_dirs>."
+	echo "  Assumes any duplicate file copy will have the same name, otherwise misses them."
+	echo
 	exit 0
 
 fi
@@ -21,9 +24,16 @@ do
 	fi
 done
 
+maybe_do() {
+	if [ -z "$DO" ]
+	then echo "Would do: $*"
+	else verbosely "$@"
+	fi
+}
+
 find "$DIR"/ -type f |
 
-catwithprogress |
+# catwithprogress |
 
 while read FILE
 do
@@ -37,16 +47,11 @@ do
 
 		if cmp "$FILE" "$POSSIBLE_TARGET_FILE" >/dev/null && [ ! "`realpath "$FILE"`" = "`realpath "$POSSIBLE_TARGET_FILE"`" ]
 		then
-			if [ -n "$DO" ]
-			then
-				verbosely rm "$FILE"
-				verbosely ln -s "$POSSIBLE_TARGET_FILE" "$FILE"
-			else
-				echo "Would:" rm "$FILE"
-				echo "Would:" ln -s "$POSSIBLE_TARGET_FILE" "$FILE"
-			fi
+			maybe_do rm -f "$FILE"
+			# maybe_do del "$FILE"
+			maybe_do ln -s "$POSSIBLE_TARGET_FILE" "$FILE"
 			# break
-			## What on earth is this for?  It didn't appear to prevent it multiple files from being linked!
+			## What on earth is this for?  It didn't appear to prevent multiple files from being linked!
 			while read DUMMY; do : ; done
 		fi
 
