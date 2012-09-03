4e656cf1ed25d6b2c630fbed5ee879c48650681f
joey <joey>
2012-09-03 17:41:25 +0000

    joey@hwi -8 +27 2012/09/03 01:32 BST

--- code/shellscript/autohighlight/highlight_words.sh
+++ code/shellscript/autohighlight/highlight_words.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
+## BUG: $COLBOLD has no affect in eterm, so all colors appear dark, but it
+## actually has lots of colors we could use!
+
 ## With /bin/sh we sometimes get:
 ##   sed: -e expression #1, char 27: unknown option to `s'
 
-
-TAIL=tail
-if [ "$1" = -rare ]
-then TAIL=head ; shift ## Often (but not always) one colour for 16 unique words.
+# This rather depends on the context: size and structure of the document.
+# A large number of colors filling a small document can be very distracting.
+if [ -z "$MAX_WORDS_TO_COLOR" ]
+then
+	MAX_WORDS_TO_COLOR=7
+	# MAX_WORDS_TO_COLOR=14
+	# MAX_WORDS_TO_COLOR=99
 fi
+
+## Do we want to highlight the most common/abundant words,
+TAIL=tail
+## or the least popular, more rare words?
+if [ "$1" = -rare ]
+then TAIL=head ; shift
+fi
+## This might produce one colour for 16 unique words, if it was not for
+## EXCLUDE_SINGLES below, which keeps only words with 2 or more occurrences.
 
 TMPFILE="/tmp/jsh.highlight_words.$$"
 cat "$@" > "$TMPFILE"
@@ -33,8 +48,9 @@ grep -v "^ *$" |
 countduplicates |
 sort -n -k 1 |
 $EXCLUDE_SINGLES |
-unj $TAIL -n 16 |
+unj $TAIL -n $MAX_WORDS_TO_COLOR |
 # pipeboth |
+# tee /tmp/hwdata1.$USER |
 sort -n -r -k 1 |
 dropcols 1 # |
 `
@@ -47,25 +63,28 @@ COLHEAD=`printf "\033\[00;"`
 # COLNORM=`curseblue`
 COLNORM=`cursenorm`
 COLBOLD=`cursebold`
-COLGREEN=`cursegreen`
-COLCYAN=`cursecyan`
 
 [ "$DEBUG" ] && echo -n "`cursegreen`""WORDS=[ `curseblue`" >&2
 
 SEDEXPR=`
 echo "$WORDS" |
 toregexp |
-numbereachline |
+PADDING=0 numbereachline |
 # pipeboth |
+# tee /tmp/hwdata2.$USER |
 # sed 's+^00*\(.\)+\1+' |
-sed 's+^00++' |
+# sed 's+^00++' |
+# sed 's+^0*\([^0]\)+\1+' |
 while read NUM WORDREGEXP
 do
 	# [ "$NUM" -lt 1 ] && NUM=6
 	# [ "$NUM" = 7 ] && NUM=1
-	CURSECOL="$((31+((NUM+1)%7)))"
+	# xterm: 6 produces white/grey, 7 loops back to red
+	CURSECOL="$((30+((NUM+1)%7)))"
 	COL="$COLHEAD$CURSECOL""m"
-	[ "$NUM" -lt 8 ] || COL="$COL$COLBOLD"
+	## This markes the *rarer* ones bolder than the most popular, only enabled for large MAX_WORDS_TO_COLOR.
+	# [ "$NUM" -gt 7 ] || [ "$MAX_WORDS_TO_COLOR" -lt 9 ] && COL="$COL$COLBOLD"
+	[ "$NUM" -lt 7 ] && COL="$COL$COLBOLD"
 	echo -n "s\<$WORDREGEXP\>$COL""\0""$COLNORMg ; "
 	[ "$DEBUG" ] && echo -n "$WORDREGEXP($CURSECOL) " >&2
 done
