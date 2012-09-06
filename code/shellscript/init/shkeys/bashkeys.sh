f48763890fe9e0d3271a9bbd8b6fa5c590d3b9fc
joey <joey>
2012-09-06 05:11:10 +0000

    joey@hwi -0 +4 2012/09/05 22:04 BST

--- code/shellscript/init/shkeys/bashkeys.sh
+++ code/shellscript/init/shkeys/bashkeys.sh
@@ -14,3 +14,7 @@ bind "\C-v":kill-word
 ## Use as alternative with x,v inhibited by Solaris:
 bind "\C-z":backward-kill-word
 bind "\C-b":kill-word
+## My mirror to C-u; although default is C-k.
+bind "\C-o":kill-line
+## Kinda dangerous to use in case bashkeys are not loaded, because C-o's
+## default is to run the current command (and load line back up into REPL)!
