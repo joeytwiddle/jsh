a32511afc0e98affef1406cd346f940e0f31d4df
joey <joey>
2012-09-06 18:26:16 +0000

    fixed fast implementation which was a complete swing and miss

--- code/shellscript/unixwrap/jwhich.sh
+++ code/shellscript/unixwrap/jwhich.sh
@@ -11,13 +11,13 @@
 
 if [ "$1" = "" ] || [ "$1" = --help ]
 then
-    echo "jwhich <command> [ quietly ]"
-    echo "  will find the executable file in your \$PATH minus anything that looks like jsh's JPATH"
-    echo "  now deprecated in favour of: unj which <command>"
-    echo "jwhich inj <command> [ quietly ]"
-    echo "  will look in cyour current \$JPATH for <file>"
-    echo "  quietly means it just checks and returns 1/0, but does not print anything."
-    exit 1
+	echo "jwhich <command> [ quietly ]"
+	echo "  will find the executable file in your \$PATH minus anything that looks like jsh's JPATH"
+	echo "  now deprecated in favour of: unj which <command>"
+	echo "jwhich inj <command> [ quietly ]"
+	echo "  will look in your current \$JPATH for <file>"
+	echo "  quietly means it just checks and returns 1/0, but does not print anything."
+	exit 1
 fi
 
 ## This script won't stop an infloop if you symlink a jsh script into a different PATH folder, e.g. ~/bin !
@@ -28,26 +28,26 @@ fi
 ## This would be slow but would remove the occasional possible inf-loop problems.
 
 fakeungrep () {
-   sed "s|.*$*.*||"
+	sed "s|.*$*.*||"
 }
 
 if test "$1" = "inj"
 then
-    # PATHS=`echo "$PATH" | tr ":" "\n"`
-    # INJ=true
-    # shift
+	# PATHS=`echo "$PATH" | tr ":" "\n"`
+	# INJ=true
+	# shift
 
 	## New fast implementation
-	[ -x "$JPATH/$2" ] && echo "$JPATH/$2" || false
+	[ -x "$JPATH/tools/$2" ] && echo "$JPATH/tools/$2" || false
 	exit
 else
-    # Remove all references to JLib from the path
-    PATHS=`echo "$PATH" | tr ":" "\n" |
-			if [ "$JPATH" ] ## just in case we arent in jsh!
-			then fakeungrep "$JPATH"
-			else cat
-			fi
-		`
+	# Remove all references to JLib from the path
+	PATHS=`echo "$PATH" | tr ":" "\n" |
+		if [ "$JPATH" ] ## just in case we arent in jsh!
+		then fakeungrep "$JPATH"
+		else cat
+		fi
+	`
 fi
 FILE="$1"
 QUIETLY="$2"
@@ -58,16 +58,16 @@ QUIETLY="$2"
 # This seems to work better, although there may be problems with spaces in the PATH
 for dir in $PATHS
 do
-  if test -f "$dir/$FILE"
-  then
-      ## Skip this one if it looks like _another_ instance of jsh on the path.
-      ## (Except if doing inj.)  Needed this because simple_init was recursively calling screen and filling mem evil-ly :/ :/
-      if [ ! "$INJ" ] && ( [ -f "$dir/../startj" ] || [ -f "$dir/../startj-hwi" ] )
-      then continue
-      fi
-      test ! "$QUIETLY" && echo $dir/$FILE
-      exit 0      # Found!  :)
-  fi
+	if test -f "$dir/$FILE"
+	then
+		## Skip this one if it looks like _another_ instance of jsh on the path.
+		## (Except if doing inj.)  Needed this because simple_init was recursively calling screen and filling mem evil-ly :/ :/
+		if [ ! "$INJ" ] && ( [ -f "$dir/../startj" ] || [ -f "$dir/../startj-hwi" ] )
+		then continue
+		fi
+		test ! "$QUIETLY" && echo $dir/$FILE
+		exit 0      # Found!  :)
+	fi
 done
 
 ## For debug: returns this command as "found" file (useful cos it's non-empty so it reaches further!)
