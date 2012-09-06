46f4297514bf48988dbd618a82828a7af61607cf
joey <joey>
2012-09-06 18:30:44 +0000

    joey@hwi -0 +2 2012/09/03 02:35 BST

--- code/shellscript/wget/wget_get_everything_below.sh
+++ code/shellscript/wget/wget_get_everything_below.sh
@@ -1,1 +1,3 @@
+## Could be renamed: get_high_fidelity_sub_mirror
+echo "Warning: May not be browseable (use -k or wget_get_browseable_copy for that), but may have more fidel to original."
 wget --mirror --no-parent "$@"
