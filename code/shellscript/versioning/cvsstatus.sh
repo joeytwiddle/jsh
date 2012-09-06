4740c9664f1ee918b8fed551d9eb097142dfa2d0
joey <joey>
2012-09-06 18:30:13 +0000

    blacklist rather than whitelist filtering, to make errors visible

--- code/shellscript/versioning/cvsstatus.sh
+++ code/shellscript/versioning/cvsstatus.sh
@@ -1,6 +1,15 @@
 ## Only tested on up-to-date checkout :P
-cvs status "$@" |
-grep "^File:" |
+
+cvs status "$@" 2>&1 |
+
+## Positive match for the info we want:
+# grep "^File:" |
+
+## Match for the data we don't want (allows unrecognised errors through)
+grep -v "^cvs status: Examining " |
+grep -v "^[ =]" |
+grep -v "^$" |
+
 # takecols 4 2
 # dropcols 1 3
 sed ' s+^File: ++ ; s+\<Status: ++ '
