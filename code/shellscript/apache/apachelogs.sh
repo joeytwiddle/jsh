45eac47d6b7b3161877096fc123f656216e1792e
joey <joey>
2012-09-17 05:32:45 +0000

    joey@hwi -1 +11 2012/09/13 20:46 BST

--- code/shellscript/apache/apachelogs.sh
+++ code/shellscript/apache/apachelogs.sh
@@ -1,12 +1,22 @@
 root=/mnt/hwibot
 [ -d "$root" ] || root=/
-tail -n 5 -f "$root"/var/log/apache2/*.log |
+
+tail -n 15 -f "$root"/var/log/apache2/*.log |
+
+# Hide bots, may hide wanted stuff too.
+# grep -v "HTTP/.*[Bb][Oo][Tt]" |
+# grep -v "HTTP/.*compatible.*[Bb][Oo][Tt]/" |
+## Alternatively, highlight them:
+highlight "(compatible; .*[Bb][Oo][Tt]/[^)]*)" red |
+## We might also want to drop "spider/"
 
 sed -u 's+\(.*\)"\(.*\)"+\1{\2}+' |
 
 ## Reduce the length of the lines a little bit
 ## Hide date fields
 sed -u 's+\[[^]]*[0-9][0-9]:[0-9][0-9]:[0-9][0-9][^]]*\] ++' |
+## Hide referrer
+sed -u 's+"http://[^"]*" ++' |
 
 highlight '{[^}]*}' magenta |
 highlight '\[[^]]*\]' grey |
