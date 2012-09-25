9da12ed3a800f8fac2bcb0b26aa69d0ab95d06eb
joey <joey>
2012-09-25 23:35:38 +0000

    doc + why

--- code/shellscript/apt/apt_find_autoable_packages.sh
+++ code/shellscript/apt/apt_find_autoable_packages.sh
@@ -1,16 +1,25 @@
+#!/bin/bash
+
+## Marks as many packages as possible auto-installed, allowing any packages to be removed.
+## The result is a minimal set of (i) install selections in dpkg/apt.
+## Useful if you have accidentally marked a load of packages as "wanted" in Aptitude, but you really only want the top-level applications you used to be marked.
+## This runs very slowly!  But it is also fairly safe (squeeze 2012).
+
 printf '' > selected_packages.txt
 
 ## Select packages which are installed but not marked auto.
 
-## First implementation can fail for long package names which get truncated.
+## This implementation can fail for long package names which get truncated.
 # for pkg in $(aptitude search ~i | grep -v "i A" | cut -d " " -f 4)
 
 for pkg in $(aptitude search "?installed ?not(?automatic)" --display-format "%p" | reverse)
 do
-	# If we mark it auto, will it cause other packages to be removed?
+	# If we mark it auto, will it cause any packages to be removed?
 	if ! aptitude markauto --simulate --assume-yes $pkg 2>&1 | grep 'will be REMOVED' > /dev/null
 	then
 		# It won't!
+		## Optional: show why
+		aptitude why $pkg
 		# Do not delay this action, or else it might appear we can remove the
 		# requiring package too, because the presence of this one requires him!
 		# (That's also why we do |reverse - so we will tend to mark child
