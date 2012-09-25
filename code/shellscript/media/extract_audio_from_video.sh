65b9d43ea9114249de2e5de98da16dbda54242b5
joey <joey>
2012-09-25 23:44:18 +0000

    joey@hwi -2 +3 2012/09/14 20:55 BST

--- code/shellscript/media/extract_audio_from_video.sh
+++ code/shellscript/media/extract_audio_from_video.sh
@@ -1,8 +1,9 @@
 ## This works on some YouTube videos containing aac or ogg audio.  Thanks pup!
-
+## But it fails on many other video formats.
 ## If it does not work on the video you have, you can try instead:
-##   convert_to_ogg or convert_to_mp3
+##   convert_to_ogg or convert_to_mp3 or dump_audio_from
 
 ## TODO: We should detect the type before or after, to properly name the output file.
 
 ffmpeg -i "$1" -vn -acodec copy "$1".audio
+
