eadffde4c80028052874806d98f81302a5b2c139
joey <joey>
2013-01-05 16:16:56 +0000

    highlighting ==== marker lines

--- code/shellscript/versioning/cvslog.sh
+++ code/shellscript/versioning/cvslog.sh
@@ -1,6 +1,8 @@
-cvs log "$@" |
+cvs log "$@" 2>&1 |
+grep -v "^cvs log: Logging " |
 
 highlight "^[-]*$" magenta |
+highlight "^[=]*$" magenta |
 highlight "^\(date:\|revision \).*" yellow |
 
 more
