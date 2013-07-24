68963ade71e544f80e8a36611ae996738219349b
Paul Joey Clark <joey@neuralyte.org>
2013-07-24 13:02:27 +0800

    single-argument invocation, and bail rather than fail on num=0

--- code/shellscript/versioning/gitvimdiff.sh
+++ code/shellscript/versioning/gitvimdiff.sh
@@ -1,9 +1,11 @@
-if [ "$1" = "" ] || [ "$2" = "" ] || [ "$1" = --help ]
+if [ "$1" = "" ] || [ "$1" = --help ]
 then
 	echo
 	echo "gitvimdiff <commit> <filename>"
 	echo
 	echo "gitvimdiff -n <num> <filename>"
+	echo
+	echo "gitvimdiff <filename>"
 	echo
 	echo "  will check out a temporary version of <filename> and do a vimdiff..."
 	echo
@@ -11,22 +13,41 @@ then
 	echo
 	echo "  The -n form goes <num> revisions back, so you don't have to lookup the commit ids."
 	echo
+	echo "  The final form, with only one file argument, diffs against the most recent commit."
+	echo
 	exit 1
 fi
 
-if [ "$1" = -n ]
+if [ -n "$1" ] && [ "$2" = "" ]
+then
+	revisionIndex=1
+	filename="$1"
+	shift
+elif [ "$1" = '-n' ]
 then
 	shift
 	revisionIndex="$1"
 	shift
 	filename="$1"
-	commitID=`git log "$filename" | grep "^commit " | head -n "$revisionIndex" | tail -n 1 | cut -d ' ' -f 2`
+	shift
 else
 	commitID="$1"
 	shift
+	filename="$1"
+	shift
 fi
 
-filename="$1"
+if [ -n "$1" ]
+then echo 'Too many arguments given!' ; exit 1
+fi
+
+if [ -n "$revisionIndex" ]
+then
+	if [ "$revisionIndex" -lt 1 ]
+	then echo 'revisionIndex should be >= 1!' ; exit 1
+	fi
+	commitID=`git log "$filename" | grep "^commit " | head -n "$revisionIndex" | tail -n 1 | cut -d ' ' -f 2`
+fi
 
 olderFile="$filename"."$commitID"
 
