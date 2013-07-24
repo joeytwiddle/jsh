c740f99ea043dc6dd6b86216e74a90f23e063547
Paul Joey Clark <joey@neuralyte.org>
2013-07-24 12:30:45 +0800

    volume changes first card

--- code/shellscript/audio/get_volume.sh
+++ code/shellscript/audio/get_volume.sh
@@ -2,7 +2,7 @@ if which amixer >/dev/null
 then
 	## No we don't want the % from alsa - this has quantum size ~4!
 	# amixer -c 1 sget "Master" | grep "Front .* Playback" | head -n 1 | sed 's+^[^[]*\[++ ; s+%\].*$++'
-	amixer -c 1 sget "Master" | grep "Front .* Playback" | head -n 1 | sed 's+^.* Playback ++ ; s+ .*++'
+	amixer -c 0 sget "Master" | grep "Front .* Playback" | head -n 1 | sed 's+^.* Playback ++ ; s+ .*++'
 elif which aumix >/dev/null
 then
 	# aumix -d $MIXER -q | grep "pcm " | after "pcm " | before ","
--- code/shellscript/audio/set_volume.sh
+++ code/shellscript/audio/set_volume.sh
@@ -1,6 +1,6 @@
 VOL="$1"
 if which amixer >/dev/null
-then amixer -c 1 sset "Master" "playback" "$VOL" >/dev/null
+then amixer -c 0 sset "Master" "playback" "$VOL" >/dev/null
 elif which aumix >/dev/null
 # then aumix -d $MIXER -w $VOL
 then aumix $AUMIX_OPTS -w "$VOL"
