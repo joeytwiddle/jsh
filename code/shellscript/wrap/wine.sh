cb8b71c03d5f092fc74976ab61117168caa5cd37
Paul Joey Clark <joey@neuralyte.org>
2013-07-24 13:03:39 +0800

    read and reset desktop resolution, rather than hard-coded

--- code/shellscript/wrap/wine.sh
+++ code/shellscript/wrap/wine.sh
@@ -6,12 +6,15 @@
 ## This was confusing, when jwhich failed and returned "", we just called "@", which occasionally does run a working command under linux >.<
 # `jwhich wine` "$@"
 
+xwindimensions=`getxwindimensions`
+
 ## Fullscreen wine apps (games, demos) often shrink the desktop, disturbing window positioning.
 ## Not all of them restore the desktop after running either!
 ## So we will store window positions before running, and restore afterwards.
 wmctrl_store_positions
 unj wine "$@"
-xrandr -s 1280x1024
+#xrandr -s 1280x1024
+xrandr -s "$xwindimensions"
 # wmctrl_restore_positions doesn't work in compiz
 findjob compiz >/dev/null ||
 wmctrl_restore_positions
