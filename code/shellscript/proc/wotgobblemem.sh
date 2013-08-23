5adebfff83ce00d1e1d29e9f73cb064870ded508
Paul Joey Clark <joey@neuralyte.org>
2013-08-24 04:57:14 +0800

    tail -n, note on aggregation

--- code/shellscript/proc/wotgobblemem.sh
+++ code/shellscript/proc/wotgobblemem.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 # myps -A | sort -n -k 6 | tail "$@"
-ps -o time,ppid,pid,nice,pcpu,pmem,user,comm -A | sort -n -k 6 | tail -15 "$@"
+ps -o time,ppid,pid,nice,pcpu,pmem,user,comm -A | sort -n -k 6 | tail -n 15 "$@"
+
+# We can sum the memory used by multiple threads of a single task, e.g.:
+#   | g chromium-browse | takecols 6 | awksum
+# but how to automate this to provide --aggregate ?
+# I suppose we should extract process names on the first run, loop and aggregate each.
