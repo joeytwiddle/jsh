a611da03f0b854aea760cac92883d19c864cb2a5
Paul Joey Clark <joey@neuralyte.org>
2013-08-24 04:56:52 +0800

    shebang needed to get valid UID

--- code/shellscript/net/whatisonport.sh
+++ code/shellscript/net/whatisonport.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 # Meh.  We get this message when using sudo, because UID remains unchanged.
+# Meh.  Now getting this even when logged in as root and already started jsh.  Ok changing shebang from sh to bash fixed that.
 [ "$UID" = 0 ] || echo "You probably need to be root."
 
 PORT="$1" ## For multiple, separate with ','s.
