5f7695156894f5497c11cfb6327b62bdc9be1a9e
Paul Joey Clark <joey@neuralyte.org>
2013-08-24 04:52:03 +0800

    doc and fixes

--- code/shellscript/file/mergedirs.sh
+++ code/shellscript/file/mergedirs.sh
@@ -1,4 +1,26 @@
 #!/bin/sh
+
+if [ "$1" = "" ] || [ "$1" = --help ]
+then
+cat << !
+
+mergedirs <dirA> <dirB>  [ |sh ]
+
+  Displays the commands required to move files from <dirA> into <dirB>
+  recursively, except when there is a non-identical file already present, in
+  which case the conflict is reported on stdout.
+
+  If you want to run what mergedirs suggests, simply re-run with |sh
+
+  TODO: Allow interactive conflict resolution by showing the size and dates of
+  conflicting files, and allowing display of a diff or resolution with vimdiff.
+
+  WARNING: There is no intelligent handling of symlinks.
+
+!
+  exit 1
+fi
+
 DIRA="`realpath \"$1\"`"
 DIRB="`realpath \"$2\"`"
 
@@ -7,15 +29,16 @@ DIRB="`realpath \"$2\"`"
 find . -type f |
 while read FILE
 do
-  if test ! -f "$DIRB/$FILE" ||
+  if [ ! -f "$DIRB/$FILE" ] ||
      cmp "$DIRA/$FILE" "$DIRB/$FILE"
   then
     echo mkdir -p '"'"`dirname \"$DIRB/$FILE\"`"'"'
     echo mv '"'$DIRA/$FILE'"' '"'$DIRB/$FILE'"'
     # echo mv "$DIRA/$FILE" "$DIRB/$FILE"
   else
-    echo "## Conflict: $FILE"
-    cksum "$DIRA/$FILE" "$DIRB/$FILE"
+    echo "## Conflict: $FILE" >&2
+    ls -l "$DIRA/$FILE" "$DIRB/$FILE" >&2
+    cksum "$DIRA/$FILE" "$DIRB/$FILE" >&2
   fi
 done
 
