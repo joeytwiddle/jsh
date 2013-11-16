66e483d0a2c786e4dbdd27e86d0545fdbffb8849
Paul Joey Clark <joey@neuralyte.org>
2013-11-17 04:34:58 +0800

    docs and IFS improvement

--- code/shellscript/unix/pipeboth.sh
+++ code/shellscript/unix/pipeboth.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-# jsh-depends: jdeltmp jgettmp
 
-## Takes input on stdin (stdout), echos output on both stdout and stderr
-## Can be useful for debugging:  Insert |pipeboth| somewhere in a chain of |s to print the state of the stream at that point to stderr, without breaking the original operation.
-## pipeboth's behaviour is to output on both streams, but only accept input from the stdin (the previous stdout), not stderr.
+## Takes input on stdin, echos output on both stdout and stderr.
+## Can be useful for debugging.  Insert |pipeboth| somewhere in a chain of |s to print the state of the stream at that point to stderr, without breaking the original operation.
+## For example, when debugging the following command, if you want to check if the output from c is what you expected, then do:
+##   a | b | c | pipeboth | d | e | f
+
+## pipeboth's behaviour is to output on both streams, but bash only allows it to accept input from the stdin (the previous stdout), not stderr.
 ## So if you want to pipe FROM both stdout and stderr streams of previous call, you should do:
 # ... command ... 2>&1 | pipeboth | ...
 
@@ -16,6 +18,7 @@ exit
 
 
 ## OLD IMPLEMENTATIONS
+# OLD jsh-depends: jdeltmp jgettmp
 
 # cat |
 
@@ -26,7 +29,7 @@ then
 	## but for txt streams, advantages are: it's line-buffered, and it doesn't use a tempfile.
 	shift
 	cat "$@" |
-	while read X
+	while IFS='' read X
 	do
 		# echo "$X"
 		# echo "$X" >&2
