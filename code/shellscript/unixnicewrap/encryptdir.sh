941e18647a47c7b1bb8648683e93aef6197bb96d
Paul Joey Clark <joey@neuralyte.org>
2014-01-05 03:35:14 +0800

    reformatted in new style; removed some jsh dependencies; changes that allow encrypted tarball to be a symlink

--- code/shellscript/unixnicewrap/encryptdir.sh
+++ code/shellscript/unixnicewrap/encryptdir.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-## If you do not yet have a public/private key pair, run: gpg --gen-key
+# If you do not yet have a public/private key pair, run: gpg --gen-key
 
 NOTAR=
-if test "$1" = "-notar"
+if [ "$1" = "-notar" ]
 then
 	NOTAR=true
 	shift
@@ -11,27 +11,30 @@ fi
 DIR="$1"
 FILE="$DIR.tgz.encrypted"
 
-test -f "$FILE" && [ ! "$NOBACKUP" ] &&
+[ -f "$FILE" ] && [ -z "$NOBACKUP" ] &&
 cp "$FILE" "$FILE.bak"
-
 # For when the above cp was a mv
-touch "$FILE"
-chmod 600 "$FILE"
+#touch "$FILE"
+#chmod 600 "$FILE"
 
-# [ "$WHICHKEY" ] || WHICHKEY=`gpg --list-keys | grep "^pub" | head -n 1 | dropcols 1 2 3`
-[ "$WHICHKEY" ] || WHICHKEY=`gpg --list-keys --with-colons | grep "^pub:" | head -n 1 | tr : ' ' | takecols 5`
+NEWFILE="$FILE.tmp"
+touch "$NEWFILE"
+chmod 600 "$NEWFILE"
 
-if test "$NOTAR"
-then
-	cat "$DIR"
-else
-	tar cz "$DIR"
+#[ -n "$WHICHKEY" ] || WHICHKEY=`gpg --list-keys | grep "^pub" | head -n 1 | dropcols 1 2 3`
+[ -n "$WHICHKEY" ] || WHICHKEY=`gpg --list-keys --with-colons | grep "^pub:" | head -n 1 | tr : ' ' | takecols 5`
+
+if [ -n "$NOTAR" ]
+then cat "$DIR"
+else tar cz "$DIR"
 fi |
+gpg $ENCDIR_GPGOPTS -r "$WHICHKEY" -e > "$NEWFILE"
 
-gpg $ENCDIR_GPGOPTS -r "$WHICHKEY" -e > "$FILE"
-
-if test ! "$?" = 0
+if [ "$?" = 0 ]
 then
-	rm -f "$FILE" ## Neater; cleans up the file if compression failed (usually means 0 length anyway).
+	# If the compression succeeded, move the data into the correct place
+	cat "$NEWFILE" > "$FILE" && rm -f "$NEWFILE"
+else
+	#rm -f "$NEWFILE" # Neater; cleans up the file if compression failed (usually means 0 length anyway).
 	exit 1
 fi
