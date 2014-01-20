c92c760e6541df297dbcbb77d6f9b68b4679ed38
Paul Joey Clark <joey@neuralyte.org>
2014-01-20 17:03:11 +0800

    slow checks for OS to choose options (currently commented out)

--- code/shellscript/super/l.sh
+++ code/shellscript/super/l.sh
@@ -3,8 +3,24 @@
 ## I know 'l' should really be an alias, but I personally like to use it e.g.
 ## from within vim.
 
+# Not needed if we have installed coreutils on PATH :)
+#JSH_OSNAME=`uname -s`
+#if [ "$JSH_OSNAME" = Darwin ]
+#then color_args="-G"
+#elif [ "$JSH_OSNAME" = Linux ]
+#then color_args="--color"
+#fi
+
+# We could check with: ls --version 2>&1 | grep 'GNU coreutils'
+# but this is somewhat overhead.  Perhaps jsh should do this at startup, and export a var.
+# In this case when certain scripts (or groups of scripts) need something preloaded we could make: $JPATH/preload/l
+# This might make it easier to drop the related preloads when a script or group of scripts are unwanted and removed.
+
+# For GNU ls:
+color_args="--color"
+
 ## useless_unless_sourced
-ls -lartFh --color "$@"
+ls -lartFh $color_args "$@"
 
 # if [ "$1" = "$PWD" ]
 # then :
