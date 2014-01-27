84215e92e019383199aba7cbdf3bd866a9993ca7
Paul Joey Clark <joey@neuralyte.org>
2014-01-27 08:32:15 +0800

    comments

--- code/shellscript/monitors/monitorfileaccess.sh
+++ code/shellscript/monitors/monitorfileaccess.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+# See also: watchforfileaccess
 jwatch listopenfiles -mergethreads . 2>/dev/null | grep -v "\<listopenfiles\>"
--- code/shellscript/monitors/watchfolderforchanges.sh
+++ code/shellscript/monitors/watchfolderforchanges.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# See also: watchforfileaccess
 [ "$DELAY" ] || DELAY=15
 TARGET_DIRS="$*" ## BUG: Fails on dirs with spaces.  Could fix with: for X in "$@"; do ...
 jwatch "find $TARGET_DIRS -maxdepth 4 -type f | withalldo nicels -ld" 2>&1 | dateeachline
