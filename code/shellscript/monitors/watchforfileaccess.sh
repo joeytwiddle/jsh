23c41568bd0b318afef5eb505d5858b06aeb25e8
Paul Joey Clark <joey@neuralyte.org>
2014-01-27 08:32:05 +0800

    use fasttreeprofile for subdirs

--- code/shellscript/monitors/watchforfileaccess.sh
+++ code/shellscript/monitors/watchforfileaccess.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-jwatch ls -ul $@
+# See also: package/command 'watchdog'.
+# See also: watchfolderforchanges, monitorfileaccess
+#jwatch ls -ul $@
+# Can add -R but does become slower (and doesn't show full paths)!
+#jwatch ls -ulR $@
+#jwatch fasttreeprofile "$@"
+# But we aren't really interested when files are access, only changed, so:
+jwatch eval "fasttreeprofile $@ | dropcols 2 3"
