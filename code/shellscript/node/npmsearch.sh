75fc2bf6d58b9eb9cfe62bf7610f3c818b1f520c
Paul Joey Clark <joey@neuralyte.org>
2014-07-06 21:18:13 +0800

    remove trailing spaces; for some reason i had a tonne of them!

--- code/shellscript/node/npmsearch.sh
+++ code/shellscript/node/npmsearch.sh
@@ -1,1 +1,3 @@
-memo -nd -t "2 months" npm search . | grep "$@"
+memo -nd -t "2 months" npm search . |
+sed 's+ *$++' |   # I don't know why but one of the memo-s I cached in June 2014 had a lot of trailing spaces
+grep "$@"
