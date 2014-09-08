65b1f7b143ab98cc8508d42c4076e3f10b524d89
Paul Joey Clark <joey@neuralyte.org>
2014-09-08 18:54:09 +0800

    avoid wrapping lines by using a pager

--- code/shellscript/node/npmsearch.sh
+++ code/shellscript/node/npmsearch.sh
@@ -1,3 +1,4 @@
 memo -nd -t "2 months" npm search . |
 sed 's+ *$++' |   # I don't know why but one of the memo-s I cached in June 2014 had a lot of trailing spaces
-grep "$@"
+grep "$@" |
+less -REXS        # Chop long lines (don't wrap them); provide pager if more than one screen
