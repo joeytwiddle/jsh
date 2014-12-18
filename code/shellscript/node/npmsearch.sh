006c8fb52bdeae2fd2e81f7ddcfb768ccaa5cc30
Paul Joey Clark <joey@neuralyte.org>
2014-12-19 02:48:04 +0800

    comments

--- code/shellscript/node/npmsearch.sh
+++ code/shellscript/node/npmsearch.sh
@@ -1,3 +1,7 @@
+# See also: https://github.com/josher19/npm-search
+#      and: https://www.npmjs.org/package/local-npm
+# Apparently npmd can do search also, but I never got that working :P
+
 memo -nd -t "2 months" npm search . |
 sed 's+ *$++' |   # I don't know why but one of the memo-s I cached in June 2014 had a lot of trailing spaces
 grep "$@" |
