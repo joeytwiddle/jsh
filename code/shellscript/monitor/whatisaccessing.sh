6deedf9652a9d561b23b92c100dd3a3692043b0a
Paul Joey Clark <joey@neuralyte.org>
2015-01-04 05:02:40 +0800

    comments/notes

--- code/shellscript/monitor/whatisaccessing.sh
+++ code/shellscript/monitor/whatisaccessing.sh
@@ -5,3 +5,8 @@ else
 	echo "whatsisaccessing <file/dir> lists processes reading or blocking that FS node."
 	exit 1
 fi
+
+# Alternative, probably faster:
+#fuser "$f"
+
+# See also: monitorfileaccess
