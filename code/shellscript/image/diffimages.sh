74f4bc025af6850a86bbf54fcc434c77382f70fe
Paul Joey Clark <joey@neuralyte.org>
2015-02-08 14:31:23 +0800

    display number and percentage of differing images

--- code/shellscript/image/diffimages.sh
+++ code/shellscript/image/diffimages.sh
@@ -60,8 +60,14 @@ then
 	echo 'Images are identical.'
 	true
 else
-	# It might be nice to get a measure of the difference here, e.g. %age of pixels which are not black.
 	echo 'There are differences.'
+	total_pixels=`grep '^    Pixels: ' <<< "$info" | sed 's+.*: ++'`
+	# BUG: If there are many differences (e.g. Colors: 5844) then no Histogram section is shown.
+	black_pixels=`grep -A1 '^  Histogram:$' <<< "$info" | grep ' #000000 black$' | sed 's+^ *++ ; s+:.*++'`
+	non_black_pixels=$((total_pixels - black_pixels))
+	#difference_percentage=`expr '(' $non_black_pixels '*' 200 + 1 ')' / $total_pixels / 2`
+	difference_percentage=`echo "scale=2; $non_black_pixels * 100 / $total_pixels" | bc | sed 's+^\.+0.+'`
+	echo "The two images differ by $difference_percentage% ($non_black_pixels pixels)."
 	false
 fi
 
