ba08bb4414e72f477f8b625dcfdfbc7828e6db70
Paul Joey Clark <joey@neuralyte.org>
2015-06-13 18:39:27 +0000

    notes

--- code/shellscript/wget/wget_get_everything_below.sh
+++ code/shellscript/wget/wget_get_everything_below.sh
@@ -1,3 +1,7 @@
 ## Could be renamed: get_high_fidelity_sub_mirror
-echo "Warning: May not be browseable (use -k or wget_get_browseable_copy for that), but may have more fidel to original."
+echo "Warning: May not be browseable (use -k or wget_get_browseable_copy for that).  This kind of backup has more fidelity to the original, but may not work when placed on a different domain, and won't pass checksum comparisons."
+echo
+sleep 1
 wget --mirror --no-parent "$@"
+## Some more useful options:
+# -k -e robots=off --wait 1
