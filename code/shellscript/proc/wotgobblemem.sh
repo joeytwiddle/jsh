9e1884d6fa9d7e4eed69877224e50d9c6459d539
Paul Joey Clark <joey@neuralyte.org>
2015-06-13 18:38:25 +0000

    show the command's arguments as well as its name

--- code/shellscript/proc/wotgobblemem.sh
+++ code/shellscript/proc/wotgobblemem.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # myps -A | sort -n -k 6 | tail "$@"
-ps -o time,ppid,pid,nice,pcpu,pmem,user,comm -A | sort -n -k 6 | tail -n 15 "$@"
+ps -o time,ppid,pid,nice,pcpu,pmem,user,args -A | sort -n -k 6 | tail -n 15 "$@"
 
 # We can sum the memory used by multiple threads of a single task, e.g.:
 #   | g chromium-browse | takecols 6 | awksum
