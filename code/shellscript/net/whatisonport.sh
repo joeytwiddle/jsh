ba7e883111c55f95432764c2fcb8682fa69abeee
Paul Joey Clark <joey@neuralyte.org>
2015-07-19 13:41:49 +0100

    use netstat, faster than fuser

--- code/shellscript/net/whatisonport.sh
+++ code/shellscript/net/whatisonport.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+PORT="$1"
+
+
+# New method using netstat
+netstat -anp --numeric-ports | grep ":$PORT\>.*:"
+exit
+
+
+# Old method using fuser; slower and requires root!
+
 # Meh.  We get this message when using sudo, because UID remains unchanged.
 # Meh.  Now getting this even when logged in as root and already started jsh.  Ok changing shebang from sh to bash fixed that.
 [ "$UID" = 0 ] || echo "You probably need to be root."
 
-PORT="$1" ## For multiple, separate with ','s.
+# For multiple ports, you can separate with ','s.
 fuser -v "$PORT"/tcp
+
