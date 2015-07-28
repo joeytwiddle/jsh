0fe0c59b720d9f6f116ba1a0a0f58a99e0dadb81
Paul Joey Clark <joey@neuralyte.org>
2015-07-28 01:25:58 +0100

    Descend into folders to get a summary of their status

--- code/shellscript/nicels/gitls.sh
+++ code/shellscript/nicels/gitls.sh
@@ -6,7 +6,22 @@ while read node
 do
 	extra="--"
 	if [ -d "$node" ]
-	then extra="  "
+	then
+		if [ -n "$GITLS_CHECK_FOLDERS" ]
+		then
+			modified=$(git status --porcelain "$node" 2>/dev/null | grep -m 1 -o "^.M")
+			if [ -n "$modified" ]
+			then extra="$modified"
+			else
+				unknown=$(git status --porcelain "$node" 2>/dev/null | grep -m 1 -o "^??")
+				if [ -n "$unknown" ]
+				then extra="$unknown"
+				else extra="  "
+				fi
+			fi
+		else
+			extra="::"
+		fi
 	elif [ -f "$node" ]
 	then
 		extra="$(git status --porcelain --ignored "$node" 2>/dev/null | cut -c 1-2)"
