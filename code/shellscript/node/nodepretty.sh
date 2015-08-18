fca864184a4c7df20e9e120766722d068f1af4aa
Paul Joey Clark <joey@neuralyte.org>
2015-08-18 17:18:08 +0100

    Updates for a more recent versions.  Seems to work ok without arguments now.

--- code/shellscript/node/nodepretty.sh
+++ code/shellscript/node/nodepretty.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
 
-prettyApp=$HOME/npm/lib/node_modules/prettydiff/api/node-local.js
+# You will need prettydiff installed: npm install -g prettydiff
 
-mode=beautify
+[ -z "$PRETTYDIFF_APP" ] && PRETTYDIFF_APP=$HOME/npm/bin/prettydiff
+
+[ -z "$PRETTYDIFF_MODE" ] && PRETTYDIFF_MODE=beautify
 
 if [ "$#" = 0 ]
-then node "$prettyApp" source:"`cat`" readmethod:screen     mode:"$mode" report:false
+then
+	# This did something weird.  It replaced all `/` with `\` and left everything else unchanged.
+	#node "$PRETTYDIFF_APP" source:"`cat`" readmethod:screen     mode:"$PRETTYDIFF_MODE" report:false
+	# It rejects /dev/stdin as input because it isn't a file.
+	# But reading from a tempfile worked fine:
+	tmpfile=/tmp/nodepretty.$USER.$$.js
+	cat > "$tmpfile"
+	node "$PRETTYDIFF_APP" source:"$tmpfile" readmethod:filescreen mode:"$PRETTYDIFF_MODE" report:false
+	rm -f "$tmpfile"
 elif [ "$#" = 1 ]
-then node "$prettyApp" source:"$1"    readmethod:filescreen mode:"$mode" report:false
+then node "$PRETTYDIFF_APP" source:"$1"    readmethod:filescreen mode:"$PRETTYDIFF_MODE" report:false
 elif [ "$#" = 2 ]
-then node "$prettyApp" source:"$1"    readmethod:file       mode:"$mode" report:false output:"$2"
+then node "$PRETTYDIFF_APP" source:"$1"    readmethod:file       mode:"$PRETTYDIFF_MODE" report:false output:"$2"
 fi
 
