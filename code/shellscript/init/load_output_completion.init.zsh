4aea236155951e885aace613a2434e4c3a99339f
Paul Joey Clark <joey@neuralyte.org>
2015-10-07 12:19:10 +0200

    enable output completion if var is set; more aliases and a '[skip]' echo

--- code/shellscript/init/load_output_completion.init.zsh
+++ code/shellscript/init/load_output_completion.init.zsh
@@ -1,8 +1,23 @@
 ### zsh commandline completion based on output of recent (latest) command
 
+# How it works:
+# We add 'record-log ' before every command to be executed.
+# So in fact record-log is executed.  It then runs the command, but it sends output to a file.
+# Later, tab-completion can read this file to obtain useful words.
+
+# You can enable ALWAYS_RECORD_OUTPUT_COMPLETION to get it to automatically record the output of commands.
+# But it is quite disruptive at the moment, so a friendlier alternative is to put `record-log ` at the beginning of a command before running it.  The output of the command will be stored and you will later be able to perform completion on its words.
+
 # ISSUES:
+# - Only applies to the commands before a '|' I believe.
+# - Does not work on aliases.
 # - History gets polluted with record-log appended to commands
-# - tee forces things like man to simply cat the file, instead of providing the interactive pager
+# - The use of tee forces things like man to simply cat the file, instead of providing the interactive pager
+# - Completion doesn't work on all commands.  (I have had trouble with `git log` and aliases to it.  `l` and `echo` always seem to work.)
+
+# For the bash version, we may be able to use the "DEBUG trap": http://askubuntu.com/questions/22233/always-prompt-the-user-before-executing-a-command-in-the-shell#22256
+# This page has a nice example of a history search under the section "Programmable Completion": http://www.cl.cam.ac.uk/local/sys/unix/applications/bash/
+# I found that through this page: http://unix.stackexchange.com/questions/61871/can-any-shell-do-argument-level-interactive-search
 
 RUNTIME_FILE_PREFIX="/tmp/joey-output-completion.$$"
 
@@ -37,13 +52,26 @@ add-log-recorder() {
 	zle .$WIDGET "$@"
 }
 
-zle -N accept-line add-log-recorder
+# Tell zsh to run our add-log-recorder function before executing a command, which gives it the opportunity to modify the command.
+# At the moment this is a little disruptive, so I don't enable it by default.
+if [ -n "$ALWAYS_RECORD_OUTPUT_COMPLETION" ]
+then
+	zle -N accept-line add-log-recorder
+fi
+
+alias log=record-log
+alias record=record-log
+alias copy=record-log
+alias ccopy=record-log
+alias clog=record-log
+alias crec=record-log
 
 output-completion() {
 	read -c COMMAND ARGS
 	## Heuristic:
 	if [ -z "$ARGS" ]
 	then
+		echo -n "[skip]"
 		reply=
 	else
 		# This stops the cat from throwing errors if no completions were generated.
@@ -52,4 +80,7 @@ output-completion() {
 	fi
 }
 
+# It might be nice if we could complete whole filenames from recent output, instead of being forced to step through each folder with Tab.  We need to mute the default folder/file completion.
+# I tried removing -n but still found completions were completing based on the filesystem nodes.
 compctl -f -c -u -r -K output-completion "*" -tn
+# Anyway there is room for improvement here, not least moving to |zsh-compsys|
