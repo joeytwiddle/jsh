140b64a8169fc3a3c8106456fa59342ec1eb5882
Paul Joey Clark <joey@neuralyte.org>
2015-10-07 17:46:05 +0200

    Always use mplayer to play each track; optionally convert oggs to mp3 before playing

--- code/shellscript/music/playmp3andwait.sh
+++ code/shellscript/music/playmp3andwait.sh
@@ -1,6 +1,42 @@
 #!/bin/sh
-if endswith "$1" "\.ogg" && which ogg123 >/dev/null 2>&1
-then ogg123 "$@" ; exit
+
+file_to_play="$1"
+
+if endswith "$file_to_play" "\.ogg" && which ogg123 >/dev/null 2>&1
+then
+
+	#ogg123 "$@" ; exit
+
+	# nextsong does not work so well on oggs played through pulseaudio by mplayer
+	# Let's re-encode it to an mp3 file
+
+	# --- DISABLED currently ---
+	#CONVERT_OGGS_TO_MP3S_BEFORE_PLAYING=1
+
+	if [ -n "$CONVERT_OGGS_TO_MP3S_BEFORE_PLAYING" ]
+	then
+
+		ogg_clone="/tmp/$$.ogg"
+		mp3_file="/tmp/$$.mp3"
+
+		jshinfo "Converting to $mp3_file"
+
+		cat "$file_to_play" > "$ogg_clone"
+
+		if nice ionice convert_to_mp3 "$ogg_clone"
+		then
+			echo "Converted $file_to_play to $mp3_file"
+
+			# Change the file which will be played later to our newly encoded file
+			file_to_play="$mp3_file"
+		else
+			echo "Failed to convert to mp3: $ogg_clone"
+		fi
+
+	fi
+
+	#echo "[log] file_to_play: $file_to_play"
+
 # # else mpg123 -b 10000 "$@" > /dev/null 2>&1
 # else unj mplayer "$@" > /dev/null 2>&1
 fi
@@ -9,20 +45,23 @@ fi
 
 
 
-find_exe() {
-	for X
-	do
-		if jwhich "$X" >/dev/null 2>&1
-		then echo "$X"; return
-		fi
-	done
-}
-
 ## totem doesn't exit after it's played (but we could watch for it) - it probably won't work without X
 ## noatun dunno... :)
 
-PLAYER=`find_exe mplayer-minixterm mplayer totem noatun`
-unj "$PLAYER" "$@"
+#find_exe() {
+#	for X
+#	do
+#		if jwhich "$X" >/dev/null 2>&1
+#		then echo "$X"; return
+#		fi
+#	done
+#}
+#PLAYER=`find_exe mplayer-minixterm mplayer totem noatun`
+#verbosely unj "$PLAYER" $AUDIO_PLAYER_OPTS "$file_to_play"
+
+verbosely mpg123 $AUDIO_PLAYER_OPTS "$file_to_play"
+
+#verbosely mplayer $AUDIO_PLAYER_OPTS "$file_to_play"
 
 # mplayer "$@" |
 # (
