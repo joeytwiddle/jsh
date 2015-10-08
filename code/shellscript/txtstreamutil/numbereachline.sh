76f18fa328eb60b3587c75c330a84bbd243f289c
Paul Joey Clark <joey@neuralyte.org>
2015-10-08 03:28:58 +0200

    Let user set PADDING, PADCHAR and DELIM independently

--- code/shellscript/txtstreamutil/numbereachline.sh
+++ code/shellscript/txtstreamutil/numbereachline.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
-if [ "$PADDING" ]
-then PADSTR="%0$PADDING"i
-else PADSTR="%03i"
-fi
+# e.g. PADCHAR=0
+[ -z "$PADDING" ] && PADDING=3
+# e.g. PADCHAR=" "
+[ -z "$PADCHAR" ] && PADCHAR='0'
+
+[ -z "$DELIM" ] && DELIM="\t"
+
+# e.g. PADSTR="%03i"
+PADSTR="%0${PADDING}i"
 
 N=0
 escapeslash | ## since echo "$LINE" will lose \s if not doubled.  BUG: what else might echo "$LINE" lose?
 while read LINE
 do
 	# printf "%s\n" "$N	$LINE"
-	printf "$PADSTR\t%s\n" "$N" "$LINE"
+	printf "${PADSTR}${DELIM}%s\n" "$N" "$LINE"
 	N=$(($N+1))
 done
