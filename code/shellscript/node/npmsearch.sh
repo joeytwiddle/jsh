c73d10af285ce78230062243e193d32d9f338ad4
Paul Joey Clark <joey@neuralyte.org>
2015-10-08 03:25:26 +0200

    npm search is just too slow these days; use the web service instead

--- code/shellscript/node/npmsearch.sh
+++ code/shellscript/node/npmsearch.sh
@@ -2,6 +2,9 @@
 #      and: https://www.npmjs.org/package/local-npm
 # Apparently npmd can do search also, but I never got that working :P
 
+browse "https://www.npmjs.com/search?q=$*"
+exit
+
 memo -nd -t "2 months" npm search . |
 sed 's+ *$++' |   # I don't know why but one of the memo-s I cached in June 2014 had a lot of trailing spaces
 grep "$@" |
