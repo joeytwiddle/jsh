0ea3a0a82aea5fb7f5a04581b0ac1fc4de69d637
Paul Joey Clark <joey@neuralyte.org>
2015-12-14 09:14:45 +0800

    zsh completion now works again for cd into dirnames containing spaces

--- code/shellscript/dirhistory/dirhistorysetup.zsh
+++ code/shellscript/dirhistory/dirhistorysetup.zsh
@@ -1,10 +1,16 @@
 # My dirhistory tool
-# You need to source this in your ~/.<mysh>rc
+# @sourceme You need to source this from your ~/.zshrc
 
 if which d.zsh b.zsh f.zsh 2>&1 > /dev/null; then
 	alias cd='d'
-	# alias d='SUPPRESS_PRECMD=undo;SUPPRESS_PREEXEC=undo . $JPATH/tools/d.zsh'
-	alias d='SUPPRESS_PREEXEC=undo . $JPATH/tools/d.zsh'
+	#alias d='SUPPRESS_PRECMD=undo;SUPPRESS_PREEXEC=undo . $JPATH/tools/d.zsh'
+	#alias d='SUPPRESS_PREEXEC=undo . $JPATH/tools/d.zsh'
+	# In order to let d use cd's Tab-completion, we must make it a function
+	d () {
+		SUPPRESS_PREEXEC=undo . $JPATH/tools/d.zsh "$@"
+	}
+	compdef d=cd
+
 	alias f='SUPPRESS_PREEXEC=undo . $JPATH/tools/f.zsh'
 	alias b='SUPPRESS_PREEXEC=undo . $JPATH/tools/b.zsh'
 else
