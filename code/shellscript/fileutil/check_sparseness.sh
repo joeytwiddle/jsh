d86f27200c32a13c2a63d3471caa1d5783f5239c
Paul Joey Clark <joey@neuralyte.org>
2015-12-30 09:40:09 +0800

    Extra alternative solutions

--- code/shellscript/fileutil/check_sparseness.sh
+++ code/shellscript/fileutil/check_sparseness.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# Some solutions drawn from: http://unix.stackexchange.com/questions/86442/finding-sparse-files
+
+# A quick way to search.  We could adjust sed to show the percentages.
+# I suspect this suffers from the same BUG outlined below.
+#find "$@" -type f ! -size 0 -printf '%S:%p\0' | sed -zn 's/^0[^:]*://p' | tr '\0' '\n'
+
 for file
 do
 	# BUG: Sometimes produces %age > 100%, because as 'du' says "apparent size is usually smaller".
@@ -19,4 +25,10 @@ do
 	percentage_full=$(expr "$data_size" '*' 100 / "$total_size")
 
 	printf "%s is %s%% full\n" "$file" "$percentage_full"
+
+	# Another way to check is:
+	#if perl -le 'seek STDIN,0,4;$p=tell STDIN; seek STDIN,0,2; exit 1 if $p == tell STDIN' < "$file"
+	#then printf "%s\n" "$file is sparse"
+	#else printf "%s\n" "$file is not sparse"
+	#fi
 done
