5ac8546e7c2779c0f0b61d9ddd1882297d3c55cb
Paul Joey Clark <joey@neuralyte.org>
2016-01-04 15:00:37 +0800

    Change folder to see file status from his perspective

--- code/shellscript/nicels/gitls.sh
+++ code/shellscript/nicels/gitls.sh
@@ -13,25 +13,28 @@ else cat
 fi |
 while read node
 do
+	cwd="$PWD"
+	lnode="$(basename "$node")"
+	cd "$(dirname "$node")"
 	# Fallback (default) status.  Not many things get this.  Broken symlinks do (untracked, not sure about tracked).
 	extra="--"
-	if [ -d "$node" ]
+	if [ -d "$lnode" ]
 	then
 		# Recursive mode is optional because it's a lot slower on large repositories.
 		if [ -n "$GITLS_CHECK_FOLDERS" ]
 		then
 			# If any file below is modified, display that
-			modified=$(git status --porcelain "$node" 2>/dev/null | grep -m 1 -o "^.M")
+			modified=$(git status --porcelain "$lnode" 2>/dev/null | grep -m 1 -o "^.M")
 			if [ -n "$modified" ]
 			then extra="$modified"
 			else
 				# If any file below is unknown, then display that
-				unknown=$(git status --porcelain "$node" 2>/dev/null | grep -m 1 -o "^??")
+				unknown=$(git status --porcelain "$lnode" 2>/dev/null | grep -m 1 -o "^??")
 				if [ -n "$unknown" ]
 				then extra="$unknown"
 				else
-					# Report whatever git reports.
-					whatever=$(git status --porcelain "$node" 2>/dev/null | grep -m 1 -o "^..")
+					# Just display the first thing that git reports
+					whatever=$(git status --porcelain "$lnode" 2>/dev/null | grep -m 1 -o "^..")
 					if [ -n "$whatever" ]
 					then extra="$whatever"
 					else extra="  "
@@ -45,15 +48,17 @@ do
 			#extra="##"
 			#extra="  "
 		fi
-	elif [ -f "$node" ]
+	elif [ -f "$lnode" ]
 	then
 		# Get the two-character status that git reports for this file
 		# We look for ignored files, they sometimes produce "!!" but occasionally ""
 		# Up-to-date files always produce ""
-		extra="$(git status --porcelain --ignored "$node" 2>/dev/null | cut -c 1-2)"
+		# Unfortunately, if we are not in a git folder, then we also get ""!
+		extra="$(git status --porcelain --ignored "$lnode" 2>/dev/null | cut -c 1-2)"
 		[ "$extra" = "" ] && extra="  "
 	fi
 	#echo -n "$extra "
+	cd "$cwd"
 	ls -ld --color "$node" | sed "s+^\([^ ]* *\)\{8\}+\0[$extra] +"
 done |
 if which columnise-clever >/dev/null
