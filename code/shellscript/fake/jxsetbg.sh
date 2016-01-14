545fbe43646e0ba022a7c84bb7950bc54c1cff90
Paul Joey Clark <joey@neuralyte.org>
2016-01-14 13:27:58 +0800

    Determine desktop aspect ratio and image aspect ratio dynamically, so they can be compared.

--- code/shellscript/fake/jxsetbg.sh
+++ code/shellscript/fake/jxsetbg.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # BUG: This reports xsetbg as required, even though this script in fact works fine without it.
 #      But is there something a little better about xsetbg?
@@ -11,8 +11,8 @@ IMAGE="$1"
 
 ## xsetbg is faster, but sometimes poor aspect for tall pictures, and doesn't support bmp
 ## TODO: convert to jpeg if bmp offered!
-# `jwhich xsetbg` -dither -fullscreen -border black "$IMAGE" ||
-# `jwhich xsetbg` -fullscreen -onroot -fit -border black "$IMAGE" ||
+# $(jwhich xsetbg) -dither -fullscreen -border black "$IMAGE" ||
+# $(jwhich xsetbg) -fullscreen -onroot -fit -border black "$IMAGE" ||
 (
 	echo "Converting..." >&2
 	## CONVERTOPTS="-interlace Plane"
@@ -23,17 +23,27 @@ IMAGE="$1"
 	if which convert >/dev/null 2>&1
 	then
 
-		SIZE=`imagesize "$IMAGE"` # | pipeboth
-		XRES=`echo "$SIZE" | beforefirst x`
-		YRES=`echo "$SIZE" | afterfirst x`
-		ASPECT_PERCENT=$((XRES*100/YRES))
-		#if [ "$ASPECT_PERCENT" -gt 123 ] && [ "$ASPECT_PERCENT" -lt 141 ]
-		#then FIX='!'
-		#fi
-		# jshinfo "aspect=$ASPECT_PERCENT FIX=$FIX"
+		## If image aspect ratio is similar to desktop aspect ratio, then stretch it instead of adding a border.
+		DESKTOP_SIZE=$(getxwindimensions)
+		DESKTOP_XRES=$(echo "$DESKTOP_SIZE" | beforefirst x)
+		DESKTOP_YRES=$(echo "$DESKTOP_SIZE" | afterfirst x)
+		DESKTOP_ASPECT_PERCENT=$((DESKTOP_XRES*100/DESKTOP_YRES))
 
-		TARGET_DIMENSIONS=`getxwindimensions`
-		# DITHER=just_testing ## todo: autodetect via xdpyinfo
+		IMAGE_SIZE=$(imagesize "$IMAGE")
+		IMAGE_XRES=$(echo "$IMAGE_SIZE" | beforefirst x)
+		IMAGE_YRES=$(echo "$IMAGE_SIZE" | afterfirst x)
+		IMAGE_ASPECT_PERCENT=$((IMAGE_XRES*100/IMAGE_YRES))
+
+		RATIO_DIFFERENCE_PERCENT=$((DESKTOP_ASPECT_PERCENT - IMAGE_ASPECT_PERCENT))
+
+		if [ "$RATIO_DIFFERENCE_PERCENT" -gt -8 ] && [ "$RATIO_DIFFERENCE_PERCENT" -lt 10 ]
+		then FIX='!'
+		fi
+
+
+		## Stretch the image to fit the screen, giving it a border if necessary.
+		# DITHER=just_for_fun ## todo: autodetect whether it is needed via xdpyinfo
+		TARGET_DIMENSIONS=$(getxwindimensions)
 		if [ "$DITHER" ]
 		then convert "$IMAGE" -geometry "$TARGET_DIMENSIONS""$FIX" -depth 8 -dither $CONVERTOPTS /tmp/tmp-dithered.png && IMAGE=/tmp/tmp-dithered.png
 		else convert "$IMAGE" -geometry "$TARGET_DIMENSIONS""$FIX" $CONVERTOPTS /tmp/tmp.png && IMAGE=/tmp/tmp.png
