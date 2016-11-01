d28ced9c253a351669f89dcbcd2b1816c6a19551
Paul Joey Clark <joey@neuralyte.org>
2016-11-01 17:33:04 +0800

    Also remove any files left around by git-mergetool

--- code/shellscript/git/git-merge-clean.sh
+++ code/shellscript/git/git-merge-clean.sh
@@ -1,1 +1,4 @@
-find . -name '*.orig' -or -name '*.rej' -or -name '*.BASE.*' -or -name '*.REMOTE.*' -or -name '*.LOCAL.*' -or -name '*.BACKUP.*' | foreachdo del
+find . -name '*.orig' -or -name '*.rej' \
+	-or -name '*.BASE.*' -or -name '*.REMOTE.*' -or -name '*.LOCAL.*' -or -name '*.BACKUP.*' \
+	-or -name '*_BASE_*' -or -name '*_REMOTE_*' -or -name '*_LOCAL_*' -or -name '*_BACKUP_*' \
+	| foreachdo del
