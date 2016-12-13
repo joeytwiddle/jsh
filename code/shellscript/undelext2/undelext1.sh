3ced855d973b65681d0b5b918989f9f6229fd0c6
Paul Joey Clark <joey@neuralyte.org>
2016-12-13 12:31:13 +0800

    Noted that undelext[12] are poorly named, and some alternatives

--- code/shellscript/undelext2/undelext1.sh
+++ code/shellscript/undelext2/undelext1.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+
+# TODO: Rename these scripts!  They are for ext2, not for ext1 or ext3.  And move them to 'forensics' folder.
+# Remove these scripts.  They are largely useless, since nobody uses ext2 any more, and they fail to recover anything on ext3.
+
+# See also: http://unix.stackexchange.com/questions/320149/read-past-end-of-file-to-recover-data/320150
+# That is very inefficient but it supports general purpose recovery on a non-encrypted partition, by searching for a text string:
+# strings -n 12 -t d /dev/partition | grep -F 'text snippet'
+
 if test "x$1" = "x"; then
   echo "undel1 <filesystem-device> [ > <file_to_edit> ]"
   echo "provides recent inode deletions from filesystem."
