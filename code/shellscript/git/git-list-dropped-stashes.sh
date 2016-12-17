0f990476aeeb8c32d8b7b79aee902e7ced3b16cf
Paul Joey Clark <joey@neuralyte.org>
2016-12-17 11:32:46 +0800

    Sort commits into reverse date order

--- code/shellscript/git/git-list-dropped-stashes.sh
+++ code/shellscript/git/git-list-dropped-stashes.sh
@@ -2,6 +2,9 @@
 
 # Whoops did you just pop a stash and then lose the changes with an accidental reset?  Don't panic, that stash is probably still in history.
 
-# TODO: Would be nice to sort them into reverse-date order
+list_of_commits="$(memo git fsck --no-reflog | awk '/dangling commit/ {print $3}')"
 
-git show -p $(memo git fsck --no-reflog | awk '/dangling commit/ {print $3}')
+# Optional: sort the commits into reverse date order
+sorted_list="$(git show --pretty="%at %H" --no-patch ${list_of_commits} | sort -n -k 1 -r | awk '{print $2}')"
+
+git show --stat -p ${sorted_list}
