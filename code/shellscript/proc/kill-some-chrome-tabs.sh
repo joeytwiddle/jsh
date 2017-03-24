4c4983e69fdb51cc9eadcafefd5ddedd63e64799
Paul Joey Clark <joey@neuralyte.org>
2017-03-24 15:23:36 +0800

    Skip chrome extensions; use ps directly instead of wotgobblemem

--- code/shellscript/proc/kill-some-chrome-tabs.sh
+++ code/shellscript/proc/kill-some-chrome-tabs.sh
@@ -3,4 +3,6 @@
 # When Linux gets low on memory, earlyoom kills some Chrome tabs for me
 # When Mac OS X gets low on memory, we can kill some Chrome tabs using this script
 
-wotgobblemem | grep "Google Chrome Helper" | takecols 3 | tail -n 10 | withalldo kill
+#ps -o time,ppid,pid,nice,pcpu,pmem,user,args -A | sort -n -k 6 |
+
+wotgobblemem | grep "Google Chrome Helper" | grep -v ' --extension-process ' | takecols 3 | tail -n 5 | withalldo kill
