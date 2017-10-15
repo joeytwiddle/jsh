d7ef3b225939b50098f1166dc5aa6d98451d9895
Paul Joey Clark <joey@neuralyte.org>
2017-10-15 14:47:51 +0800

    Remove duplicate pkill

---
+++ code/shellscript/xwin/make_caps_lock_control_escape.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+pkill -u "$UID" xcape
+setxkbmap -option 'caps:ctrl_modifier'
+xcape -t 200 -e 'Caps_Lock=Escape'
+#xcape -e 'Caps_Lock=Escape;Control_L=Escape;Control_R=Escape'
+
+# To undo:
+#     killall xcape
+#     setxkbmap -option
