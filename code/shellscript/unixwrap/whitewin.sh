7283b8c1bcda3e5735b143bf566be6012f9dd264
Paul Joey Clark <joey@neuralyte.org>
2017-11-09 00:32:29 +0800

    Change the colors depending which OS we are on

--- code/shellscript/unixwrap/whitewin.sh
+++ code/shellscript/unixwrap/whitewin.sh
@@ -12,16 +12,28 @@
 #xterm -fg "#005500" -bg "#eeee99" "$@" &
 # This still looks a bit too rich, but acceptable:
 #xterm -fg "#004400" -bg "#ddddaa" "$@" &
-# This looks far too dirty grey:
-#xterm -fg "#004400" -bg "#bbbb99" "$@" &
 # Medium-strong cream, acceptable but still a bit bright:
 #xterm -fg "#004400" -bg "#ddddbb" "$@" &
-# Medium-strong cream, acceptable:
-#xterm -fg "#004400" -bg "#ccccaa" "$@" &
-# Softened yellow, about right (originally weaker b4):
-xterm -fg "#004400" -bg "#ccccb0" "$@" &
-# Darker, faded weak yellow, approaching dirty grey:
+# Softened yellow, about right on Mac, but way too washed out on Linux:
+#xterm -fg "#004400" -bg "#ccccb0" "$@" &
+# Darker, faded weak yellow, approaching dirty grey, again just grey on Linux:
 #xterm -fg "#004400" -bg "#c0c0a8" "$@" &
+## Trying to find a compromise
+# Awful, completely grey on Linux:
+#xterm -fg "#004400" -bg "#bbbbaa" "$@" &
+# Medium-strong cream, acceptable on Mac.  A bit white on Linux, but ok.
+#xterm -fg "#004400" -bg "#ccccaa" "$@" &
+## Looks fine on Linux (a visible cream):
+#xterm -fg "#004400" -bg "#cccc99" "$@" &
+# Looks a bit grey on Linux, but acceptable it if we cannot accept the previous:
+#xterm -fg "#004400" -bg "#bbbb99" "$@" &
+# Attempt at a compromise:
+#xterm -fg "#004400" -bg "#cccca4" "$@" &
+
+if [ "$(uname)" = "Linux" ]
+then xterm -fg "#004400" -bg "#cccc99" "$@" &
+else xterm -fg "#004400" -bg "#ccccaa" "$@" &
+fi
 
 # Black on green (like the SpyAmp's LCD theme):
 #xterm -fg "#222222" -bg "#88bb88" "$@" &
