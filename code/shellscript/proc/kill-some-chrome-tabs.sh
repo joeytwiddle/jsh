f20983963dc4f957936b4754beb5fade4611bc87
Paul Joey Clark <joeytwiddle@gmail.com>
2018-06-06 13:25:04 +0800

    Include processes even if their memory usage isn't the highest

--- code/shellscript/proc/kill-some-chrome-tabs.sh
+++ code/shellscript/proc/kill-some-chrome-tabs.sh
@@ -3,6 +3,9 @@
 # When Linux gets low on memory, earlyoom kills some Chrome tabs for me
 # When Mac OS X gets low on memory, we can kill some Chrome tabs using this script
 
-#ps -o time,ppid,pid,nice,pcpu,pmem,user,args -A | sort -n -k 6 |
+ps -o time,ppid,pid,nice,pcpu,pmem,user,args -A | sort -n -k 6 |
+#wotgobblemem |
 
-wotgobblemem | grep "Google Chrome Helper" | grep -v ' --extension-process ' | takecols 3 | tail -n 5 | withalldo kill
+grep "Google Chrome Helper" | grep -v ' --extension-process ' |
+
+tail -n 5 | pipeboth | takecols 3 | withalldo kill
