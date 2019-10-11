2d3f5a99c9b10545733b461c4acd96a1e2d42831
Paul Joey Clark <joeytwiddle@gmail.com>
2019-10-11 17:43:37 +0800

    Update npmsearch; add npmscripts

---
+++ code/shellscript/npm/npmscripts.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+while true
+do
+	if [ -f package.json ]
+	then
+	if command -v jq >/dev/null
+	then jq .scripts package.json
+	else node -e "console.log(JSON.parse(require('fs').readFileSync('./package.json')).scripts)"
+	# Flattened
+	#else node -e "console.log(Object.entries(JSON.parse(require('fs').readFileSync('./package.json')).scripts).map(p => p.join(': ')).join('\n'))" | highlight '^.*: '
+	fi
+	exit "$?"
+	fi
+	if [ "$PWD" = / ]
+	then
+	echo >&2 "Could not find package.json"
+	exit 1
+	fi
+	cd ..
+done
--- code/shellscript/npm/npmsearch.sh
+++ code/shellscript/npm/npmsearch.sh
@@ -2,13 +2,22 @@
 #      and: https://www.npmjs.org/package/local-npm
 # Apparently npmd can do search also, but I never got that working :P
 
-browse "https://www.npmjs.com/search?q=$*"
-browse "https://npmsearch.com/?q=$*"
+#browse "https://www.npmjs.com/search?q=$*"
+#browse "https://npmsearch.com/?q=$*"
+
+# We now have this
+npm search "$*"
+#npm search --long "$*"
+
+# npms shows the scores for each match
+#npx npms search --color -s 250 "$*" | less -REX
 
 # Performs a daily download, but not so bloated as npm's.  (About 1 minute.)
 # It orders things by stars, but it also seems to skip some packages with 0 stars.
 #nipster "$*"
 exit
+
+
 
 memo -nd -t "2 months" npm search . |
 sed 's+ *$++' |   # I don't know why but one of the memo-s I cached in June 2014 had a lot of trailing spaces
