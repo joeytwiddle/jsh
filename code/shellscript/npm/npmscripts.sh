8c54bff24d460c8cce18675dd49afc79c93fd6d1
Paul Joey Clark <joeytwiddle@gmail.com>
2019-10-16 11:53:19 +0800

    fix indentation

--- code/shellscript/npm/npmscripts.sh
+++ code/shellscript/npm/npmscripts.sh
@@ -4,18 +4,18 @@ while true
 do
 	if [ -f package.json ]
 	then
-	if command -v jq >/dev/null
-	then jq .scripts package.json
-	else node -e "console.log(JSON.parse(require('fs').readFileSync('./package.json')).scripts)"
-	# Flattened
-	#else node -e "console.log(Object.entries(JSON.parse(require('fs').readFileSync('./package.json')).scripts).map(p => p.join(': ')).join('\n'))" | highlight '^.*: '
-	fi
-	exit "$?"
+		if command -v jq >/dev/null
+		then jq .scripts package.json
+		else node -e "console.log(JSON.parse(require('fs').readFileSync('./package.json')).scripts)"
+		# Flattened
+		#else node -e "console.log(Object.entries(JSON.parse(require('fs').readFileSync('./package.json')).scripts).map(p => p.join(': ')).join('\n'))" | highlight '^.*: '
+		fi
+		exit "$?"
 	fi
 	if [ "$PWD" = / ]
 	then
-	echo >&2 "Could not find package.json"
-	exit 1
+		echo >&2 "Could not find package.json"
+		exit 1
 	fi
 	cd ..
 done
