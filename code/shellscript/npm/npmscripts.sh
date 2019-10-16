a1e7798fc2a5d5784855ca8d251c9eb2deb4ace1
Paul Joey Clark <joeytwiddle@gmail.com>
2019-10-16 14:14:52 +0800

    Simplify: use npm root to find the package.json

--- code/shellscript/npm/npmscripts.sh
+++ code/shellscript/npm/npmscripts.sh
@@ -1,21 +1,18 @@
 #!/bin/sh
 
-while true
-do
-	if [ -f package.json ]
-	then
-		if command -v jq >/dev/null
-		then jq .scripts package.json
-		else node -e "console.log(JSON.parse(require('fs').readFileSync('./package.json')).scripts)"
-		# Flattened
-		#else node -e "console.log(Object.entries(JSON.parse(require('fs').readFileSync('./package.json')).scripts).map(p => p.join(': ')).join('\n'))" | highlight '^.*: '
-		fi
-		exit "$?"
-	fi
-	if [ "$PWD" = / ]
-	then
-		echo >&2 "Could not find package.json"
-		exit 1
-	fi
-	cd ..
-done
+npm_root="$(npm root)"
+
+package_json="${npm_root}/../package.json"
+
+if [ -z "$npm_root" ] || [ ! -f "$package_json" ]
+then
+	echo >&2 "Could not find package.json"
+	exit 1
+fi
+
+if command -v jq >/dev/null
+then jq .scripts package.json
+else node -e "console.log(JSON.parse(require('fs').readFileSync('${package_json}')).scripts)"
+# Flattened
+#else node -e "console.log(Object.entries(JSON.parse(require('fs').readFileSync('${package_json}')).scripts).map(p => p.join(': ')).join('\n'))" | highlight '^.*: '
+fi
