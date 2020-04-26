26d20e05dff35360bf9d127d962e7eaf13b040a1
Paul Joey Clark <joeytwiddle@gmail.com>
2020-04-26 17:45:15 +0800

    Minor

--- code/shellscript/git/git-commit-by-date.sh
+++ code/shellscript/git/git-commit-by-date.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
+# See also: git-commit-all-with-name
+
+set -e
+
 fnd () {
-  find . -type f "$@" | grep -v '/.git/'
+  find . -type f "$@" | grep -v -F '/.git/'
 }
 
 fnd | sortfilesbydate | sed 's+^./++' |
 while read file
 do
-    fnd -not -newer "$file" | withalldo git add
-    date="$(LC_TIME=C date -r "$file")"
-    GIT_AUTHOR_DATE="$date" GIT_COMMITTER_DATE="$date" git commit -m "Add $file"
+  fnd -not -newer "$file" | withalldo git add
+  date="$(LC_TIME=C date -r "$file")"
+  GIT_AUTHOR_DATE="$date" GIT_COMMITTER_DATE="$date" git commit -m "Add $file"
 done
