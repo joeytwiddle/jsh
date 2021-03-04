89e560a1b211465fb91f98f4de7ca7e4aa4396ef
Paul Joey Clark <joeytwiddle@gmail.com>
2021-03-04 16:44:16 +0800

    Convert code to modern style

--- code/shellscript/init/startj-simple.sh
+++ code/shellscript/init/startj-simple.sh
@@ -1,28 +1,30 @@
 # @sourceme
 
-## Hmmm.  At very least this should call startj -simple, or startj should call this.
+# Hmmm.  At very least this should call startj -simple, or startj should call this.
 
-## Try to guess the top directory of j install
-## If all below fails, then you should set it youself with export JPATH=...; source $JPATH/startj
-if test -z "$JPATH"; then
-	if test -d "$HOME/j"; then
-		export JPATH=$HOME/j
-	## This doesn't work: bash cannot see it unless we call startj direct (no source)
-	elif test -d `dirname "$0"`; then
-		export JPATH=`dirname "$0"`
+# Try to guess the top directory of j install
+# If all below fails, then you should set it youself with export JPATH=...; source $JPATH/startj
+if [ -z "$JPATH" ]
+then
+	if [ -d "$HOME/j" ]
+	then export JPATH="$HOME/j"
+	# This doesn't work: bash cannot see it unless we call startj direct (no source)
+	elif [ -d "$(dirname "$0")" ]
+	then
+		export JPATH="$(dirname "$0")"
 		echo "startj: guessed JPATH=$JPATH"
 	else
 		echo "startj: Could not find JPATH. Not starting."
-		# env > /tmp/env.out
+		#env > /tmp/env.out
 		exit 0
 	fi
 fi
-# export PATH="$JPATH/tools:$PATH"
+#export PATH="$JPATH/tools:$PATH"
 export PATH="$PATH:$JPATH/tools"
 
-## Although we don't need things like aliases, we do need things like nvm setup.
+# Although we don't need things like aliases, we do need things like nvm setup.
 [ -f "$JPATH/global.conf" ] && . "$JPATH/global.conf"
 [ -f "$JPATH/local.conf" ] && . "$JPATH/local.conf"
 
-# . javainit
-# . hugsinit
+#. javainit
+#. hugsinit
