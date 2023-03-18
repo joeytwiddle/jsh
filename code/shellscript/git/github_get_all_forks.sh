92b3cc7a1e626a21942bbc63c3ce9bcdf8b23db6
Paul Joey Clark <joeytwiddle@gmail.com>
2023-03-19 00:52:00 +0800

    Use .url because .git_url can have auth issues

--- code/shellscript/git/github_get_all_forks.sh
+++ code/shellscript/git/github_get_all_forks.sh
@@ -10,12 +10,14 @@ full_repo_name="$(echo "$origin_url" | sed 's+.*github.com/++')"
 forks_url="https://api.github.com/repos/${full_repo_name}/forks"
 
 #[ -e "forks.json" ] ||
-curl -s "https://api.github.com/repos/gcanti/tcomb-form-native/forks" -o forks.json
+curl -s "$forks_url" -o forks.json
 
 node -e "
   var forks = JSON.parse(fs.readFileSync('forks.json', 'utf-8'));
   forks.forEach(forkData => {
-    console.log('git remote add \"' + forkData.owner.login + '\" \"' + forkData.git_url + '\"');
+    // git_url stopped working with the message: The unauthenticated git protocol on port 9418 is no longer supported.
+    // But we can choose one of: url, ssh_url, clone_url
+    console.log('git remote add \"' + forkData.owner.login + '\" \"' + forkData.url + '\"');
   });
   console.log('git fetch --all');
 " |
