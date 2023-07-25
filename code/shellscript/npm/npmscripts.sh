88fb40cc09cec5fc5195e51ca49e560c002e745d
Paul Joey Clark <joeytwiddle@gmail.com>
2023-07-25 16:28:31 +0800

    Use project_root instead of npm_root (because .. does not work if node_modules was not created yet)

--- code/shellscript/npm/npmscripts.sh
+++ code/shellscript/npm/npmscripts.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
 
-npm_root="$(npm root)"
+# See also: npm run
+# See also: yarn run
 
-package_json="${npm_root}/../package.json"
+project_root="$(dirname "$(npm root)")"
 
-if [ -z "$npm_root" ] || [ ! -f "$package_json" ]
+package_json="${project_root}/package.json"
+
+if [ ! -f "$package_json" ]
 then
 	echo >&2 "Could not find package.json"
 	exit 1
 fi
 
 if command -v jq >/dev/null
-then jq .scripts package.json
-else node -e "console.log(JSON.parse(require('fs').readFileSync('${package_json}')).scripts)"
-# Flattened
-#else node -e "console.log(Object.entries(JSON.parse(require('fs').readFileSync('${package_json}')).scripts).map(p => p.join(': ')).join('\n'))" | highlight '^.*: '
+then jq .scripts "$package_json"
+else
+	#node -e "console.log(JSON.parse(require('fs').readFileSync('${package_json}')).scripts)"
+	# Flattened
+	#node -e "console.log(Object.entries(JSON.parse(require('fs').readFileSync('${package_json}')).scripts).map(p => p.join(': ')).join('\n'))" | highlight '^.*: '
+	node -e "console.log(Object.entries(JSON.parse(require('fs').readFileSync('${package_json}')).scripts).map(p => p.join(' ')).join('\n'))" | columnise-clever -ignore '^[^ ]* *[^ ]*' | highlight -bold '^[^ ]* ' red
 fi
