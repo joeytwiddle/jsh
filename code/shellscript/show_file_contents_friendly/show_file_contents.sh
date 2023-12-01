8848cdfb3d03f42e3e4a1b0d1fd6ccd21f88fecf
Paul Joey Clark <joeytwiddle@gmail.com>
2023-12-01 11:11:11 +0000

    Work on multiple input files

--- code/shellscript/show_file_contents_friendly/show_file_contents.sh
+++ code/shellscript/show_file_contents_friendly/show_file_contents.sh
@@ -4,8 +4,6 @@ set -e
 # If the file is a text file, this script will show you the contents
 # But if it's not a text file, then it will try to work out what type of file it is, and show details about it, or what is inside it
 
-filename="$1"
-
 # Adapted from https://unix.stackexchange.com/a/275691/33967
 is_binary() {
     LC_MESSAGES=C grep -Hm1 '^' < "$1" 2>&1 | grep -q '\(^Binary\|: binary file matches$\)'
@@ -20,26 +18,32 @@ pager() {
     fi
 }
 
-if [ -z "$filename" ]
+if [ -z "$1" ] || [ "x$1" = "x-" ]
 then
     # Presumably we are being piped input
     pager -
-elif is_archive "$filename"
-then
-    #nicels -l "$filename"
-    #echo
-    #echo "Content:"
-    list_files_in_archive "$filename"
-elif is_binary "$filename"
-then
-    file "$filename"
-elif [ -d "$filename" ]
-then
-    nicels -l "$filename/"
-elif [ -f "$filename" ]
-then
-    # Show contents
-    pager "$filename"
-else
-    nicels -ld "$filename"
+    exit "$?"
 fi
+
+for filename
+do
+    if is_archive "$filename"
+    then
+        #nicels -l "$filename"
+        #echo
+        #echo "Content:"
+        list_files_in_archive "$filename"
+    elif is_binary "$filename"
+    then
+        file "$filename"
+    elif [ -d "$filename" ]
+    then
+        nicels -l "$filename/"
+    elif [ -f "$filename" ]
+    then
+        # Show contents
+        pager "$filename"
+    else
+        nicels -ld "$filename"
+    fi
+done
