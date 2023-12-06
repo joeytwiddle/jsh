703e5b2b25b233aa3fe3a4279f525a2eece4c5d8
Paul Joey Clark <joeytwiddle@gmail.com>
2023-12-06 14:05:42 +0000

    Update comments

--- code/shellscript/git/git-fetch-all-prs.sh
+++ code/shellscript/git/git-fetch-all-prs.sh
@@ -6,24 +6,28 @@
 # Note that if you want to make some amendments, you will not be able to push
 # back to these branches, like you can when you add forks as remotes.
 #
-# From here: https://gist.github.com/piscisaureus/3342247
+# Explanation here:
+# https://gist.github.com/piscisaureus/3342247
+#
+# But we currently use the implementation from here:
+# https://gist.github.com/chernjie/b16fe4dccf3f386d52ff
 
 # See also: A perl tool to help manage PRs from the command line:
 #
 # https://github.com/robinsmidsrod/App-GitHubPullRequest
 
-# There is sometimes a bug (fixed now?) which can be solved by putting the PR
-# line before the normal fetch line:
+# There used to be a bug which can be solved by putting the PR line before the
+# normal fetch line:
 #
-# https://gist.github.com/piscisaureus/3342247#gistcomment-776888 (bash script)
+# https://gist.github.com/piscisaureus/3342247#gistcomment-776888
+#
+# https://github.com/jasoncodes/dotfiles/blob/a29509902cb839b6aaf26d2d5409d66199b86d1d/shell/aliases/git.sh#L93-L110
 #
 # https://gist.github.com/piscisaureus/3342247#gistcomment-1292509
+#
+# But I think the bug has been fixed now, so we no longer need to worry.
 
-# https://github.com/jasoncodes/dotfiles/blob/a29509902cb839b6aaf26d2d5409d66199b86d1d/shell/aliases/git.sh#L93-L110
-
-# From https://gist.github.com/chernjie/b16fe4dccf3f386d52ff
 # NOTE: This operates on all remotes, although usually we only need it for 'origin'.
-# TODO: Should really arrange the two entries in reverse, as mentioned in #gistcomment-1292509 above.
 git remote -v | grep fetch | grep github | \
     while read remote url _; do
         if ! git config --get-all "remote.$remote.fetch" | grep -q refs/pull
