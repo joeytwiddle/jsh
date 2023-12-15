32afc97feeff6506ea0bd1f9756228608cfbbc71
Paul Joey Clark <joeytwiddle@gmail.com>
2023-12-15 11:37:21 +0000

    Remove duplication, add workaround

--- code/shellscript/git/git-compare-branches.sh
+++ code/shellscript/git/git-compare-branches.sh
@@ -20,27 +20,32 @@ else
 	exit 1
 fi
 
-alias glc='git log --pretty=format:"%C(yellow bold)%h%C(magenta bold)%d%C(reset) %C(black bold)%s %C(reset)%C(cyan)- %an (%ad)%Creset" --date=relative'
 shopt -s expand_aliases
-
+# Very basic
+#alias glc='git log --pretty=oneline'
+# Standard one-liner, with fewer details
+#alias glc='git log --pretty=oneline --decorate --abbrev-commit'
+# More details, using default colours (mostly white)
+alias glc="git log --pretty=format:'%C(auto)%h%d %s - %an (%ad)' --abbrev-commit --date=relative"
 if [ "$USER" = joey ]
 then
 	# My favourite, but the colours might not suit all users
+	unalias glc
 	alias glc='git log --pretty=format:"%C(yellow bold)%h%C(magenta bold)%d%C(reset) %C(black bold)%s %C(reset)%C(cyan)- %an (%ad)%Creset" --date=relative'
-else
-	# Standard one-liner, with fewer details
-	#alias glc='git log --pretty=oneline --decorate --abbrev-commit'
-	# More details, using default colours (mostly white)
-	alias glc="git log --pretty=format:'%C(auto)%h%d %s - %an (%ad)' --abbrev-commit --date=relative"
 fi
-shopt -s expand_aliases
 
 base_commit="$(git merge-base "$branch1" "$branch2")"
 
 #echo "### Base commit"
 #glc -n 1 "$base_commit"
 
-echo "<<< Only on ${branch2}"
-glc "${base_commit}...${branch2}"
-echo ">>> Only on ${branch1}"
-glc "${base_commit}...${branch1}"
+(
+	echo "<<< Only on ${branch2}"
+	glc --color=always "${base_commit}...${branch2}"
+	# On macOS, the previous command was not emitting a trailing newline
+	# (Although curiously it works properly with the "Very basic" alias)
+	echo
+	echo ">>> Only on ${branch1}"
+	glc --color=always "${base_commit}...${branch1}"
+) |
+less -RX
