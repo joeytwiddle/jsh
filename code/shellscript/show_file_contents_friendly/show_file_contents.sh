dbbf3a1ce3919085ec5f119b0cf4765469bc264f
Paul Joey Clark <joeytwiddle@gmail.com>
2023-12-23 17:25:26 +0000

    Revert "Work on multiple input files"
    This reverts commit 8848cdfb3d03f42e3e4a1b0d1fd6ccd21f88fecf.

--- code/shellscript/show_file_contents_friendly/show_file_contents.sh
+++ code/shellscript/show_file_contents_friendly/show_file_contents.sh
@@ -4,6 +4,8 @@ set -e
 # If the file is a text file, this script will show you the contents
 # But if it's not a text file, then it will try to work out what type of file it is, and show details about it, or what is inside it
 
+filename="$1"
+
 # Adapted from https://unix.stackexchange.com/a/275691/33967
 is_binary() {
     LC_MESSAGES=C grep -Hm1 '^' < "$1" 2>&1 | grep -q '\(^Binary\|: binary file matches$\)'
@@ -18,32 +20,26 @@ pager() {
     fi
 }
 
-if [ -z "$1" ] || [ "x$1" = "x-" ]
+if [ -z "$filename" ]
 then
     # Presumably we are being piped input
     pager -
-    exit "$?"
+elif is_archive "$filename"
+then
+    #nicels -l "$filename"
+    #echo
+    #echo "Content:"
+    list_files_in_archive "$filename"
+elif is_binary "$filename"
+then
+    file "$filename"
+elif [ -d "$filename" ]
+then
+    nicels -l "$filename/"
+elif [ -f "$filename" ]
+then
+    # Show contents
+    pager "$filename"
+else
+    nicels -ld "$filename"
 fi
-
-for filename
-do
-    if is_archive "$filename"
-    then
-        #nicels -l "$filename"
-        #echo
-        #echo "Content:"
-        list_files_in_archive "$filename"
-    elif is_binary "$filename"
-    then
-        file "$filename"
-    elif [ -d "$filename" ]
-    then
-        nicels -l "$filename/"
-    elif [ -f "$filename" ]
-    then
-        # Show contents
-        pager "$filename"
-    else
-        nicels -ld "$filename"
-    fi
-done
