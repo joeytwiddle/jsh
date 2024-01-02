bb33563bf6e08437f304255cc89358dbc02fc343
Paul Joey Clark <joeytwiddle@gmail.com>
2024-01-02 13:17:53 +0000

    Update comments

--- code/shellscript/git/git-undo-reset.sh
+++ code/shellscript/git/git-undo-reset.sh
@@ -9,6 +9,9 @@ verbosely git fsck --lost-found
 #     gitk --all $( git fsck --no-reflog | awk '/dangling commit/ {print $3}' )
 # From here: https://stackoverflow.com/questions/89332/how-do-i-recover-a-dropped-stash-in-git
 
-# I tried formatting the output, but I got fewer results that git fsck listed!
-# I'm not sure for-each-ref is the right thing to use.  Maybe that only works on branhces?
-#     hashed="$(memo git fsck --lost-found | cut -d' ' -f 3)" ; git for-each-ref --sort=committerdate --format="%(refname) (%(refname:short)) ${author_color}%(committerdate:relative) (%(authorname)) -${reset_color} ${subject_color}%(subject)${reset_color}" $hashes
+# I tried formatting the output, but it didn't list everything.
+# I'm not sure for-each-ref is the right thing to use.  I think that only works on branches?
+#     hashes="$(memo git fsck --lost-found | cut -d' ' -f 3)" ; git for-each-ref --sort=committerdate --format="%(refname) (%(refname:short)) ${author_color}%(committerdate:relative) (%(authorname)) -${reset_color} ${subject_color}%(subject)${reset_color}" ${hashes}
+
+# Another script attempt.  Better, but I would like to add the sorting.
+#     git fsck --lost-found | grep -o '\<[0-9a-z]*$' | TODO_sort_blobs_by_date | while read blob; do git show --color=always "$blob"; done | less -RX
