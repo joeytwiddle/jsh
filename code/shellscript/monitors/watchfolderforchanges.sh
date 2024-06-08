6e8f3a87279c7313ad9fc6efd85df4aa77cb8289
Paul Joey Clark <joeytwiddle@gmail.com>
2024-06-08 21:34:44 +0100

    Only show new line, not old line

--- code/shellscript/monitors/watchfolderforchanges.sh
+++ code/shellscript/monitors/watchfolderforchanges.sh
@@ -2,4 +2,4 @@
 # See also: watchforfileaccess
 [ "$DELAY" ] || DELAY=15
 TARGET_DIRS="$*" ## BUG: Fails on dirs with spaces.  Could fix with: for X in "$@"; do ...
-jwatch "find $TARGET_DIRS -maxdepth 4 -type f | withalldo nicels -ld" 2>&1 | dateeachline
+jwatch -oneway "find $TARGET_DIRS -maxdepth 4 -type f | withalldo nicels -ld" 2>&1 | dateeachline
