f61dd8bd0b2c8fa127a76f9f0d5725baa10ce773
Joey Clark <joey.clark@virginmediao2.co.uk>
2024-11-19 18:23:25 +0000

    Force color (actually for git-undo-reset)

--- code/shellscript/git/git-list-dropped-stashes.sh
+++ code/shellscript/git/git-list-dropped-stashes.sh
@@ -2,9 +2,10 @@
 
 # Whoops did you just pop a stash and then lose the changes with an accidental reset?  Don't panic, that stash is probably still in history.
 
+# From here: https://stackoverflow.com/questions/89332/how-do-i-recover-a-dropped-stash-in-git
 list_of_commits="$(memo git fsck --no-reflog | awk '/dangling commit/ {print $3}')"
 
 # Optional but useful: sort the commits into reverse date order
 sorted_list="$(git show --pretty="%at %H" --no-patch ${list_of_commits} | sort -n -k 1 -r | awk '{print $2}')"
 
-git show --stat -p ${sorted_list}
+git show --color=always --stat -p ${sorted_list}
