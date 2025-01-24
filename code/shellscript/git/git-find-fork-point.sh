aac1265d347a878111ff35caef3bae03a13b7912
Joey Clark <joey.clark@virginmediao2.co.uk>
2025-01-24 16:15:48 +0200

    Fix git-find-fork-point so it actually works

--- code/shellscript/git/git-find-fork-point.sh
+++ code/shellscript/git/git-find-fork-point.sh
@@ -33,23 +33,38 @@ else
 fi
 
 # Get the tree hashes for the 'other' branch
-# I think --no-walk will not look inside the merges of the other branch, but that's something we probably need to do
-other_tree_hashes="$(git rev-list -n 50 "$other_branch" | while read commit; do git rev-parse "${commit}^{tree}"; done)"
+# This is quite heavy, so we won't look too far back
+other_tree_hashes="$(
+	git rev-list -n 100 "$other_branch" |
+	while read commit
+	do printf "%s\n" "$(git rev-parse "${commit}^{tree}") ${commit}"
+	done
+)"
 
 # Iterate through the commits in the 'current' branch
 git rev-list -n 500 "$current_branch" |
 while read commit
 do
-	# Get the tree hash for the current commit
+	# Get the tree hash for this commit
 	current_tree_hash="$(git rev-parse "$commit^{tree}")"
 
 	# Check if the current tree hash is in the other branch's tree hashes
-	if printf "%s\n" "$other_tree_hashes" | grep -q "$current_tree_hash"
+	if other_commit="$(printf "%s\n" "$other_tree_hashes" | grep -m 1 "^${current_tree_hash} " | cut -d ' ' -f 2 | grep .)"
 	then
-		echo "First commit in $current_branch which matches a commit in $other_branch is: $commit"
-		echo
-		echo "You may now like to: 'git rebase -i $other_branch' and delete all commits up to and including $commit"
-		exit 0
+		if [ "$other_commit" = "$commit" ]
+		then
+			echo "Branch ${current_branch} and branch ${other_commit} have a common ancestor: ${commit}"
+			exit 0
+		else
+			echo "Commit ${commit} in ${current_branch} has an identical file tree to commit ${other_commit} in ${other_branch}"
+			echo
+			echo "Therefore commit ${commit} is redundant if you rebase against ${other_branch}"
+			echo
+			echo "You may now like to:"
+			echo "  git rebase -i ${other_branch}"
+			echo "and delete all commits up to and including ${commit}"
+			exit 0
+		fi
 	fi
 done | grep ^ ||
 echo "No matching tree hash found in '$current_branch'."
