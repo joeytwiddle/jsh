6d546b2c658aba4faff5b12ad2e1e6b07c90e704
Joey Clark <joey.clark@virginmediao2.co.uk>
2025-01-31 09:49:36 +0200

    When using catch, try to preserve pretty colors

--- code/shellscript/wrap/catch.sh
+++ code/shellscript/wrap/catch.sh
@@ -4,9 +4,17 @@ export TMPFILE=`jgettmp caught-err`
 (
 	# "$@" 2>&1
 	# eval "$@" 2>&1
+
+	# When using catch, try to preserve pretty colors
+	export COLOR=always
+	export FORCE_COLOR=1
+	export NPM_CONFIG_COLOR=always
+
 	highlightstderr "$@" 2>&1
 	echo "$?" > "$TMPFILE"
 ) | less -RX
+
+#script -q /dev/null "$@" 2>&1 | less -RX
 
 CAUGHTERR=`cat "$TMPFILE"`
 jdeltmp "$TMPFILE"
