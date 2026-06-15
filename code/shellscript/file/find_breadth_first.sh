2f30d0c0653eb665445b507b7efc3b2e9a847b88
Paul Joey Clark <joeytwiddle@gmail.com>
2026-06-15 18:11:23 +0100

    find_breadth_first: Do not quit find_breadth_first when we run out of results

--- code/shellscript/file/find_breadth_first.sh
+++ code/shellscript/file/find_breadth_first.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
+set -e
+
+MAX_DEPTH="${MAX_DEPTH:-8}"
 
 dir="$1" ; shift
 
 depth=1
 
-# Loop as long as find returns any results for the current depth
 while true
 do
     # -printf "%d\t%p\n"
@@ -13,8 +15,16 @@ do
       then sort
       else cat
       fi |
-      grep . || break
+      # Loop as long as find returns any results for the current depth
+      #grep . || break
+      # Loop until MAX_DEPTH is reached
+      cat
+      # Orders results, but takes longer for deep results to appear
+      #sort
     depth="$((depth + 1))"
+    if [ "$depth" -gt "$MAX_DEPTH" ]
+    then break
+    fi
 done
 
 true
